4b05ba30e88564558c444bc5f17b36d9d443df12
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2020-12-04 16:28:18 -0500

    Manipulating symlinks in script

--- generate-debuggable-environment.sh
+++ generate-debuggable-environment.sh
@@ -11,7 +11,8 @@ mkdir -p build/ios
 cd build/ios
 cmake ../.. -DBUILD_SHARED_LIBS=OFF \
             -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="./lib"\
-            -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_SYSROOT=iphonesimulator \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_OSX_SYSROOT=iphonesimulator \
             -DCMAKE_SYSTEM_NAME=iOS \
             -DMAPBOX_COMMON_BUILD_TYPE='SHARED' \
             -DMAPBOX_COMMON_TARGET_NAME='MapboxCommon' \
@@ -26,7 +27,15 @@ cd mapbox-maps-ios
 git checkout main && git pull
 git checkout $2
 make deps
+
+# Manipulating symlinks
+cd lib
+rm MapboxCommon.framework
+rm MapboxCoreMaps.framework
+ln -s ./../../DerivedData/Umbrella/Build/Products/Debug-iphonesimulator/MapboxCoreMaps.framework .
+ln -s ./../../mapbox-gl-native-internal/build/ios/lib/Debug/MapboxCommon.framework .
 cd ..
 
 # Open the workspace
+cd ..
 xed Umbrella.xcworkspace

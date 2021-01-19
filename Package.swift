4ab281407b52f21ae4e4b821b387792c4774919d
neel mistry <neel.mistry@mapbox.com>
2021-01-19 11:30:57 -0500

    use https

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@
              targets: ["MapboxMapsWrapper"]),
      ],
      dependencies: [
-         .package(name: "MapboxCommon", url: "git@github.com:mapbox/mapbox-common-ios.git", .exact("10.0.0-beta.4")),
-         .package(name: "MapboxCoreMaps", url: "git@github.com:mapbox/mapbox-core-maps-ios.git", .exact("10.0.0-beta.11")),
+         .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("10.0.0-beta.4")),
+         .package(name: "MapboxCoreMaps", url: "https://github.com/mapbox/mapbox-core-maps-ios.git", .exact("10.0.0-beta.11")),
          .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", .exact("0.10.7")),
          .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", .exact("2.0.0-alpha.1")),
      ],

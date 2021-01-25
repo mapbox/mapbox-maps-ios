fb1199f976720315ab491cc7556dfe2dbaeb23cd
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-01-25 00:18:48 -0500

    Add Light tests + fix inconsistency in use of ColorRepresentable

--- Mapbox/MapboxMapsStyle/Generated/Light/Light.swift
+++ Mapbox/MapboxMapsStyle/Generated/Light/Light.swift
@@ -11,53 +11,34 @@ import MapboxCommon
  */
 public struct Light: Codable {  
 
-  /// Whether extruded geometries are lit relative to the map or viewport.
-  public var anchor: Anchor?  
-
-  
+    /// Whether extruded geometries are lit relative to the map or viewport.
+  public var anchor: Anchor?
   /// Color tint for lighting extruded geometries.
-  public var color: UIColor? {
-    get {
-    
-      return UIColor(hex: colorInternal)
-    
-    } set(newVal) {
-    
-      if let validValue = newVal {
-        self.colorInternal = ColorRepresentable(color: validValue) 
-      }
-    
-    }
-  }
-
-  private var colorInternal: ColorRepresentable?
-  
+  public var color: ColorRepresentable?
   
   /// Transition property for `color`
   public var colorTransition: StyleTransition?
-  
-  /// Intensity of lighting (on a scale from 0 to 1). Higher numbers will present as more extreme contrast.
-  public var intensity: Double?  
 
+  /// Intensity of lighting (on a scale from 0 to 1). Higher numbers will present as more extreme contrast.
+  public var intensity: Double?
   
   /// Transition property for `intensity`
   public var intensityTransition: StyleTransition?
-  
-  /// Position of the light source relative to lit (extruded) geometries, in [r radial coordinate, a azimuthal angle, p polar angle] where r indicates the distance from the center of the base of an object to its light, a indicates the position of the light relative to 0&#xB0; (0&#xB0; when `light.anchor` is set to `viewport` corresponds to the top of the viewport, or 0&#xB0; when `light.anchor` is set to `map` corresponds to due north, and degrees proceed clockwise), and p indicates the height of the light (from 0&#xB0;, directly above, to 180&#xB0;, directly below).
-  public var position: [Double]?  
 
+  /// Position of the light source relative to lit (extruded) geometries, in [r radial coordinate, a azimuthal angle, p polar angle] where r indicates the distance from the center of the base of an object to its light, a indicates the position of the light relative to 0&#xB0; (0&#xB0; when `light.anchor` is set to `viewport` corresponds to the top of the viewport, or 0&#xB0; when `light.anchor` is set to `map` corresponds to due north, and degrees proceed clockwise), and p indicates the height of the light (from 0&#xB0;, directly above, to 180&#xB0;, directly below).
+  public var position: [Double]?
   
   /// Transition property for `position`
   public var positionTransition: StyleTransition?
+
   
-    
   public init() { }
 
   enum CodingKeys: String, CodingKey {
     
     case anchor = "anchor"
     
-    case colorInternal = "color"
+    case color = "color"
     case colorTransition = "color-transition"
         
     case intensity = "intensity"

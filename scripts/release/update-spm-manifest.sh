d2856add9c9dc637df8abb469ea70cac9251d635
Neel Mistry <neel.mistry@mapbox.com>
2021-02-04 17:09:43 -0500

    automation to updating podspec and package manifest

--- scripts/release/update-spm-manifest.sh
+++ scripts/release/update-spm-manifest.sh
@@ -4,26 +4,36 @@ set -euo pipefail
 
 #
 # Usage:
-#   ./scripts/release/update-spm-manifest.sh <maps version number> <common version number> <core version number> <maps xcframework checksum>
+#   ./scripts/release/update-spm-manifest.sh <maps version number> <maps xcframework checksum> <common version number> <core version number>
 #
 
 MAPS_VERSION=${1}
-COMMON_VERSION=${2//v/}
-CORE_VERSION=${3//v/}
-CHECKSUM=${4}
-GITHUB_TOKEN=$(./scripts/get_token.js)
+CHECKSUM=${2}
+COMMON_VERSION=${3//v/}
+CORE_VERSION=${4//v/}
+EVENTS_VERSION=${5//v/}
+TURF_VERSION=${6//v/}
+GITHUB_TOKEN=$(./scripts/release/get_token.js)
 
 # Update Package.swift
 sed -i '' s/"let version = \".*\""/"let version = \"${MAPS_VERSION}\""/ Package.swift
 sed -i '' s/"let checksum = \".*\""/"let checksum = \"${CHECKSUM}\""/ Package.swift
 sed -i '' s/"mapbox-common-ios.git\", .exact(\".*\")"/"mapbox-common-ios.git\", .exact(\"${COMMON_VERSION}\")"/ Package.swift
 sed -i '' s/"mapbox-core-maps-ios.git\", .exact(\".*\")"/"mapbox-core-maps-ios.git\", .exact(\"${CORE_VERSION}\")"/ Package.swift
+sed -i '' s/"mapbox-events-ios.git\", .exact(\".*\")"/"mapbox-events-ios.git\", .exact(\"${EVENTS_VERSION}\")"/ Package.swift
+sed -i '' s/"turf-swift.git\", .exact(\".*\")"/"turf-swift.git\", .exact(\"${TURF_VERSION}\")"/ Package.swift
+
+# Update MapboxCoreMaps.podspec
+sed -i '' s/"maps_version = '.*'"/"maps_version = '${MAPS_VERSION}'"/ MapboxMaps.podspec
+sed -i '' s/"m.dependency 'MapboxCommon', '.*'"/"m.dependency 'MapboxCommon', '${COMMON_VERSION}'"/ MapboxMaps.podspec
+sed -i '' s/"m.dependency 'MapboxCoreMaps', '.*'"/"m.dependency 'MapboxCoreMaps', '${CORE_VERSION}'"/ MapboxMaps.podspec
+sed -i '' s/"m.dependency 'MapboxMobileEvents', '.*'"/"m.dependency 'MapboxMobileEvents', '${EVENTS_VERSION}'"/ MapboxMaps.podspec
 
 #
 # Commit to a branch
 #
-git add -A
-git commit -m "Update SPM configs for ${MAPS_VERSION} release"
+# git add -A
+# git commit -m "Update SPM configs for ${MAPS_VERSION} release"
 
 # TODO: Automate PR Creation when run on CI
 # #

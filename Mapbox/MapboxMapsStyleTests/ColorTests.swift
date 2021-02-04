8661e34f9baefceb1e9c2173b2170e49f03222f4
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-02-04 11:51:30 -0500

    clean up tests

--- Mapbox/MapboxMapsStyleTests/ColorTests.swift
+++ Mapbox/MapboxMapsStyleTests/ColorTests.swift
@@ -9,36 +9,36 @@ internal class ColorTests: XCTestCase {
 
     func testColorEncodingAndDecoding() throws {
         let testColor = UIColor(red: 0.537, green: 0.812, blue: 0.941, alpha: 0.3)
-        encodeAndDecodeColor(testColor: testColor)
+        verifyEncodeAndDecodeColor(testColor: testColor)
     }
 
     func testColorEncodingAndDecodingBlack() throws {
-        encodeAndDecodeColor(testColor: .black)
+        verifyEncodeAndDecodeColor(testColor: .black)
     }
 
     func testColorEncodingAndDecodingWhite() throws {
-        encodeAndDecodeColor(testColor: .white)
+        verifyEncodeAndDecodeColor(testColor: .white)
     }
 
-    func encodeAndDecodeColor(testColor: UIColor, test: String = #function) {
+    func verifyEncodeAndDecodeColor(testColor: UIColor, line: UInt = #line) {
         let color = ColorRepresentable(color: testColor)
         var data: Data?
         do {
             data = try JSONEncoder().encode(color)
         } catch {
-            XCTFail("Could not encode color in test: \(test)")
+            XCTFail("Could not encode color", line: line)
         }
 
         guard let validData = data else {
-            XCTFail("Color data is nil in test: \(test)")
+            XCTFail("Color data is nil", line: line)
             return
         }
 
         do {
             let decodedColor = try JSONDecoder().decode(ColorRepresentable.self, from: validData)
-            XCTAssert(decodedColor == color, "Color should not change after encoding - decoding pass in test: \(test)")
+            XCTAssert(decodedColor == color, "Color should not change after encoding - decoding pass", line: line)
         } catch {
-            XCTFail("Could not decode color in test: \(test)")
+            XCTFail("Could not decode color", line: line)
         }
     }
 }

a9035d9e666080726b7122f7a36d0208a619b56d
Nadia Barbosa <nadiabarbosa@me.com>
2021-02-05 12:01:05 -0800

    Rework implementation

--- Examples/Examples/All Examples/SwiftUIExample.swift
+++ Examples/Examples/All Examples/SwiftUIExample.swift
@@ -7,10 +7,12 @@ import SwiftUI
 public struct ContentView: View {
 
     public var body: some View {
+
         SwiftUIMapView(resourceOptions: ResourceOptions(accessToken: AccountManager.shared.accessToken!))
-            .styleURL(.dark)
-            .zoomLevel(14.0)
+            .zoomLevel(18.0)
             .centerCoordinate(CLLocationCoordinate2D(latitude: 20.6905, longitude: -88.2024))
+            .zoomLevel(14.0)
+            .styleURL(.dark)
             .on(.mapLoadingFinished) { ( mapView ) in
                 let pointAnnotation = PointAnnotation(coordinate: mapView.centerCoordinate)
                 mapView.annotationManager.addAnnotation(pointAnnotation)
@@ -18,46 +20,62 @@ public struct ContentView: View {
     }
 }
 
+/// Create a view model that is an `ObservableObject`
+/// so the map will be able to re-rendeer when map
+/// properties change.
+public class MapViewModel: ObservableObject {
+    @Published var centerCoordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    @Published var zoomLevel = CGFloat.zero
+    @Published var styleURL = StyleURL.streets
+}
+
 /// To render a `MapView` as a SwiftUI view, configure a new
 /// struct that implements the required `UIViewRepresentable` protocol.
 public struct SwiftUIMapView: UIViewRepresentable {
 
     let resourceOptions: ResourceOptions
-    internal let observerConcrete: ObserverConcrete // Manages the observation of map events
-    internal let mapView: MapView
+    @State internal var observerConcrete = ObserverConcrete() // Manages the observation of map events
+    @ObservedObject internal var mapViewModel: MapViewModel
 
     init(resourceOptions: ResourceOptions) {
         self.resourceOptions = resourceOptions
-        self.mapView = MapView(with: .zero, resourceOptions: resourceOptions)
-        self.observerConcrete = ObserverConcrete(mapView: self.mapView)
-
-        // Subscribe to map events
-        let events = MapEvents.EventKind.allCases.map { $0.rawValue }
-        try? mapView.__map.subscribe(for: observerConcrete, events: events)
+        self.mapViewModel = MapViewModel()
     }
 
     // MARK: - UIViewRepresentable required methods
     public func makeUIView(context: UIViewRepresentableContext<SwiftUIMapView>) -> MapboxMaps.MapView {
+        let mapView = MapView(with: .zero, resourceOptions: resourceOptions)
+
+        // Subscribe to map events
+        observerConcrete.mapView = mapView
+        let events = MapEvents.EventKind.allCases.map { $0.rawValue }
+        try? mapView.__map.subscribe(for: observerConcrete, events: events)
+
         return mapView
     }
 
     public func updateUIView(_ uiView: MapView, context: Context) {
         // Unimplemented
+        uiView.cameraManager.setCamera(centerCoordinate: mapViewModel.centerCoordinate,
+                                       zoom: mapViewModel.zoomLevel,
+                                       animated: false)
+        uiView.style.styleURL = mapViewModel.styleURL
     }
 
     // MARK: - Setting basic map properties
-    public func styleURL(_ newValue: StyleURL) -> SwiftUIMapView {
-        mapView.style.styleURL = newValue
+    
+    public func centerCoordinate(_ newValue: CLLocationCoordinate2D) -> SwiftUIMapView {
+        self.mapViewModel.centerCoordinate = newValue
         return self
     }
 
     public func zoomLevel(_ newValue: CGFloat) -> SwiftUIMapView {
-        mapView.cameraManager.setCamera(zoom: newValue)
+        self.mapViewModel.zoomLevel = newValue
         return self
     }
 
-    public func centerCoordinate(_ newValue: CLLocationCoordinate2D) -> SwiftUIMapView {
-        mapView.cameraManager.setCamera(centerCoordinate: newValue)
+    public func styleURL(_ newValue: StyleURL) -> SwiftUIMapView {
+        self.mapViewModel.styleURL = newValue
         return self
     }
 
@@ -73,17 +91,16 @@ public struct SwiftUIMapView: UIViewRepresentable {
     /// The nested class responsible for listening to map events.
     class ObserverConcrete: Observer {
         public var peer: MBXPeerWrapper?
-        internal let mapView: MapView
+        public var mapView: MapView?
 
-        init(mapView: MapView) {
-            self.mapView = mapView
-        }
+        init() { }
 
         /// Map of event types to subscribed event handlers.
         internal var eventHandlers: [String: [(MapboxMaps.MapView) -> Void]] = [:]
 
         /// Notify the correct handler when the event occurs.
         public func notify(for event: MapboxCoreMaps.Event) {
+            guard let mapView = mapView else { return }
             let handlers = eventHandlers[event.type]
             handlers?.forEach({ (handler) in
                 handler(mapView)

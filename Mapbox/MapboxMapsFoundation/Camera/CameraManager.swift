90e3b9f2ebf41dd0076cdeaec28ee6fd7eaa5d08
Neel Mistry <neel.mistry@mapbox.com>
2021-02-18 14:46:07 -0500

    updating docs

--- Mapbox/MapboxMapsFoundation/Camera/CameraManager.swift
+++ Mapbox/MapboxMapsFoundation/Camera/CameraManager.swift
@@ -399,6 +399,7 @@ public class CameraManager {
      - Parameter pitch: The degrees to adjust the map's tilt by.
      - Parameter zoom: The amount to adjust the camera's zoom level by.
      - Parameter animated: Indicates  whether the camera changes should be animated.
+     - Parameter didFling: Indicates if the camera is experiencing a fling, which means the offset value needs to be manipulated
      */
     public func moveCamera(by offset: CGPoint? = nil, rotation: CGFloat? = nil, pitch: CGFloat? = nil, zoom: CGFloat? = nil, animated: Bool = false, didFling: Bool = false) {
         guard let mapView = mapView else {
@@ -454,6 +455,7 @@ public class CameraManager {
     /**
      Return a new center coordinate shifted by a given offset value.
      - Parameter offset: The `CGPoint` value to shift the map's center by.
+     - Parameter didFling: Indicates if the camera is experiencing a fling, which means the offset value needs to be manipulated
      */
     func shiftCenterCoordinate(by offset: CGPoint, didFling: Bool) -> CLLocationCoordinate2D {
         guard let mapView = mapView else {

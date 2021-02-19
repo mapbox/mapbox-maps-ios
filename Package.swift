6cfc27d71860c7bf0b398f27497495c94c61e6f3
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-02-18 20:04:37 -0600

    Tidy up the package file formatting

--- Package.swift
+++ Package.swift
@@ -1,25 +1,25 @@
 // swift-tools-version:5.3
- // The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
- import PackageDescription
+import PackageDescription
 
- let package = Package(
-     name: "MapboxMaps",
-     platforms: [.iOS(.v11)],
-     products: [
-         .library(
-             name: "MapboxMaps",
-             targets: ["MapboxMaps"]),
-     ],
-     dependencies: [
+let package = Package(
+    name: "MapboxMaps",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "MapboxMaps",
+            targets: ["MapboxMaps"]),
+    ],
+    dependencies: [
         .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("10.0.0-beta.11")),
         .package(name: "MapboxCoreMaps", url: "https://github.com/mapbox/mapbox-core-maps-ios.git", .exact("10.0.0-beta.15")),
         .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", .exact("0.12.0-alpha.1")),
-        .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", "1.2.0"..<"2.0.0"),
-     ],
-     targets: [
-         .target(
-             name: "MapboxMaps",
+        .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", from: "1.2.0"),
+    ],
+    targets: [
+        .target(
+            name: "MapboxMaps",
             dependencies: ["MapboxCommon", "MapboxCoreMaps", "Turf", "MapboxMobileEvents"],
             exclude: [
                 "MapboxMaps/Info.plist",
@@ -32,34 +32,36 @@
                 "MapboxMapsOffline/Info.plist",
                 "MapboxMapsLocation/Info.plist",
                 "MapboxMapsOrnaments/Info.plist",
-                "MapboxMapsGestures/Info.plist"
+                "MapboxMapsGestures/Info.plist",
             ]
-         ),
-        .testTarget(name: "MapboxMapsTests",
-                    dependencies: ["MapboxMaps"],
-                    exclude: ["MapboxMapsTests/Info.plist",
-                              "MapboxMapsStyleTests/Info.plist",
-                              "MapboxMapsGesturesTests/Info.plist",
-                              "MapboxMapsAnnotationsTests/Info.plist",
-                              "MapboxMapsLocationTests/Info.plist",
-                              "MapboxMapsFoundationTests/Info.plist",
-                              "MapboxMapsOfflineTests/Info.plist",
-                              "MapboxMapsOrnamentsTests/Info.plist",
-                              "MapboxMapsTests/Info.plist",
-                              "MapboxMapsSnapshotTests/Info.plist"
-                    ],
-                    resources: [
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/point.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/multipoint.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/multiline.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/geometry-collection.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/featurecollection.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/featurecollection-no-properties.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/simple-line.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/polygon.geojson"),
-                        .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/multipolygon.geojson"),
-                        .copy("TestHelpers/MapboxAccessToken")
-                        ]
-                    )
-     ]
- )
+        ),
+        .testTarget(
+            name: "MapboxMapsTests",
+            dependencies: ["MapboxMaps"],
+            exclude: [
+                "MapboxMapsTests/Info.plist",
+                "MapboxMapsStyleTests/Info.plist",
+                "MapboxMapsGesturesTests/Info.plist",
+                "MapboxMapsAnnotationsTests/Info.plist",
+                "MapboxMapsLocationTests/Info.plist",
+                "MapboxMapsFoundationTests/Info.plist",
+                "MapboxMapsOfflineTests/Info.plist",
+                "MapboxMapsOrnamentsTests/Info.plist",
+                "MapboxMapsTests/Info.plist",
+                "MapboxMapsSnapshotTests/Info.plist",
+            ],
+            resources: [
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/point.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/multipoint.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/multiline.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/geometry-collection.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/featurecollection.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/featurecollection-no-properties.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/simple-line.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/polygon.geojson"),
+                .copy("MapboxMapsFoundationTests/GeoJSON/Fixtures/multipolygon.geojson"),
+                .copy("TestHelpers/MapboxAccessToken"),
+            ]
+        )
+    ]
+)

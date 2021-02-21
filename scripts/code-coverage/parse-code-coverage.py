d75e0bdcacd5d006b2fa5282e6c5096091544443
Julian Rex <julian.rex@gmail.com>
2021-02-21 14:58:23 -0500

    Use older coverage format

--- scripts/code-coverage/parse-code-coverage.py
+++ scripts/code-coverage/parse-code-coverage.py
@@ -10,6 +10,9 @@ import json
 import gzip
 import datetime
 
+scripts_dir = os.path.dirname(os.path.realpath(__file__))
+S3_DIRECTORY = "mobile_staging.codecoverage_v2"
+
 def parseReport(reportPath):
     with open(reportPath) as f:
         results = json.load(f)
@@ -27,12 +30,20 @@ def publish_coverage_report(report, fileName):
     with gzip.open(fileName, mode="wt") as f:
         f.write(json.dumps(report))
 
-    print("TODO: publish " + fileName + " to S3")
+    publish_script = os.path.join(scripts_dir, 'publish_to_aws.sh')
+    try:
+        publish_results = subprocess.check_output(['sh', publish_script, S3_DIRECTORY, fileName], stderr=subprocess.STDOUT)
+        print(publish_results)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+    finally:
+        # remove the temporary zip file
+        os.remove(fileName)
 
-    # remove the temporary zip file
-    os.remove(fileName)
 
 if __name__ == "__main__":
+    # Example:
+    # ./scripts/code-coverage/parse-code-coverage.py --report data.profraw.json --scheme MapboxMapsTestsWithHost -c MapboxMaps -g . -d
     parser = argparse.ArgumentParser(description='Script to parse the lcov JSON coverage report.')
     parser.add_argument('--report', help= 'Provide path the lcov JSON report', required=True)
     parser.add_argument('--scheme', help= 'Xcode scheme', required=True)
@@ -80,20 +91,38 @@ if __name__ == "__main__":
     message = repo.head.object.message
 
     coverage_info = {}
-    coverage_info["version"] = "1"
-    coverage_info["scheme"]  = scheme
-    coverage_info["build"]   = buildNumber
-    coverage_info["totals"]  = parseReport(reportPath)
+    coverage_info["version"]    = "1"
+    coverage_info["component"]  = component
+    coverage_info["scheme"]     = scheme
+    coverage_info["build"]      = buildNumber
+    coverage_info["totals"]     = parseReport(reportPath)
+
+    build_info = {}
+    build_info["build"]     = buildNumber
+    build_info["project"]   = project
+    build_info["branch"]    = branch
+    build_info["sha"]       = sha
+    build_info["author"]    = repo.head.object.author.name
+    build_info["timestamp"] = repo.head.object.committed_date
+    build_info["message"]   = message
 
     report = {}
-    report["version"]        = "1"
-    report["created_at"]     = datetime.datetime.utcnow().isoformat()
-    report["project"]        = project
-    report["branch"]         = branch
-    report["component"]      = component
-    report["commit_sha"]     = sha
-    report["commit_message"] = message
-    report["coverage"]       = coverage_info
+    report["name"]      = project
+    report["version"]   = "2"
+    report["created"]   = datetime.datetime.utcnow().isoformat()    
+    report["build"]     = build_info
+    report["result"]    = coverage_info
+
+    # Pending format:
+    # report = {}
+    # report["version"]        = "1"
+    # report["created_at"]     = datetime.datetime.utcnow().isoformat()
+    # report["project"]        = project
+    # report["branch"]         = branch
+    # report["component"]      = component
+    # report["commit_sha"]     = sha
+    # report["commit_message"] = message
+    # report["coverage"]       = coverage_info
 
     # Print the stats of current file
     print(json.dumps(report, indent=2))

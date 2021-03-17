5af85116eda1697dc98b4f56bbbae5e6a7fe03e7
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-03-16 19:26:57 -0500

    Make Examples tests more robust
    - Remove navigation animations
    - Fix potential race condition with finish notifications
    
    [run app device tests]

---
+++ Apps/Examples/Examples/Models/ExampleProtocol.swift
@@ -0,0 +1,28 @@
+import Foundation
+import MapboxMaps
+
+public protocol ExampleProtocol: AnyObject {
+    func resourceOptions() -> ResourceOptions
+    func finish()
+}
+
+extension ExampleProtocol {
+    public func resourceOptions() -> ResourceOptions {
+        guard let accessToken = AccountManager.shared.accessToken else {
+            fatalError("Access token not set")
+        }
+
+        guard !accessToken.isEmpty else {
+            fatalError("Empty access token")
+        }
+
+        let resourceOptions = ResourceOptions(accessToken: accessToken)
+        return resourceOptions
+    }
+
+    public func finish() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            NotificationCenter.default.post(name: Example.finishNotificationName, object: self)
+        }
+    }
+}

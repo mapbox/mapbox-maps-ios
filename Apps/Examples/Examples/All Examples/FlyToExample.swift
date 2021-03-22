4ea022b1fb24f99cf086c68e2320b1c4f5013d8f
Neel Mistry <neel.mistry@mapbox.com>
2021-03-22 17:32:49 -0400

    Update FlyTo example to reference new FlyTo function

--- Apps/Examples/Examples/All Examples/FlyToExample.swift
+++ Apps/Examples/Examples/All Examples/FlyToExample.swift
@@ -42,11 +42,8 @@ public class FlyToExample: UIViewController, ExampleProtocol {
                                 pitch: 50)
 
         mapView.cameraManager.setCamera(to: start) { _ in
-            self.mapView.cameraManager.fly(to: end) { [weak self] _ in
-                print("Camera fly-to finished")
-                // The below line is used for internal testing purposes only.
-                self?.finish()
-            }
+            let animator = self.mapView.cameraManager.flyTo(to: end)
+            animator?.startAnimation()
         }
     }
 }

d7d4e35fa306d02dda37d5e1b5779a581bb41845
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-03-24 14:02:44 -0500

    Make LocationManager.locationOptions getter public (#209)

--- Tests/MapboxMapsTests/Location/LocationManagerTests.swift
+++ Tests/MapboxMapsTests/Location/LocationManagerTests.swift
@@ -9,68 +9,83 @@ import MapboxMapsFoundation
 
 internal class LocationManagerTests: XCTestCase {
 
-    var locationSupportableMapMock: LocationSupportableMapViewMock!
-    var locationProviderOptionsMock: LocationOptions!
-    var locationConsumerMock: LocationConsumerMock!
+    var locationSupportableMapView: LocationSupportableMapViewMock!
 
     override func setUp() {
-        locationSupportableMapMock = LocationSupportableMapViewMock()
-        locationProviderOptionsMock = LocationOptions()
-        locationConsumerMock = LocationConsumerMock()
+        locationSupportableMapView = LocationSupportableMapViewMock()
         super.setUp()
     }
 
     override func tearDown() {
-        locationSupportableMapMock = nil
-        locationProviderOptionsMock = nil
-        locationConsumerMock = nil
+        locationSupportableMapView = nil
         super.tearDown()
     }
 
     func testLocationManagerDefaultInitialization() {
-        let locationManager = LocationManager(locationOptions: locationProviderOptionsMock,
-                                              locationSupportableMapView: locationSupportableMapMock)
-        locationManager.addLocationConsumer(newConsumer: locationConsumerMock)
+        let locationOptions = LocationOptions()
 
-        XCTAssertNotNil(locationManager.consumers)
-        XCTAssertNotNil(locationManager.locationSupportableMapView)
+        let locationManager = LocationManager(
+            locationOptions: locationOptions,
+            locationSupportableMapView: locationSupportableMapView)
+
+        XCTAssertEqual(locationManager.locationOptions, locationOptions)
+        XCTAssertTrue(locationManager.locationSupportableMapView === locationSupportableMapView)
         XCTAssertNil(locationManager.delegate)
     }
 
-    func testLocationManagerPuckTypeModified() {
+    func testAddLocationConsumer() {
+        let locationManager = LocationManager(
+            locationOptions: LocationOptions(),
+            locationSupportableMapView: locationSupportableMapView)
+        let locationConsumer = LocationConsumerMock()
+
+        locationManager.addLocationConsumer(newConsumer: locationConsumer)
+
+        XCTAssertTrue(locationManager.consumers.contains(locationConsumer))
+    }
+
+    func testUpdateLocationOptionsWithModifiedPuckType() {
         var locationOptions = LocationOptions()
         locationOptions.puckType = .puck2D(Puck2DConfiguration(scale: .constant(1.0)))
-        let locationManager = LocationManager(locationOptions: locationOptions,
-                                              locationSupportableMapView: locationSupportableMapMock)
+        let locationManager = LocationManager(
+            locationOptions: locationOptions,
+            locationSupportableMapView: locationSupportableMapView)
 
         var locationOptions2 = LocationOptions()
         locationOptions2.puckType = .puck2D(Puck2DConfiguration(scale: .constant(2.0)))
         locationManager.updateLocationOptions(with: locationOptions2)
+
+        XCTAssertEqual(locationManager.locationOptions, locationOptions2)
         XCTAssertEqual(locationManager.locationPuckManager?.puckType, locationOptions2.puckType)
     }
 
-    func testLocationManagerPuckTypeSetToNil() {
+    func testUpdateLocationOptionsWithPuckTypeSetToNil() {
         var locationOptions = LocationOptions()
         locationOptions.puckType = .puck2D()
-        let locationManager = LocationManager(locationOptions: locationOptions,
-                                              locationSupportableMapView: locationSupportableMapMock)
+        let locationManager = LocationManager(
+            locationOptions: locationOptions,
+            locationSupportableMapView: locationSupportableMapView)
 
         var locationOptions2 = LocationOptions()
         locationOptions2.puckType = nil
         locationManager.updateLocationOptions(with: locationOptions2)
+
+        XCTAssertEqual(locationManager.locationOptions, locationOptions2)
         XCTAssertNil(locationManager.locationPuckManager)
     }
 
-    func testLocationManagerPuckTypeSetToNonNil() {
+    func testUpdateLocationOptionsWithPuckTypeSetToNonNil() {
         var locationOptions = LocationOptions()
         locationOptions.puckType = nil
-        let locationManager = LocationManager(locationOptions: locationOptions,
-                                              locationSupportableMapView: locationSupportableMapMock)
+        let locationManager = LocationManager(
+            locationOptions: locationOptions,
+            locationSupportableMapView: locationSupportableMapView)
 
         var locationOptions2 = LocationOptions()
         locationOptions2.puckType = .puck2D()
         locationManager.updateLocationOptions(with: locationOptions2)
-        XCTAssertNotNil(locationManager.locationPuckManager)
+
+        XCTAssertEqual(locationManager.locationOptions, locationOptions2)
         XCTAssertEqual(locationManager.locationPuckManager?.puckType, locationOptions2.puckType)
     }
 }

4fc3b494a827e4622d7b82872ca622979b912139
Julian Rex <julian.rex@mapbox.com>
2021-03-29 16:17:00 -0400

    Add test to check values are within range. (#226)

--- Tests/MapboxMapsTests/Foundation/Camera/FlyToTests.swift
+++ Tests/MapboxMapsTests/Foundation/Camera/FlyToTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Turf
 
 #if canImport(MapboxMaps)
 @testable import MapboxMaps
@@ -49,14 +50,77 @@ internal class FlyToTests: XCTestCase {
         let d1 = flyTo.coordinate(at: 0.5)
         let d2 = flyTo.coordinate(at: 1.0)
 
-        XCTAssertEqual(d0.latitude, s0.latitude, accuracy: 0.00001)
-        XCTAssertEqual(d0.longitude, s0.longitude, accuracy: 0.00001)
+        let epsilon = 0.0001
+        XCTAssertEqual(d0.latitude, s0.latitude, accuracy: epsilon)
+        XCTAssertEqual(d0.longitude, s0.longitude, accuracy: epsilon)
 
-        XCTAssertEqual(d1.latitude, (s0.latitude+s2.latitude)/2, accuracy: 0.00001)
-        XCTAssertEqual(d1.longitude, (s0.longitude+s2.longitude)/2, accuracy: 0.00001)
+        XCTAssertEqual(d1.latitude, (s0.latitude+s2.latitude)/2, accuracy: epsilon)
+        XCTAssertEqual(d1.longitude, (s0.longitude+s2.longitude)/2, accuracy: epsilon)
 
-        XCTAssertEqual(d2.latitude, s2.latitude, accuracy: 0.00001)
-        XCTAssertEqual(d2.longitude, s2.longitude, accuracy: 0.00001)
+        XCTAssertEqual(d2.latitude, s2.latitude, accuracy: epsilon)
+        XCTAssertEqual(d2.longitude, s2.longitude, accuracy: epsilon)
     }
     // swiftlint:enable identifier_name
+
+    func testForValidValues() {
+
+        let epsilon: CLLocationDegrees = 0.0001
+
+        for _ in 0..<10 {
+
+            // Longitude increases easterly for this test.
+            let sourceCoord = CLLocationCoordinate2D(
+                latitude: CLLocationDegrees.random(in: -85..<85),
+                longitude: CLLocationDegrees.random(in: 0..<180)
+            )
+
+            let destCoord = CLLocationCoordinate2D(
+                latitude: CLLocationDegrees.random(in: -85..<85),
+                longitude: CLLocationDegrees.random(in: 180..<360)
+            )
+
+            let source = CameraOptions(center: sourceCoord,
+                                       padding: .zero,
+                                       zoom: 14,
+                                       bearing: 0,
+                                       pitch: 0)
+            let dest = CameraOptions(center: destCoord,
+                                     zoom: 18,
+                                     bearing: 90,
+                                     pitch: 45)
+
+            guard let flyTo = FlyToInterpolator(from: source, to: dest, size: CGSize(width: 500.0, height: 500.0)),
+                  var boundingBox = BoundingBox(from: [sourceCoord, destCoord]) else {
+                XCTFail("Failed to create interpolator")
+                continue
+            }
+
+            boundingBox.southWest.latitude -= epsilon
+            boundingBox.southWest.longitude -= epsilon
+            boundingBox.northEast.latitude += epsilon
+            boundingBox.northEast.longitude += epsilon
+
+            let duration = flyTo.duration()
+            XCTAssert(duration > 0)
+
+            for t: Double in stride(from: 0, to: 1, by: 0.01) {
+
+                let coordinate = flyTo.coordinate(at: t)
+
+                XCTAssert(boundingBox.contains(coordinate, ignoreBoundary: false), "t=\(t) coordinate=\(coordinate) boundingBox=\(boundingBox)")
+
+                // Zoom doesn't go below start or end
+                let zoom = CGFloat(flyTo.zoom(at: t))
+                XCTAssert(zoom <= max(source.zoom!, dest.zoom!), "t=\(t) zoom=\(zoom)")
+
+                let bearing = CLLocationDirection(flyTo.bearing(at: t))
+                XCTAssert(bearing >= source.bearing!, "t=\(t) bearing=\(bearing)")
+                XCTAssert(bearing <= dest.bearing!, "t=\(t) bearing=\(bearing)")
+
+                let pitch = CGFloat(flyTo.pitch(at: t))
+                XCTAssert(pitch >= source.pitch!, "t=\(t) pitch=\(pitch)")
+                XCTAssert(pitch <= dest.pitch!, "t=\(t) pitch=\(pitch)")
+            }
+        }
+    }
 }

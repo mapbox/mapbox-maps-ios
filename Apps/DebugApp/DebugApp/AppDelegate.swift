53f0c23bbe008c76fc3dfacd8116d2a197d16851
Julian Rex <julian.rex@gmail.com>
2021-04-09 14:07:54 -0400

    Removed temp code.

--- Apps/DebugApp/DebugApp/AppDelegate.swift
+++ Apps/DebugApp/DebugApp/AppDelegate.swift
@@ -9,7 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
-//        CredentialsManager.default.accessToken = nil
     }
 
     func applicationWillResignActive(_ application: UIApplication) {

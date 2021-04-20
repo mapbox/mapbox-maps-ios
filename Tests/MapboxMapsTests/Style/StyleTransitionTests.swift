185fd8c8ae1efb848afe40f635ee4148c6072292
Neel Mistry <neel.mistry@mapbox.com>
2021-04-20 08:28:56 -0400

    pr comments on assertions

--- Tests/MapboxMapsTests/Style/StyleTransitionTests.swift
+++ Tests/MapboxMapsTests/Style/StyleTransitionTests.swift
@@ -8,11 +8,8 @@ class StyleTransitionTests: XCTestCase {
     func testDecodeHasCorrectConversion() {
         let jsonData = jsonString.data(using: .utf8)!
         let decodedTransition = try! JSONDecoder().decode(StyleTransition.self, from: jsonData)
-        let comparingTransition = StyleTransition(duration: 1.0, delay: 0.5)
 
-        XCTAssertEqual(comparingTransition.duration, 1.0, "Duration should be equal to 1.0")
         XCTAssertEqual(decodedTransition.duration, 1.0, "Duration should be equal to 1.0")
-        XCTAssertEqual(comparingTransition.delay, 0.5, "Delay should be equal to 0.5")
         XCTAssertEqual(decodedTransition.delay, 0.5, "Delay should be equal to 0.5")
     }
 

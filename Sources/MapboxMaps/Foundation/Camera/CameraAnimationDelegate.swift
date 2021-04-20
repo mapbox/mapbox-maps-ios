f0a87440f7acd6a74303cd804da18a05dddbdb83
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-04-20 17:48:08 -0400

    add docs

--- Sources/MapboxMaps/Foundation/Camera/CameraAnimationDelegate.swift
+++ Sources/MapboxMaps/Foundation/Camera/CameraAnimationDelegate.swift
@@ -5,22 +5,24 @@ public typealias AnimationCompletion = (UIViewAnimatingPosition) -> Void
 
 // MARK: CameraAnimatorDelegate Protocol
 internal protocol CameraAnimatorDelegate: class {
-
+    
+    /// The current camera of the map
     var camera: CameraOptions { get }
 
+    /// Ask the map to set camera to new camera
     func jumpTo(camera: CameraOptions)
-
+    
+    /// Adds the view to the MapView's subviews
     func addViewToViewHeirarchy(_ view: CameraView)
 
+    /// Calculates the anchor after taking padding into consideration
     func anchorAfterPadding() -> CGPoint
 
-    /**
-    This delegate function notifies that the completion block needs to be scheduled
-
-    - Parameter animator: The current animator that this delegate function is being called from
-    - Parameter completion: The completion block that needs to be scheduled
-    - Parameter animatingPosition  The position of the animation needed for the closure
-    */
+    /// This delegate function notifies that the completion block needs to be scheduled on the next tick of the displaylink
+    /// - Parameters:
+    ///   - animator: The current animator that this delegate function is being called from
+    ///   - completion: The completion block that needs to be scheduled
+    ///   - animatingPosition: The position of the animation needed for the closure
     func schedulePendingCompletion(forAnimator animator: CameraAnimatorProtocol,
                                    completion: @escaping AnimationCompletion,
                                    animatingPosition: UIViewAnimatingPosition)

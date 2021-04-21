21ab036b8926e86dd03930db8646f07064308673
ZiZi <zmiles@gwu.edu>
2021-04-21 13:57:46 -0400

    testing geojson strings

--- Apps/DebugApp/DebugApp/DebugViewController.swift
+++ Apps/DebugApp/DebugApp/DebugViewController.swift
@@ -9,85 +9,119 @@ import Turf
  */
 
 public class DebugViewController: UIViewController {
-
+    
     internal var mapView: MapView!
-    internal var runningAnimator: CameraAnimator?
-
+    let identifier = "geoJSON-data-source"
+    var geoJSONSource = GeoJSONSource()
+    var newFeatures = String()
+    var originalFeatures = String()
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
-
+        let resourceOptions = ResourceOptions(accessToken: "access_token")
         mapView = MapView(frame: view.bounds)
-        mapView.update { (mapOptions) in
-            mapOptions.location.puckType = .puck2D()
-        }
-
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(mapView)
-
-        /**
-         The closure is called when style data has been loaded. This is called
-         multiple times. Use the event data to determine what kind of style data
-         has been loaded.
-         
-         When the type is `style` this event most closely matches
-         `-[MGLMapViewDelegate mapView:didFinishLoadingStyle:]` in SDK versions
-         prior to v10.
-         */
-        mapView.on(.styleDataLoaded) { (event) in
-            guard let data = event.data as? [String: Any],
-                  let type = data["type"] else {
-                return
+        
+        // Set the center coordinate and zoom level.
+        let centerCoordinate = CLLocationCoordinate2DMake(35.42486791930558, 136.95556640625)
+        mapView.centerCoordinate = centerCoordinate
+        mapView.zoom = 3
+        
+        // Allow the view controller to receive information about map events.
+        mapView.on(.mapLoaded) { [weak self] _ in
+            guard let self = self else { return }
+            
+            self.originalFeatures = """
+                    {
+                      "type": "Feature",
+                      "properties": {},
+                      "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                          [
+                            136.95556640625,
+                            35.42486791930558
+                          ]
+                        ]
+                      }
+                    }
+                """
+            
+            self.newFeatures = """
+                    {
+                      "type": "Feature",
+                      "properties": {},
+                      "geometry": {
+                        "type": "LineString",
+                        "coordinates": [
+                          [
+                            136.95556640625,
+                            35.42486791930558
+                          ],
+                          [
+                            137.603759765625,
+                            36.146746777814364
+                          ],
+                          [
+                            139.273681640625,
+                            36.01356058518153
+                          ],
+                          [
+                            139.075927734375,
+                            37.39634613318923
+                          ],
+                          [
+                            140.48217773437497,
+                            37.64903402157866
+                          ]
+                        ]
+                      }
+                    }
+                """
+            
+            self.addGeoJSONSource(identifier: "source", features: self.originalFeatures as NSString)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.updateGeoJSONSource(identifier: "my-source", features: self.newFeatures as NSString)
             }
-
-            print("The map has finished loading style data of type = \(type)")
-        }
-
-        /**
-         The closure is called during the initialization of the map view and
-         after any `styleDataLoaded` events; it is called when the requested
-         style has been fully loaded, including the style, specified sprite and
-         source metadata.
-
-         This event is the last opportunity to modify the layout or appearance
-         of the current style before the map view is displayed to the user.
-         Adding a layer at this time to the map may result in the layer being
-         presented BEFORE the rest of the map has finished rendering.
-
-         Changes to sources or layers of the current style do not cause this
-         event to be emitted.
-         */
-        mapView.on(.styleLoaded) { (event) in
-            print("The map has finished loading style ... Event = \(event)")
-        }
-
-        /**
-         The closure is called whenever the map finishes loading and the map has
-         rendered all visible tiles, either after the initial load OR after a
-         style change has forced a reload.
-
-         This is an ideal time to add any runtime styling or annotations to the
-         map and ensures that these layers would only be shown after the map has
-         been fully rendered.
-         */
-        mapView.on(.mapLoaded) { (event) in
-            print("The map has finished loading... Event = \(event)")
-        }
-
-        /**
-         The closure is called whenever the map has failed to load. This could
-         be because of a variety of reasons, including a network connection
-         failure or a failure to fetch the style from the server.
-
-         You can use the associated error message to notify the user that map
-         data is unavailable.
-         */
-        mapView.on(.mapLoadingError) { (event) in
-            guard let data = event.data as? [String: Any],
-                  let type = data["type"],
-                  let message = data["message"] else {
-                return
-            }
-
-            print("The map failed to load.. \(type) = \(message)")
+            
         }
     }
+    
+    public func addGeoJSONSource(identifier: String, features: NSString) -> String {
+        mapView.style.addSource(source:  geoJSONSource, identifier: identifier)
+        let originalFeatureDict = convertStringToDictionary(text: originalFeatures as String)
+        _ = try? mapView.__map.setStyleSourcePropertyForSourceId(identifier, property: "data", value: features)
+        print("here are our features: \(features)")
+        //        var layer = try? CircleLayer(jsonObject: originalFeatureDict!)
+        //        layer?.sourceLayer = identifier
+        //        layer?.paint?.circleColor = .constant(ColorRepresentable(color: UIColor.lightGray))
+        //        _ = mapView.style.addLayer(layer: layer?)
+        
+        return identifier
+    }
+    
+    public func updateGeoJSONSource(identifier: String, features: NSString) {
+        let newFeatureDict = convertStringToDictionary(text: newFeatures as String)
+        _ = try? mapView.__map.setStyleSourcePropertyForSourceId(identifier, property: "data", value: features)
+        print("here are our features: \(features)")
+        //        var layer = try? LineLayer(jsonObject: newFeatureDict!)
+        //        layer?.sourceLayer = identifier
+        //        layer?.paint?.lineColor = .constant(ColorRepresentable(color: UIColor.lightGray))
+        //        _ = mapView.style.addLayer(layer: layer as! Layer)
+    }
+    
+    func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:AnyObject]
+                return json
+            } catch {
+                print("Something went wrong")
+            }
+        }
+        return nil
+    }
+    
 }

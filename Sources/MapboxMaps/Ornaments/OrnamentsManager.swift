f8c115b4fb11831db38234bb89fa1dffe890a186
Jordan Kiley <jordan.kiley@mapbox.com>
2021-04-21 10:23:45 -0400

    Port position and margins code

--- Sources/MapboxMaps/Ornaments/OrnamentsManager.swift
+++ Sources/MapboxMaps/Ornaments/OrnamentsManager.swift
@@ -31,7 +31,7 @@ internal class OrnamentsManager: NSObject {
     private let logoView: LogoView
     private let scalebarView: MapboxScaleBarOrnamentView
     private let compassView: MapboxCompassOrnamentView
-    private let infoButton: MapboxInfoButtonOrnament
+    private let attributionButton: MapboxInfoButtonOrnament
 
     private var constraints = [NSLayoutConstraint]()
 
@@ -57,9 +57,9 @@ internal class OrnamentsManager: NSObject {
         view.addSubview(compassView)
 
         // Info Button
-        infoButton = MapboxInfoButtonOrnament()
-        infoButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(infoButton)
+        attributionButton = MapboxInfoButtonOrnament()
+        attributionButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(attributionButton)
 
         super.init()
 
@@ -80,72 +80,101 @@ internal class OrnamentsManager: NSObject {
     }
 
     private func updateOrnaments() {
-        // 1. Move ornaments to correct locations (use margins & position from self.options)
-        // remove previously-added constraints
-        NSLayoutConstraint.deactivate(constraints)
-        constraints.removeAll()
+        // Remove previously-added constraints
+        if constraints.count > 0 {
+            NSLayoutConstraint.deactivate(constraints)
+            constraints.removeAll()
+        }
 
         // Update the position for the ornaments
-        infoButton.
-        // TODO: re-create new constraints for each view (and add them to the `constraints` array)
+        let logoViewConstraints = constraints(with: logoView, position: options.logoViewPosition, margins: options.logoViewMargins)
+        constraints.append(contentsOf: logoViewConstraints)
+        let compassViewConstraints = constraints(with: compassView, position: options.compassViewPosition, margins: options.compassViewMargins)
+        constraints.append(contentsOf: compassViewConstraints)
+        let scaleBarViewConstraints = constraints(with: scalebarView, position: options.scaleBarPosition, margins: options.scaleBarMargins)
+        constraints.append(contentsOf: scaleBarViewConstraints)
+        let attributionButtonConstraints = constraints(with: attributionButton, position: options.attributionButtonPosition, margins: options.attributionButtonMargins)
+        constraints.append(contentsOf: attributionButtonConstraints)
 
         // Activate new constraints
         NSLayoutConstraint.activate(constraints)
 
-        // 2. Sync visiblity using .isHidden on each ornament view; For ornaments that support `adaptive` visibility, adaptive should map to isHidden = false. The ornament view itself should use `alpha` to when it wants to hide based on some other input. See the change handler above.
-
         logoView.isHidden = !options._logoViewIsVisible
         scalebarView.isHidden = options.scaleBarVisibility == .hidden
         compassView.isHidden = options.compassVisibility == .hidden
-        infoButton.isHidden = !options._attributionButtonIsVisible
+        attributionButton.isHidden = !options._attributionButtonIsVisible
     }
 
-    func constraints(with view: UIView, position: OrnamentPosition, margin: CGPoint) -> [NSLayoutConstraint {
+    func constraints(with view: UIView, position: OrnamentPosition, margins: CGPoint) -> [NSLayoutConstraint] {
+        guard let universalLayoutGuide = layoutGuide(for: view) else { return [] }
         switch position {
         case .topLeft:
-            return [view.leadingAnchor.constraint(equalTo: universalLayoutGuide.leadingAnchor,
-                                          constant: margin.x),
-                    
+            return [
+                view.leadingAnchor.constraint(equalTo: universalLayoutGuide.leadingAnchor,
+                                          constant: margins.x),
+                view.topAnchor.constraint(equalTo: universalLayoutGuide.topAnchor,
+                                              constant: margins.y)
+                ]
 
         case .topCenter:
-
+            print("topCenter")
         case .topRight:
-
+            return  [
+                view.trailingAnchor.constraint(equalTo: universalLayoutGuide.trailingAnchor,
+                                                       constant: -margins.x),
+                view.topAnchor.constraint(equalTo: universalLayoutGuide.topAnchor,
+                                                  constant: margins.y)
+            ]
         case .centerLeft:
-
+            print("centerLeft")
         case .centerRight:
-
+            print("centerRight")
         case .bottomLeft:
-
+            return [
+                view.leadingAnchor.constraint(equalTo: universalLayoutGuide.leadingAnchor,
+                                                      constant: margins.x),
+                view.bottomAnchor.constraint(equalTo: universalLayoutGuide.bottomAnchor,
+                                                     constant: -margins.y)
+            ]
         case .bottomCenter:
-
+            print("bottomCenter")
         case .bottomRight:
+            return [
+                view.trailingAnchor.constraint(equalTo: universalLayoutGuide.trailingAnchor,
+                                                       constant: -margins.x),
+                view.bottomAnchor.constraint(equalTo: universalLayoutGuide.bottomAnchor,
+                                                     constant: -margins.y)
+            ]
         }
+        return [NSLayoutConstraint()]
     }
 
-    internal var universalLayoutGuide: UILayoutGuide {
-            if #available(iOS 11.0, *) {
-                return self.view.safeAreaLayoutGuide
+    func layoutGuide(for view: UIView) -> UILayoutGuide? {
+        guard let superview = view.superview else {
+            return nil
+        }
+        if #available(iOS 11.0, *) {
+            return superview.safeAreaLayoutGuide
+        } else {
+            let layoutGuideIdentifier = "mapboxSafeAreaLayoutGuide"
+            // If there's already a generated layout guide, return it
+            if let layoutGuide = superview.layoutGuides.filter({ $0.identifier == layoutGuideIdentifier }).first {
+                return layoutGuide
             } else {
-                let layoutGuideIdentifier = "mapboxSafeAreaLayoutGuide"
-                // If there's already a generated layout guide, return it
-                if let layoutGuide = view.layoutGuides.filter({ $0.identifier == layoutGuideIdentifier }).first {
-                    return layoutGuide
-                } else {
-                    // If not, then make a new one based off the view's edges.
-                    let layoutGuide = UILayoutGuide()
-                    layoutGuide.identifier = layoutGuideIdentifier
-                    view.addLayoutGuide(layoutGuide)
+                // If not, then make a new one based off the view's edges.
+                let layoutGuide = UILayoutGuide()
+                layoutGuide.identifier = layoutGuideIdentifier
+                view.addLayoutGuide(layoutGuide)
 
-                    NSLayoutConstraint.activate([
-                        layoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                        layoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                        layoutGuide.topAnchor.constraint(equalTo: view.topAnchor),
-                        layoutGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-                    ])
+                NSLayoutConstraint.activate([
+                    layoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    layoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    layoutGuide.topAnchor.constraint(equalTo: view.topAnchor),
+                    layoutGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+                ])
 
-                    return layoutGuide
-                }
+                return layoutGuide
             }
         }
+    }
 }

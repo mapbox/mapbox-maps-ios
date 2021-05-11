90db28c2c93424cd293b4b66563c6c40d51adaa2
Julian Rex <julian.rex@gmail.com>
2021-05-11 15:04:55 -0400

    2 more tests

--- Tests/MapboxMapsTests/Foundation/MapEventHandlerTests.swift
+++ Tests/MapboxMapsTests/Foundation/MapEventHandlerTests.swift
@@ -76,4 +76,87 @@ final class MapEventHandlerTests: XCTestCase {
         cancelable?.cancel()
         XCTAssertNil(cancelable)
     }
+
+    func testRetainCycle() {
+
+        class StrongHandlerContainer {
+            var handler: MapEventHandler?
+        }
+
+        weak var cancelable: Cancelable?
+        weak var weakContainer: StrongHandlerContainer?
+
+        do {
+            let observable = MockObservable()
+            var called = 0
+
+            let container = StrongHandlerContainer()
+
+
+            container.handler = MapEventHandler(for: ["event"],
+                                                observable: observable) { _ in
+                called += 1
+                // Force a retain cycle
+                dump(container)
+                return false
+            }
+
+            weakContainer = container
+            cancelable = container.handler
+
+            container.handler?.notify(for: Event(type: "event", data: "ignore-me"))
+            container.handler?.notify(for: Event(type: "event", data: "ignore-me"))
+
+            // Handler should only be called for first event
+            XCTAssert(called == 2)
+        }
+        XCTAssertNotNil(cancelable)
+        XCTAssertNotNil(weakContainer)
+
+        // Cancel breaks the retain cycle
+        cancelable?.cancel()
+
+        XCTAssertNil(cancelable)
+        XCTAssertNil(weakContainer)
+    }
+
+    func testWeakContainer() {
+
+        class WeakHandlerContainer {
+            weak var handler: MapEventHandler?
+
+            deinit {
+                handler?.cancel()
+            }
+        }
+
+        weak var cancelable: Cancelable?
+        weak var weakContainer: WeakHandlerContainer?
+
+        do {
+            let observable = MockObservable()
+            var called = 0
+
+            let container = WeakHandlerContainer()
+
+            let handler = MapEventHandler(for: ["event"],
+                                                observable: observable) { _ in
+                called += 1
+                return false
+            }
+
+            container.handler = handler
+            cancelable = handler
+
+            weakContainer = container
+
+            container.handler?.notify(for: Event(type: "event", data: "ignore-me"))
+            container.handler?.notify(for: Event(type: "event", data: "ignore-me"))
+
+            // Handler should only be called for first event
+            XCTAssert(called == 2)
+        }
+        XCTAssertNil(weakContainer)
+        XCTAssertNil(cancelable)
+    }
 }

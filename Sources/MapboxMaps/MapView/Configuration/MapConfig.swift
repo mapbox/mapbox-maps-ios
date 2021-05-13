e2982d1b2e746d93ece08faa05533ab54a713e32
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-05-13 11:22:01 -0400

    Make MapView hold render-related options

--- Sources/MapboxMaps/MapView/Configuration/MapConfig.swift
+++ Sources/MapboxMaps/MapView/Configuration/MapConfig.swift
@@ -3,8 +3,6 @@ import Foundation
 /// `MapConfig` is the structure used to configure the map with a set of capabilities
 public struct MapConfig: Equatable {
 
-    public var render: RenderOptions = RenderOptions()
-
     public var annotations: AnnotationOptions = AnnotationOptions()
 
     public init() {}

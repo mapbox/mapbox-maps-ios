4171c48eba3bcc1c74a8c6e67596db82c01e8340
Jordan Kiley <jordan.kiley@mapbox.com>
2021-05-18 16:23:15 -0400

    initial commit

---
+++ Apps/Examples/Examples/All Examples/SwitchStylesExample.swift
@@ -0,0 +1,25 @@
+import UIKit
+import MapboxMaps
+
+@objc(SwitchStylesExample)
+
+public class SwitchStylesExample: UIViewController, ExampleProtocol {
+
+    internal var mapView: MapView!
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapView = MapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.ornaments.options.scaleBar.visibility = .visible
+
+        view.addSubview(mapView)
+    }
+
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+         // The below line is used for internal testing purposes only.
+        finish()
+    }
+}

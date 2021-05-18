ebe0127ea82c1d50b51d35884e8aed5c49e4be86
Jordan Kiley <jordan.kiley@mapbox.com>
2021-05-18 16:38:34 -0400

    Add custom style url

--- Apps/Examples/Examples/All Examples/SwitchStylesExample.swift
+++ Apps/Examples/Examples/All Examples/SwitchStylesExample.swift
@@ -17,6 +17,15 @@ public class SwitchStylesExample: UIViewController, ExampleProtocol {
         view.addSubview(mapView)
     }
 
+
+    @objc func switchStyle(sender: UISegmentedControl) {
+        // A local style JSON (https://www.mapbox.com/mapbox-gl-style-spec/) from Mapillary: <https://d25uarhxywzl1j.cloudfront.net/v0.1/{z}/{x}/{y}.mvt>
+        let localStyleURL = Bundle.main.url(forResource: "third_party_vector_style", withExtension: "json")!
+        // A custom style design
+//        let customStyleURL = StyleURI(rawValue: "mapbox://styles/examples/cke97f49z5rlg19l310b7uu7j")!
+
+    }
+
     override public func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
          // The below line is used for internal testing purposes only.

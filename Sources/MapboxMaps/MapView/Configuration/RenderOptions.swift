09e0fbde8b531e773d970319050e9c928bb1b4c6
Neel Mistry <neel.mistry@mapbox.com>
2021-05-18 17:13:57 -0400

    changing fps to max

--- Sources/MapboxMaps/MapView/Configuration/RenderOptions.swift
+++ Sources/MapboxMaps/MapView/Configuration/RenderOptions.swift
@@ -11,7 +11,7 @@ public struct RenderOptions: Equatable {
     ///  a smooth experience.
     ///
     ///  See Also `CADisplayLink.preferredFramesPerSecond`
-    public var preferredFramesPerSecond: PreferredFPS = .normal
+    public var preferredFramesPerSecond: PreferredFPS = .maximum
 
     /// When loading a map, if `PrefetchZoomDelta` is set to any number greater than 0, the map
     /// map at lower resolution as quick as possible. It will get clamped at the tile source
@@ -27,7 +27,7 @@ public struct RenderOptions: Equatable {
     ///
     /// If, however, the `MapView` is overlaid with a `UIKit` element which must be pinned to a
     /// particular lat-long, then setting this to `true` will result in better synchronization and less jitter.
-    public var presentsWithTransaction: Bool = true
+    public var presentsWithTransaction: Bool = false
 
     public init() {}
 }

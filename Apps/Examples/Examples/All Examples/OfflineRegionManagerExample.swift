4ab934394bbdb5bc31e1b1081e216a5c82b1ba56
Julian Rex <julian.rex@gmail.com>
2021-05-24 15:47:03 -0400

    Fix compile error post rebase

--- Apps/Examples/Examples/All Examples/OfflineRegionManagerExample.swift
+++ Apps/Examples/Examples/All Examples/OfflineRegionManagerExample.swift
@@ -46,7 +46,7 @@ public class OfflineRegionManagerExample: UIViewController, ExampleProtocol {
     internal func setupExample() {
         let uriString = mapView.mapboxMap.style.uri!.rawValue
         let offlineRegionDef = OfflineRegionGeometryDefinition(styleURL: uriString,
-                                                               geometry: MBXGeometry(coordinate: coord),
+                                                               geometry: Geometry(coordinate: coord),
                                                                minZoom: zoom - 2,
                                                                maxZoom: zoom + 2,
                                                                pixelRatio: Float(UIScreen.main.scale),

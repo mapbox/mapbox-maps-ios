1af286d2cbd66957957d34af974e30fa74ca5c8d
Jordan Kiley <jordan.kiley@mapbox.com>
2021-05-24 11:43:07 -0400

    Use a local style JSON

--- Apps/Examples/Examples/All Examples/SwitchStylesExample.swift
+++ Apps/Examples/Examples/All Examples/SwitchStylesExample.swift
@@ -34,16 +34,14 @@ public class SwitchStylesExample: UIViewController, ExampleProtocol {
             mapView.mapboxMap.style.uri = .satelliteStreets
         case 2:
             // Blueprint, a custom style designed by Amy Lee Walton. https://www.mapbox.com/gallery/#blueprint
-            let studioStyleURL = URL(string: "mapbox://styles/examples/ckox18pjy140u17pdvmpgut4i")!
+            // Load the local style JSON. It conforms to the Mapbox Style Specification <https://docs.mapbox.com/mapbox-gl-js/style-spec/>
+            let localStyleURL = Bundle.main.url(forResource: "blueprint_style", withExtension: "json")!
 
             // Set the map's style URI to a custom style URL.
-            mapView.mapboxMap.style.uri = StyleURI(url: studioStyleURL)
+            mapView.mapboxMap.style.uri = StyleURI(url: localStyleURL)
 
-            // A local style JSON that conforms to the Mapbox Style Specification <https://docs.mapbox.com/mapbox-gl-js/style-spec/>
-            // can be loaded as a `StyleURI`. Include the style JSON in your app's bundle, then create a `StyleURI`
-            // with that URL.
-            // let studioStyleURL = Bundle.main.url(forResource: "blueprint_style", withExtension: "json")!
->>>>>>> 50e94bd6... Use a custom style for the third segment, add comments
+            // A Mapbox Studio style can also be loaded as a `StyleURI`.
+            // let studioStyleURL = URL(string: "mapbox://styles/examples/ckox18pjy140u17pdvmpgut4i")!
         default:
             mapView.mapboxMap.style.uri = .satelliteStreets
         }
@@ -51,7 +49,7 @@ public class SwitchStylesExample: UIViewController, ExampleProtocol {
 
     func addStyleToggle() {
         // Create a UISegmentedControl to toggle between map styles
-        let styleToggle = UISegmentedControl(items: ["Streets", "Satellite Streets", "Studio Style"])
+        let styleToggle = UISegmentedControl(items: ["Streets", "Satellite Streets", "Local Style JSON"])
         styleToggle.translatesAutoresizingMaskIntoConstraints = false
         styleToggle.tintColor = UIColor(red: 0.976, green: 0.843, blue: 0.831, alpha: 1)
         styleToggle.backgroundColor = UIColor(red: 0.973, green: 0.329, blue: 0.294, alpha: 1)

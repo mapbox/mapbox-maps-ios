ceb3cd7d85f4d74f250461abfcbe8d22257d7ceb
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-05-25 01:55:27 -0400

    Introduce AnnotationOrchestrator

--- Sources/MapboxMaps/Annotations/AnnotationOrchestrator.swift
+++ Sources/MapboxMaps/Annotations/AnnotationOrchestrator.swift
@@ -1,11 +1,90 @@
 import Foundation
+import Turf
+
+public protocol AnnotationInteractionDelegate {
+    func didSelectAnnotation(annotation: Annotation_Legacy)
+    func didDeselectAnnotation(annotation: Annotation_Legacy)
+}
+
+public enum AnnotationType {
+    case point
+    case polyline
+    case polygon
+    case circle
+}
+
+public protocol Annotation {
+    
+    /// The unique identifier of the annotation.
+    var id: String { get }
+    
+    /// The feature that is backing this annotation.
+    var feature: Turf.Feature { get }
+    
+    /// The geometry associated with an annotation.
+    var type: AnnotationType { get }
+    
+    /// A Boolean value that indicates whether an annotation is selected, either
+    /// programmatically or via user-interactions.
+    var isSelected: Bool { get set }
+
+    /// Properties associated with the annotation
+    var userInfo: [String: Any]? { get set }
+}
+
 
 public struct AnnotationOrchestrator {
     
-    private let mapView: MapView
+    private weak var view: UIView?
     
-    internal init(mapView: MapView) {
-        
+    private weak var style: Style?
+    
+    private weak var mapFeatureQueryable: MapFeatureQueryable?
+     
+    internal init(view: UIView, mapFeatureQueryable: MapFeatureQueryable, style: Style) {
+        self.view = view
+        self.mapFeatureQueryable = mapFeatureQueryable
+        self.style = style
     }
     
+    public func makePointAnnotationManager(id: String = String(UUID().uuidString.prefix(5)), layerPosition: LayerPosition? = nil) -> PointAnnotationManager {
+        
+        guard let style = style else {
+            fatalError("Style must be present when creating an annotation manager")
+        }
+        
+        return PointAnnotationManager(id: id,
+                                      style: style, layerPosition: layerPosition)
+    }
+    
+    public func makePolygonAnnotationManager(id: String = String(UUID().uuidString.prefix(5)), layerPosition: LayerPosition? = nil) -> PolygonAnnotationManager {
+        
+        guard let style = style else {
+            fatalError("Style must be present when creating an annotation manager")
+        }
+        
+        return PolygonAnnotationManager(id: id,
+                                        style: style, layerPosition: layerPosition)
+    }
+    
+    public func makePolylineAnnotationManager(id: String = String(UUID().uuidString.prefix(5)), layerPosition: LayerPosition? = nil) -> PolylineAnnotationManager {
+        
+        guard let style = style else {
+            fatalError("Style must be present when creating an annotation manager")
+        }
+        
+        return PolylineAnnotationManager(id: id,
+                                         style: style, layerPosition: layerPosition)
+    }
+    
+    public func makeCircleAnnotationManager(id: String = String(UUID().uuidString.prefix(5)), layerPosition: LayerPosition? = nil) -> CircleAnnotationManager {
+        
+        guard let style = style else {
+            fatalError("Style must be present when creating an annotation manager")
+        }
+        
+        return CircleAnnotationManager(id: id,
+                                       style: style, layerPosition: layerPosition)
+    }
 }
+
--- Sources/MapboxMaps/Foundation/MapView.swift
+++ Sources/MapboxMaps/Foundation/MapView.swift
@@ -32,7 +32,7 @@ open class MapView: UIView {
     public internal(set) var location: LocationManager!
 
     /// Controls the addition/removal of annotations to the map.
-    public internal(set) var annotations: AnnotationManager_Legacy!
+    public internal(set) var annotations: AnnotationOrchestrator!
 
     /// A reference to the `EventsManager` used for dispatching telemetry.
     internal var eventsListener: EventsListener!
@@ -179,10 +179,9 @@ open class MapView: UIView {
         location = LocationManager(locationSupportableMapView: self, style: mapboxMap.style)
 
         // Initialize/Configure annotations manager
-        annotations = AnnotationManager_Legacy(for: self,
-                                        mapEventsObservable: mapboxMap,
-                                        mapFeatureQueryable: mapboxMap,
-                                        style: mapboxMap.style)
+        annotations = AnnotationOrchestrator(view: self,
+                                             mapFeatureQueryable: mapboxMap,
+                                             style: mapboxMap.style)
     }
 
     private func checkForMetalSupport() {

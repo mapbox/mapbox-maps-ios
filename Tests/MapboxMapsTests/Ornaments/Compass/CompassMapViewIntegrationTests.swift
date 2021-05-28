efc4e1780baddf0126fbb0e16ec08715a0ac30e2
Jordan Kiley <jordan.kiley@mapbox.com>
2021-05-28 13:48:54 -0400

    Address initial feedback, add descriptions to asserts

--- Tests/MapboxMapsTests/Ornaments/Compass/CompassMapViewIntegrationTests.swift
+++ Tests/MapboxMapsTests/Ornaments/Compass/CompassMapViewIntegrationTests.swift
@@ -7,41 +7,33 @@ import XCTest
 
 class CompassMapViewIntegrationTests: MapViewIntegrationTestCase {
 
-    func testUpdateBearing() {
-        guard let mapView = mapView else {
-            XCTFail("Map view could not be found")
-            return
-        }
-        let initialSubviews = mapView.subviews.filter { $0.isKind(of: MapboxCompassOrnamentView.self) }
-        guard let compass = initialSubviews.first as? MapboxCompassOrnamentView else {
-            XCTFail("Failed because compass could not be found")
-            return
-        }
+    func testUpdateMapBearing() throws {
+        let mapView = try XCTUnwrap(self.mapView, "Map view could not be found")
 
-        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, 0)
-        XCTAssertTrue(compass.containerView.isHidden)
-        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, compass.currentBearing)
+        let initialSubviews = mapView.subviews.filter { $0.isKind(of: MapboxCompassOrnamentView.self) }
+
+        let compass = try XCTUnwrap(initialSubviews.first as? MapboxCompassOrnamentView, "The MapView should include a compass view as a subview")
+
+        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, 0, "The map's initial bearing should be equal to 0")
+        XCTAssertTrue(compass.containerView.isHidden, "The compass should be hidden initially")
+        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, compass.currentBearing, "The map's initial bearing should be equal to the compass' bearing")
 
         mapView.camera.setCamera(to: CameraOptions(bearing: 30))
-        XCTAssertFalse(compass.containerView.isHidden)
-        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, compass.currentBearing)
-        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, 30, accuracy: 0.2)
+        XCTAssertFalse(compass.containerView.isHidden, "The compass should hidden when the bearing is 30.")
+        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, compass.currentBearing, "The map's bearing should be equal to the compass' current bearing")
+        XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, 30, accuracy: 0.2, "The map's bearing should be equal to 30 with an accuracy of 0.2.")
 
         mapView.camera.setCamera(to: CameraOptions(bearing: 0))
         XCTAssertTrue(compass.containerView.isHidden)
         XCTAssertEqual(mapView.mapboxMap.cameraState.bearing, 0)
     }
 
-    func testCompassTapped() {
-        guard let mapView = mapView else {
-            XCTFail("Map view could not be found")
-            return
-        }
+    func testCompassTappedResetsToNorth() {
+        let mapView = try XCTUnwrap(self.mapView, "Map view could not be found")
+
         let initialSubviews = mapView.subviews.filter { $0.isKind(of: MapboxCompassOrnamentView.self) }
-        guard let compass = initialSubviews.first as? MapboxCompassOrnamentView else {
-            XCTFail("Failed because compass could not be found")
-            return
-        }
+
+        let compass = try XCTUnwrap(initialSubviews.first as? MapboxCompassOrnamentView, "The MapView should include a compass view as a subview")
 
         mapView.camera.setCamera(to: CameraOptions(bearing: 30))
 
@@ -49,6 +41,7 @@ class CompassMapViewIntegrationTests: MapViewIntegrationTestCase {
 
         let mapExpectation = XCTestExpectation(description: "The bearing for the map should be 0 after a tap gesture")
         let compassExpectation = XCTestExpectation(description: "The bearing for the compass should be 0 after a tap gesture.")
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             if mapView.mapboxMap.cameraState.bearing == 0 {
                 mapExpectation.fulfill()

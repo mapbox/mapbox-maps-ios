21bec45188eb01f276749fb122e57b8533886b41
Julian Rex <julian.rex@gmail.com>
2021-05-28 11:10:53 -0400

    Fix for tests failure due to aggressive teardown

--- Tests/MapboxMapsTests/MapView/Integration Tests/OfflineManagerIntegrationTests.swift
+++ Tests/MapboxMapsTests/MapView/Integration Tests/OfflineManagerIntegrationTests.swift
@@ -273,7 +273,6 @@ internal class OfflineManagerIntegrationTestCase: IntegrationTestCase {
 
             OfflineSwitch.shared.isMapboxStackConnected = true
             mapView.removeFromSuperview()
-            rootView.removeFromSuperview()
         }
 
         XCTAssertNil(weakMapView)

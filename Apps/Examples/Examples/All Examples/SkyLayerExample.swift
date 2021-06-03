a5cd9423b296a2e6dca8be55fe683a8e2ea4d5c6
Jordan Kiley <jordan.kiley@mapbox.com>
2021-06-03 15:47:42 -0400

    Setup mapInitOptions

--- Apps/Examples/Examples/All Examples/SkyLayerExample.swift
+++ Apps/Examples/Examples/All Examples/SkyLayerExample.swift
@@ -11,18 +11,28 @@ public class SkyLayerExample: UIViewController, ExampleProtocol {
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-//        let mapInitOptions = MapInitOptions(cameraOptions: <#T##CameraOptions?#>, styleURI: <#T##StyleURI?#>)
-        mapView = MapView(frame: view.bounds)
+        let center = CLLocationCoordinate2D(latitude: 35.67283, longitude: 127.60597)
+        let cameraOptions = CameraOptions(center: center, zoom: 14, pitch: 83)
+        let mapInitOptions = MapInitOptions(cameraOptions: cameraOptions, styleURI: .satelliteStreets)
+        mapView = MapView(frame: view.bounds, mapInitOptions: mapInitOptions)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 
         view.addSubview(mapView)
         mapView.mapboxMap.onNext(.styleLoaded) { _ in
-
+            self.addSkyLayer()
         }
     }
 
     func addSkyLayer() {
 
+    }
+    
+    func updateSkyLayer() {
+        
+    }
+    // Add a `UISlider` to control the gradient of the sky layer
+    @objc func addSlider() {
+        
     }
     override public func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)

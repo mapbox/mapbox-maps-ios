a1ad094ac54d29d0c7088c35a7f0248ac4e2513c
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-06-09 15:27:49 -0400

    Add test

--- Tests/MapboxMapsTests/Gestures/GestureManagerTests.swift
+++ Tests/MapboxMapsTests/Gestures/GestureManagerTests.swift
@@ -37,6 +37,16 @@ final class GestureManagerTests: XCTestCase {
         XCTAssert(gestureManager.gestureHandlers[.tap(numberOfTaps: 2, numberOfTouches: 2)] is TapGestureHandler)
         XCTAssert(gestureManager.gestureHandlers[.pan] is PanGestureHandler)
     }
+    
+    func testGestureRecognizersReturnedAreSameAsRecognizersPresentInGestureHandlers() {
+        XCTAssertTrue(gestureManager.gestureHandlers[.tap(numberOfTaps: 2, numberOfTouches: 1)]?.gestureRecognizer === gestureManager.doubleTapToZoomInGestureRecognizer)
+        XCTAssertTrue(gestureManager.gestureHandlers[.tap(numberOfTaps: 2, numberOfTouches: 2)]?.gestureRecognizer === gestureManager.doubleTapToZoomOutGestureRecognizer)
+        XCTAssertTrue(gestureManager.gestureHandlers[.pan]?.gestureRecognizer === gestureManager.panGestureRecognizer)
+        XCTAssertTrue(gestureManager.gestureHandlers[.pinch]?.gestureRecognizer === gestureManager.pinchGestureRecognizer)
+        XCTAssertTrue(gestureManager.gestureHandlers[.pitch]?.gestureRecognizer === gestureManager.pitchGestureRecognizer)
+        XCTAssertTrue(gestureManager.gestureHandlers[.quickZoom]?.gestureRecognizer === gestureManager.quickZoomGestureRecognizer)
+        XCTAssertTrue(gestureManager.gestureHandlers[.rotate]?.gestureRecognizer === gestureManager.rotationGestureRecognizer)
+    }
 
     func testTapGesturesTypesAreEqual() {
         let singleTapA = GestureType.tap(numberOfTaps: 1, numberOfTouches: 1)

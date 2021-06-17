3fbb7ef30632fc62c37e91d9f0dcc50efc2e2138
Neel Mistry <neel.mistry@mapbox.com>
2021-06-17 14:58:51 -0400

    documentation

--- Sources/MapboxMaps/Style/SupportedLanguage.swift
+++ Sources/MapboxMaps/Style/SupportedLanguage.swift
@@ -1,17 +1,45 @@
 /// A list of languages that our maps support for localization
-public enum SupportedLanguage: String, Equatable {
-    case english = "en"
-    case french = "fr"
+public enum SupportedLanguage: String, CaseIterable {
+
+    /// Support for Arabic (if available)
     case arabic = "ar"
-    case spanish = "es"
-    case german = "de"
-    case portuguese = "pt"
-    case russian = "ru"
+
+    /// Support for Chinese (if available)
     case chinese = "zh"
-    case traditionalChinese = "zh-Hant"
-    case simplifiedChinese = "zh-Hans"
-    case japanese = "ja"
-    case korean = "ko"
-    case vietnamese = "vi"
+
+    /// Support for English (if available)
+    case english = "en"
+
+    /// Support for French (if available)
+    case french = "fr"
+
+    /// Support for German (if available)
+    case german = "de"
+
+    /// Support for Italian (if available)
     case italian = "it"
+
+    /// Support for Japanese (if available)
+    case japanese = "ja"
+
+    /// Support for Korean (if available)
+    case korean = "ko"
+
+    /// Support for Portuguese (if available)
+    case portuguese = "pt"
+
+    /// Support for Russian (if available)
+    case russian = "ru"
+
+    /// Support for Simplified Chinese (if available)
+    case simplifiedChinese = "zh-Hans"
+
+    /// Support for Spanish (if available)
+    case spanish = "es"
+
+    /// Support for Traditional Chinese (if available)
+    case traditionalChinese = "zh-Hant"
+
+    /// Support for Vietnamese (if available)
+    case vietnamese = "vi"
 }

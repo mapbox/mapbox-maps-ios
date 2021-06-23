bf6364f42aea8677bcc6105e7319eadf45a2906a
Jordan Kiley <jordan.kiley@mapbox.com>
2021-06-23 10:02:44 -0400

    Add second step to tutorial

--- Mapbox/MapboxMaps.docc/Tutorial code/01-add-a-map-02.swift
+++ Mapbox/MapboxMaps.docc/Tutorial code/01-add-a-map-02.swift
@@ -1,2 +1,15 @@
-#  <#Title#>
+#  Add a map view
 
+import MapboxMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let mapView = MapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        view.addSubview(mapView)
+    }
+}

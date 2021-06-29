6bee24b7b29c49ef4e1af35b4ba4437a98750169
Jordan Kiley <jordan.kiley@mapbox.com>
2021-06-29 18:18:57 -0400

    Add more stops to the dictionary

--- Apps/Examples/Examples/All Examples/DistanceExpressionExample.swift
+++ Apps/Examples/Examples/All Examples/DistanceExpressionExample.swift
@@ -21,10 +21,11 @@ class DistanceExpressionExample: UIViewController, ExampleProtocol {
     }
 
     func addCircle() {
+        let style = mapView.mapboxMap.style
         let center = mapView.mapboxMap.cameraState.center
         var source = GeoJSONSource()
         let point = Turf.Point(center)
-        source.data = .geometry(.point(point))
+        source.data = .feature(point)
         var circle = CircleLayer(id: "circle-layer")
         circle.source = "source-id"
         let circleRadiusExp = Exp(.interpolate) {
@@ -38,20 +39,43 @@ class DistanceExpressionExample: UIViewController, ExampleProtocol {
             circleRadius(forZoom: 10)
             15
             circleRadius(forZoom: 15)
+            16
+            circleRadius(forZoom: 16)
+            16.5
+            circleRadius(forZoom: 16.5)
             17
             circleRadius(forZoom: 17)
+            17.5
+            circleRadius(forZoom: 17.5)
             18
             circleRadius(forZoom: 18)
+            18.5
+            circleRadius(forZoom: 18.5)
             19
             circleRadius(forZoom: 19)
+            19.5
+            circleRadius(forZoom: 19.5)
             20
             circleRadius(forZoom: 20)
+            20.5
+            circleRadius(forZoom: 20.5)
+            21
+            circleRadius(forZoom: 21)
+            21.5
+            circleRadius(forZoom: 21.5)
+            22
+            circleRadius(forZoom: 22)
         }
 
         circle.circleRadius = .expression(circleRadiusExp)
         circle.circleOpacity = .constant(0.3)
-        try! mapView.mapboxMap.style.addSource(source, id: "source-id")
-        try! mapView.mapboxMap.style.addLayer(circle)
+        try! style.addSource(source, id: "source-id")
+        try! style.addLayer(circle)
+        
+        let data = JSONEncoder().encode(point.self)
+        var poiLabelLayer: SymbolLayer = try! style.layer(withId: "poi-label")
+        poiLabelLayer.filter = Exp(.distance) {
+        }
     }
 
     func circleRadius(forZoom zoom: CGFloat) -> Double {

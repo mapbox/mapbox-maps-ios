cde8041e1cdadcabd61d3ee9fc964f1493fa2571
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-06-30 15:07:35 -0500

    Maps SDK Release v10.0.0-rc.3

--- scripts/release/update-version.sh
+++ scripts/release/update-version.sh
@@ -31,6 +31,8 @@ step "Updating Xcode targets to version ${SHORT_VERSION}…"
 xcrun agvtool bump -all
 xcrun agvtool new-marketing-version "${SHORT_VERSION}"
 
+cd ..
+
 # Update MapboxMaps.podspec
 step "Update Podspec"
 sed -i '' s/"maps_version = '.*'"/"maps_version = '${SEM_VERSION}'"/ MapboxMaps.podspec

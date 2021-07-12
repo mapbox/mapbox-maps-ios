2e04c9affc8871201abb7e81ff54f309030b8614
Jordan Kiley <jordan.kiley@mapbox.com>
2021-07-12 13:57:08 -0400

    Initial commit

---
+++ Apps/Examples/Examples/All Examples/ShowHideLayerExample.swift
@@ -0,0 +1,12 @@
+import MapboxMaps
+
+@objc(ShowHideLayerExample)
+
+public class ShowHideLayerExample: UIViewController, ExampleProtocol {
+
+    internal var mapView: MapView!
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

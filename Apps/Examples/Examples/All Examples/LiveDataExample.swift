44e1289c071e2151b7a831e309f5b12092b1b45b
Jordan Kiley <jordan.kiley@mapbox.com>
2021-07-21 11:23:53 -0400

    Update to use a result for the GeoJSON parsing

--- Apps/Examples/Examples/All Examples/LiveDataExample.swift
+++ Apps/Examples/Examples/All Examples/LiveDataExample.swift
@@ -6,6 +6,7 @@ import Turf
 class LiveDataExample: UIViewController, ExampleProtocol {
     var mapView: MapView!
     let sourceId = "drone-source"
+    var droneTimer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,37 +40,56 @@ class LiveDataExample: UIViewController, ExampleProtocol {
             try mapView.mapboxMap.style.addSource(source, id: sourceId)
             try mapView.mapboxMap.style.addLayer(rocketLayer)
             // Create a `Timer` that updates the `GeoJSONSource`.
-            _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
-                self.updateGeoJSONSource()
+            droneTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+                self.parseGeoJSON { result in
+                    switch result {
+                    case .success(let feature):
+                       try! self.mapView.mapboxMap.style.updateGeoJSONSource(withId: self.sourceId, geoJSON: feature)
+                    case .failure(let error):
+                        print("Error: \(error.localizedDescription)")
+                    }
+                }
             })
         } catch {
             print("Failed to update the style. Error: \(error.localizedDescription)")
         }
     }
 
-    func updateGeoJSONSource() {
-        guard let url = URL(string: "https://wanderdrone.appspot.com/") else { return }
+    func parseGeoJSON(completion: @escaping(Result<Turf.Feature, Error>)->()) {
+        let url = URL(string: "https://wanderdrone.appspot.com/")!
 
-        do {
-            // Parse the geoJSON from the URL. An example response:
-            // {"geometry": {"type": "Point", "coordinates": [-50.26804118917149, 38.76944015557226]}, "type": "Feature", "properties": {}}
-            let data = try Data(contentsOf: url)
-            let geoJSON = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        DispatchQueue.global().async {
+            let result: Result<Turf.Feature, Error>
+            do {
+                // Parse the geoJSON from the URL. An example response:
+                // {"geometry": {"type": "Point", "coordinates": [-50.26804118917149, 38.76944015557226]}, "type": "Feature", "properties": {}}
+                let data = try Data(contentsOf: url)
 
-            if let geometry = geoJSON!["geometry"] as? [String: Any], let coordinates = geometry["coordinates"] as? Array<Double> {
-                let location = CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
+                if let geoJSON = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let geometry = geoJSON["geometry"] as? [String: Any],
+                   let coordinates = geometry["coordinates"] as? Array<Double> {
+                    let location = CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
 
-                // Create a `Turf.Point` and `Turf.Feature` from the geoJSON data.
-                let point = Point(location)
-                let feature = Feature.init(geometry: Turf.Geometry.point(point))
-
-                // Update the `GeoJSONSource` with id "drone-source" with the
-                // point feature. This will also update the layer representing
-                // the point.
-                try mapView.mapboxMap.style.updateGeoJSONSource(withId: sourceId, geoJSON: feature)
+                    // Create a `Turf.Point` and `Turf.Feature` from the geoJSON data.
+                    let point = Point(location)
+                    let feature = Feature.init(geometry: Turf.Geometry.point(point))
+                    result = .success(feature)
+                } else {
+                    let error = TypeConversionError.invalidObject
+                    result = .failure(error)
+                }
+            } catch {
+                result = .failure(error)
             }
-        } catch {
-            print("Failed to update the GeoJSON source with id \(sourceId). Error: \(error.localizedDescription)")
+            DispatchQueue.main.async {
+                completion(result)
+            }
         }
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        droneTimer?.invalidate()
+        droneTimer = nil
     }
 }

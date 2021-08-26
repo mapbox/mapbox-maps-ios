1be5e67037d3eedbf081980a606f43ea67087aa9
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-08-26 12:28:16 -0500

    Update comments

--- Sources/MapboxMaps/Foundation/DisplayLinkCoordinator.swift
+++ Sources/MapboxMaps/Foundation/DisplayLinkCoordinator.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 internal protocol DisplayLinkCoordinator: AnyObject {
-    // The coordinator would only keep weak references to participants
+    // The coordinator must only keep weak references to participants
     func add(_ participant: DisplayLinkParticipant)
-    // Removal would be based on object identity
+    // Removal is be based on object identity
     func remove(_ participant: DisplayLinkParticipant)
 }
 
-// The participants would need to be NSObjects so that the DisplayLinkCoordinator implementation can use WeakSet
+// The participants must be NSObjects so that the DisplayLinkCoordinator implementation can use WeakSet
 internal protocol DisplayLinkParticipant: NSObject {
     func participate()
 }

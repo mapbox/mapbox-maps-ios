00edd51c96aa08daa37fdbc73441816483cc8eae
Jordan Kiley <jordan.kiley@mapbox.com>
2021-09-01 16:15:02 -0400

    [create xcframework] use git sha
    [create xcframework] remove references to zup
    
    [create xcframework] update directory name

--- scripts/api-compatibility-check/maps-api-check.sh
+++ scripts/api-compatibility-check/maps-api-check.sh
@@ -4,44 +4,33 @@ set -eo pipefail
 
 #
 # Usage:
-#   ./scripts/ios/maps-api-check.sh <current release tag (empty for branches)> <path to current MapboxMaps.zip> <optional, path to previous MapboxMaps.zip>
+#   ./scripts/ios/maps-api-check.sh <current release tag (empty for branches)>
 #
 
 TAGGED_RELEASE_VERSION=$1
-TAGGED_RELEASE_VERSION=${TAGGED_RELEASE_VERSION:6}
 
-if [[ ! -f $2 ]]
-then
-  echo "Please provide path to the current MapboxMaps.zip file"
-  exit 1
-fi
-
-CURRENT_DIR=$(dirname "$0")
+CURRENT_DIR=$(dirname ${CIRCLE_TAG})
 ROOT_DIR="${CURRENT_DIR}/../.."
 REPORT_DIR=${ROOT_DIR}/api_compat_report
 mkdir -p ${REPORT_DIR}
 
 TMPDIR=`mktemp -d`
-mkdir ${TMPDIR}/current ${TMPDIR}/previous
-cp $2 ${TMPDIR}/current/MapboxMaps.zip
-CURRENT_RELEASE=${TMPDIR}/current/MapboxMaps.zip
-PREVIOUS_RELEASE=${TMPDIR}/previous/MapboxMaps.zip
-CURRENT_RELEASE_DIR=$(dirname ${CURRENT_RELEASE})
-PREVIOUS_RELEASE_DIR=$(dirname ${PREVIOUS_RELEASE})
+CURRENT_RELEASE_DIR=$(dirname ${GITHUB_SHA})
+PREVIOUS_RELEASE_DIR=$(dirname ${LAST_VERSION})
 
 if [[ -z ${TAGGED_RELEASE_VERSION} ]]; then
     LAST_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 else
     LAST_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1 --skip=1 --no-walk))
 fi
-LAST_VERSION=${LAST_VERSION:1}
-
-if [[ -z $3 ]]; then
-  echo "Path to previous version of MapboxMaps.zip is not set, using ${LAST_VERSION}"
-  aws s3 cp s3://mapbox-api-downloads-production/v2/mobile-maps-ios/releases/ios/${LAST_VERSION}/MapboxMaps.zip ${PREVIOUS_RELEASE}
-else
-  cp $3 ${TMPDIR}/previous/MapboxMaps.zip
-fi
+# LAST_VERSION=${LAST_VERSION:1}
+#
+# if [[ -z $3 ]]; then
+#   echo "Path to previous version of MapboxMaps.zip is not set, using ${LAST_VERSION}"
+#   aws s3 cp s3://mapbox-api-downloads-production/v2/mobile-maps-ios/releases/ios/${LAST_VERSION}/MapboxMaps.zip ${PREVIOUS_RELEASE}
+# else
+#   cp $3 ${TMPDIR}/previous/MapboxMaps.zip
+# fi
 
 if [[ ! -d ~/mapbox-apidiff ]]; then
     git clone --depth 1 https://github.com/mapbox/apidiff ~/mapbox-apidiff
@@ -56,24 +45,24 @@ generateSourceKittenDoc() {
     OUT_DIR=$(dirname $1)
     pushd ${OUT_DIR} > /dev/null
     mkdir MapboxMaps
-    unzip -qq MapboxMaps.zip -d .
-    cp -r artifacts/MapboxMaps.xcframework/ios-arm64/MapboxMaps.framework/* ./MapboxMaps/
+    git checkout $2
 
     # Generate doc for public headers
-    sourcekitten doc --module-name sourcekitten doc -- -workspace Apps/Apps.xcworkspace \
+    sourcekitten doc --module-name sourcekitten doc -- -workspace ../../Apps/Apps.xcworkspace \
         -scheme MapboxMaps -sdk iphonesimulator > swift_public.json
 
     # Merge the output for the public and external modules
     jq -s \
         '[ .[0] + .[1] | .[] | {name: keys[0], data: .[keys[0]]} ] | group_by(.name) | [.[] | add | {(.name): .data}]' \
-        ./swift_public.jsonn > swift_api.json
+        ./swift_public.json > swift_api.json
 
-    if [[ $(jq length objc_api.json) == 0 ]]; then
+    if [[ $(jq length swift_api.json) == 0 ]]; then
         echo "sourcekitten output is empty" >&2
         exit 1
     fi
 
     popd > /dev/null
+    git checkout ${GITHUB_SHA}
 }
 
 parse_json_report() {
@@ -125,8 +114,8 @@ compareAPI() {
     fi
 }
 
-generateSourceKittenDoc ${CURRENT_RELEASE}
-generateSourceKittenDoc ${PREVIOUS_RELEASE}
+generateSourceKittenDoc ${GITHUB_SHA}
+generateSourceKittenDoc ${LAST_VERSION}
 api_compat=$(compareAPI ${PREVIOUS_RELEASE_DIR} ${CURRENT_RELEASE_DIR})
 rm -rf ${TMPDIR}
 

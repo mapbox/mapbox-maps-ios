f3787787b44f6db6a295bf5f73abe8706e88872f
Jordan Kiley <jordan.kiley@mapbox.com>
2021-09-03 16:02:08 -0400

    [create xcframework] Update script to use apidiff and custom branch, update directories
    [create xcframework] Update apidiff branch name
    
    [create xcframework] Update apidiff cloning
    
    [create xcframework] move branch checkout
    
    [create xcframework] move branch checkout

--- scripts/api-compatibility-check/maps-api-check.sh
+++ scripts/api-compatibility-check/maps-api-check.sh
@@ -9,28 +9,16 @@ set -eo pipefail
 
 TAGGED_RELEASE_VERSION=$1
 
-CURRENT_DIR=$(dirname '${CIRCLE_TAG}')
+CURRENT_DIR=$(pwd)
 ROOT_DIR="${CURRENT_DIR}/../.."
 REPORT_DIR=${ROOT_DIR}/api_compat_report
 mkdir -p ${REPORT_DIR}
-
-TMPDIR=`mktemp -d`
-CURRENT_RELEASE_DIR=$(dirname ${GITHUB_SHA})
-PREVIOUS_RELEASE_DIR=$(dirname ${LAST_VERSION})
 
 if [[ -z ${TAGGED_RELEASE_VERSION} ]]; then
     LAST_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 else
     LAST_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1 --skip=1 --no-walk))
 fi
-# LAST_VERSION=${LAST_VERSION:1}
-#
-# if [[ -z $3 ]]; then
-#   echo "Path to previous version of MapboxMaps.zip is not set, using ${LAST_VERSION}"
-#   aws s3 cp s3://mapbox-api-downloads-production/v2/mobile-maps-ios/releases/ios/${LAST_VERSION}/MapboxMaps.zip ${PREVIOUS_RELEASE}
-# else
-#   cp $3 ${TMPDIR}/previous/MapboxMaps.zip
-# fi
 
 if [[ ! -d ~/mapbox-apidiff ]]; then
     git clone --depth 1 https://github.com/mapbox/apidiff ~/mapbox-apidiff
@@ -38,32 +26,6 @@ if [[ ! -d ~/mapbox-apidiff ]]; then
     swift build
     popd > /dev/null
 fi
-
-# Generates sourcekitten json doc
-generateSourceKittenDoc() {
-    set -eo pipefail
-    OUT_DIR=$(dirname $1)
-    pushd ${OUT_DIR} > /dev/null
-    mkdir MapboxMaps
-    git checkout $2
-
-    # Generate doc for public headers
-    sourcekitten doc --module-name sourcekitten doc -- -workspace ../../Apps/Apps.xcworkspace \
-        -scheme MapboxMaps -sdk iphonesimulator > swift_public.json
-
-    # Merge the output for the public and external modules
-    jq -s \
-        '[ .[0] + .[1] | .[] | {name: keys[0], data: .[keys[0]]} ] | group_by(.name) | [.[] | add | {(.name): .data}]' \
-        ./swift_public.json > swift_api.json
-
-    if [[ $(jq length swift_api.json) == 0 ]]; then
-        echo "sourcekitten output is empty" >&2
-        exit 1
-    fi
-
-    popd > /dev/null
-    git checkout ${GITHUB_SHA}
-}
 
 parse_json_report() {
     set -eo pipefail
@@ -76,13 +38,13 @@ const issues = Object.keys(report).reduce((acc, k) => {
         const hasDuplicate = (diffType) => {
             return report[k].find(e => e.name === entry.name && e.diff === diffType);
         }
-        if (entry.diff === "addition" && !hasDuplicate("deletion")) {
+        if (entry.diff === "added" && !hasDuplicate("deletion")) {
             acc.minor++;
         }
-        if (entry.diff === "deletion" && !hasDuplicate("addition")) {
+        if (entry.diff === "deleted" && !hasDuplicate("addition")) {
             acc.major++;
         }
-        if (entry.diff === "modification") {
+        if (entry.diff === "modified") {
             acc.unknown++;
         }
         return acc;
@@ -97,10 +59,8 @@ EOF
 compareAPI() {
     set -eo pipefail
     JSON_TMP_FILE=$(mktemp)
-
-    pushd ~/mapbox-apidiff/apple/diffreport > /dev/null
-    swift run diffreport $1/swift_api.json $2/swift_api.json --json > "${JSON_TMP_FILE}"
-    popd > /dev/null
+    git checkout jk/iphone-simulator
+    src/apidiff $LAST_VERSION $GITHUB_REF swift ../../Apps/App.xcworkspace MapboxMaps > $JSON_TMP_FILE
 
     eval "$(parse_json_report ${JSON_TMP_FILE})"
     mv "${JSON_TMP_FILE}" ${REPORT_DIR}/api_compat.json
@@ -113,10 +73,5 @@ compareAPI() {
         echo patch
     fi
 }
-
-generateSourceKittenDoc ${GITHUB_SHA}
-generateSourceKittenDoc ${LAST_VERSION}
-api_compat=$(compareAPI ${PREVIOUS_RELEASE_DIR} ${CURRENT_RELEASE_DIR})
-rm -rf ${TMPDIR}
 
 ${CURRENT_DIR}/semver-check.sh "${TAGGED_RELEASE_VERSION}" "${LAST_VERSION}" "${api_compat}"

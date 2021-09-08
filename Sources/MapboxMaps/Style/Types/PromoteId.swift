00ea8333015b4f01799cdb3e385bbe79bfcc038f
Nishant Karajgikar <nishant.karajgikar@mapbox.com>
2021-09-08 10:35:29 -0400

    fix lint

--- Sources/MapboxMaps/Style/Types/PromoteId.swift
+++ Sources/MapboxMaps/Style/Types/PromoteId.swift
@@ -3,24 +3,24 @@ import Foundation
 public enum PromoteId: Equatable, Codable {
     case string(String)
     case object([String: String])
-    
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        
+
         if let decodedString = try? container.decode(String.self) {
             self = .string(decodedString)
             return
-            
+
         } else if let decodedObject = try? container.decode([String: String].self) {
             self = .object(decodedObject)
             return
         }
-        
+
         let context = DecodingError.Context(codingPath: decoder.codingPath,
                                             debugDescription: "Failed to decode PromoteId")
         throw DecodingError.dataCorrupted(context)
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
 

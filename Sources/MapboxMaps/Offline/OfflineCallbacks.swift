83a33081147789fc6d0321d005f242688b51b044
Neel Mistry <Neel.mistry@mapbox.com>
2021-09-08 13:53:20 -0400

    Nm/fix geometry in tile store 2 (#661)
    * update result type and changelog
    
    * updating syntax to new typealias
    
    * pushing debug code
    
    * debug code
    
    * support turf geometry in offline tile regions
    
    * remove whitespace
    
    * PR Comments

--- Sources/MapboxMaps/Offline/OfflineCallbacks.swift
+++ Sources/MapboxMaps/Offline/OfflineCallbacks.swift
@@ -10,9 +10,10 @@ import Foundation
 internal func coreAPIClosureAdapter<T, SwiftError, ObjCType>(
     for closure: @escaping (Result<T, Error>) -> Void,
     type: ObjCType.Type,
-    concreteErrorType: SwiftError.Type) -> ((Expected<AnyObject, AnyObject>?) -> Void) where ObjCType: AnyObject,
-                                                                                                SwiftError: CoreErrorRepresentable,
-                                                                                                SwiftError.CoreErrorType: AnyObject {
+    concreteErrorType: SwiftError.Type,
+    converter: @escaping (ObjCType) -> T? = { $0 as? T }) -> ((Expected<AnyObject, AnyObject>?) -> Void) where ObjCType: AnyObject,
+                                                                                                               SwiftError: CoreErrorRepresentable,
+                                                                                                               SwiftError.CoreErrorType: AnyObject {
     return { (expected: Expected?) in
         let result: Result<T, Error>
 
@@ -26,7 +27,7 @@ internal func coreAPIClosureAdapter<T, SwiftError, ObjCType>(
             return
         }
 
-        if expected.isValue(), let value = expected.value as? T {
+        if expected.isValue(), let value = expected.value.flatMap(converter) {
             result = .success(value)
         } else if expected.isError(), let error = expected.error {
             result = .failure(SwiftError(coreError: error))
@@ -40,7 +41,7 @@ internal func coreAPIClosureAdapter<T, SwiftError, ObjCType>(
 internal func coreAPIClosureAdapter<SwiftError>(
     for closure: @escaping (Error?) -> Void,
     concreteErrorType: SwiftError.Type) -> ((Expected<AnyObject, AnyObject>?) -> Void) where SwiftError: CoreErrorRepresentable,
-                                                                                                SwiftError.CoreErrorType: AnyObject {
+                                                                                             SwiftError.CoreErrorType: AnyObject {
     return { (expected: Expected?) in
         var error: Error?
 

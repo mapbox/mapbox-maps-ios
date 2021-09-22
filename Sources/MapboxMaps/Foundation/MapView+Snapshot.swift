5ad3f0ea44db010f655f5f717458cd704af09d24
Nishant Karajgikar <nishant-karajgikar@users.noreply.github.com>
2021-09-22 15:01:58 -0400

    Enable capturing last rendered state of MapView (#693)
    * Move snapshot code to MapView extension
    
    * Clean up
    
    * Allow developer to take a snapshot of the currently rendered map
    
    * Clarifying docs
    
    * Undoing changes to DebugViewController
    
    * Remove unused  helper
    
    * reverting changes to debugapp.xcscheme
    
    * Add integration tests [run device tests]
    
    * Changelog
    
    * fix lint [run device tests]
    
    * address review feedbacck
    
    Co-authored-by: Julian Rex <julian.rex@gmail.com>

---
+++ Sources/MapboxMaps/Foundation/MapView+Snapshot.swift
@@ -0,0 +1,61 @@
+@_implementationOnly import MapboxCommon_Private
+
+@available(iOSApplicationExtension, unavailable)
+extension MapView {
+
+    /// Errors related to rendered snapshots
+    @_spi(Experimental) public enum SnapshotError: Error {
+        /// No metal view available. Catastrophic error.
+        case noMetalView
+
+        /// Metal texture not present in mapView.
+        case invalidTexture
+
+        /// Texture failed to convert to CGImage
+        case textureConversionFailed
+
+        /// Metal validation is enabled, unsupported configuration
+        case metalValidationEnabled
+
+        /// Converted image is empty in snapshot
+        case convertedImageIsEmpty
+    }
+
+    /// Synchronously captures the last rendered map view (if available) and constructs a `UIImage` if successful.
+    /// - NOTE: This API must be called on main thread
+    @_spi(Experimental) public func snapshot() throws -> UIImage {
+
+        guard let metalView = subviews.first(where: { $0 is MTKView }) as? MTKView else {
+            Log.error(forMessage: "No metal view present.", category: "MapView.snapshot")
+            throw SnapshotError.noMetalView
+        }
+
+        // If Metal API validation is enabled, the call to CIContext().createCGImage
+        // below will crash with the following message:
+        //
+        //  -[MTLDebugComputeCommandEncoder setTexture:atIndex:]:373: failed
+        //  assertion `frameBufferOnly texture not supported for compute.'
+        guard getenv("METAL_DEVICE_WRAPPER_TYPE") == nil else {
+            Log.error(forMessage: "Metal API validation is enabled - MapView snapshot is being skipped.", category: "MapView.snapshot")
+            throw SnapshotError.metalValidationEnabled
+        }
+
+        guard let texture = metalView.currentDrawable?.texture else {
+            Log.error(forMessage: "Metal texture could not be retrieved from current drawable.", category: "MapView.snapshot")
+            throw SnapshotError.invalidTexture
+        }
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let ciImage = CIImage(mtlTexture: texture, options: [CIImageOption.colorSpace: colorSpace]),
+              let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent) else {
+            Log.error(forMessage: "Metal texture could not be converted to CGImage.", category: "MapView.snapshot")
+            throw SnapshotError.textureConversionFailed
+        }
+
+        return UIImage(
+            cgImage: cgImage,
+            scale: metalView.contentScaleFactor,
+            orientation: .downMirrored)
+
+    }
+}
---
+++ Tests/MapboxMapsTests/Foundation/Integration Tests/Map/MapViewRenderedSnapshotIntegrationTests.swift
@@ -0,0 +1,79 @@
+import Foundation
+import XCTest
+@testable @_spi(Experimental) import MapboxMaps
+
+final class MapViewRenderedSnapshotIntegrationTests: MapViewIntegrationTestCase {
+
+    func testLoadStyleAndTakeSnapshotSucceeds() {
+
+        guard let style = style else {
+            XCTFail("Should have a valid Style object")
+            return
+        }
+
+        let waitForStyleExpectation = self.expectation(description: "Wait for style to load")
+        let waitForSnapshotExpectation = self.expectation(description: "Wait for snapshot to be taken")
+
+        didFinishLoadingStyle = { _ in
+            waitForStyleExpectation.fulfill()
+        }
+
+        didBecomeIdle = { [weak self] _ in
+            guard let mapView = self?.mapView else {
+                XCTFail("Mapview must exist.")
+                return
+            }
+
+            do {
+                let image = try mapView.snapshot()
+                XCTAssertNotNil(image)
+                waitForSnapshotExpectation.fulfill()
+            } catch {
+                XCTFail("Snapshot failed with error: \(error)")
+            }
+        }
+
+        style.uri = .dark
+        wait(for: [waitForStyleExpectation, waitForSnapshotExpectation], timeout: 10)
+    }
+
+    func testSnapshotFailsDueToNoMetalView() {
+
+        guard let style = style else {
+            XCTFail("Should have a valid Style object")
+            return
+        }
+
+        let waitForStyleExpectation = self.expectation(description: "Wait for style to load")
+        let waitForSnapshotExpectation = self.expectation(description: "Wait for snapshot to be taken")
+
+        didFinishLoadingStyle = { _ in
+            waitForStyleExpectation.fulfill()
+        }
+
+        didBecomeIdle = { [weak self] _ in
+            guard let mapView = self?.mapView else {
+                XCTFail("Mapview must exist.")
+                return
+            }
+
+            // Remove the metal view before attempting snapshot
+            mapView.subviews.forEach {
+                if $0 is MTKView {
+                    $0.removeFromSuperview()
+                }
+            }
+
+            do {
+                _ = try mapView.snapshot()
+            } catch {
+                XCTAssertEqual(error as? MapView.SnapshotError,
+                               MapView.SnapshotError.noMetalView)
+                waitForSnapshotExpectation.fulfill()
+            }
+        }
+
+        style.uri = .dark
+        wait(for: [waitForStyleExpectation, waitForSnapshotExpectation], timeout: 10)
+    }
+}

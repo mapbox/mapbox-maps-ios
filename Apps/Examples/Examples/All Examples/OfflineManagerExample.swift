b5ee9bdcc4aac445038c2d942137cb016c247ac7
Minh Nguyễn <mxn@1ec5.org>
2021-09-28 03:12:01 -0700

    Updated offline manager examples to avoid internal initializer

--- Apps/Examples/Examples/All Examples/OfflineManagerExample.swift
+++ Apps/Examples/Examples/All Examples/OfflineManagerExample.swift
@@ -124,7 +124,7 @@ final class OfflineManagerExample: UIViewController, ExampleProtocol {
 
         // Load the tile region
         let tileRegionLoadOptions = TileRegionLoadOptions(
-            geometry: MapboxCommon.Geometry(coordinate: tokyoCoord),
+            geometry: MapboxCommon.Geometry(point: tokyoCoord as NSValue),
             descriptors: [outdoorsDescriptor],
             metadata: ["tag": "my-outdoors-tile-region"],
             acceptExpired: true)!

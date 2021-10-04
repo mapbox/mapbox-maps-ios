5db22976ebbad522e75e4f3b612b425effa02a16
Zizasaurus <zmiles@gwu.edu>
2021-10-04 11:56:52 -0400

    Update 3D puck model for examples

--- Apps/Examples/Examples/All Examples/Custom3DPuckExample.swift
+++ Apps/Examples/Examples/All Examples/Custom3DPuckExample.swift
@@ -27,13 +27,13 @@ public class Custom3DPuckExample: UIViewController, ExampleProtocol {
     internal func setupExample() {
 
         // Fetch the `gltf` asset
-        let uri = Bundle.main.url(forResource: "race_car_model",
-                                  withExtension: "gltf")
+        let uri = Bundle.main.url(forResource: "sportcar",
+                                  withExtension: "glb")
 
         // Instantiate the model
         let myModel = Model(uri: uri,
-                            position: [-177.150925, 39.085006],
-                            orientation: [0, 0, 0])
+                            position: [-77.150925, 39.085006],
+                            orientation: [0, 0, 180])
 
         // Setting an expression to  scale the model based on camera zoom
         let scalingExpression = Exp(.interpolate) {

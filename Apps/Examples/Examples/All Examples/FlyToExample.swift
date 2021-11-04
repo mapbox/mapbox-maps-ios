575497c2fdf25afbf6e0254f8e60dea18c6b86c5
Andrew Hershberger <andrew.hershberger@mapbox.com>
2021-11-04 13:21:53 -0500

    Fix build warning in Examples app

--- Apps/Examples/Examples/All Examples/FlyToExample.swift
+++ Apps/Examples/Examples/All Examples/FlyToExample.swift
@@ -31,7 +31,7 @@ public class FlyToExample: UIViewController, ExampleProtocol {
                                 bearing: 180,
                                 pitch: 50)
 
-        let cancelable = mapView.camera.fly(to: end) { [weak self] _ in
+        _ = mapView.camera.fly(to: end) { [weak self] _ in
             print("Camera fly-to finished")
             // The below line is used for internal testing purposes only.
             self?.finish()

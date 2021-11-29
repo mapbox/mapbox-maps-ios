3a39f455ee3a496a206285ad6272830bfd281fd1
Takuto Suzuki <takuto.suzuki@mapbox.com>
2021-11-29 20:21:50 +0900

    add fill layer not working

--- Apps/Examples/Examples/All Examples/BasicMapExample.swift
+++ Apps/Examples/Examples/All Examples/BasicMapExample.swift
@@ -12,6 +12,24 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
     internal var calledOnce: Bool = false
     internal var location: AppleLocationProvider!
     internal var currentLayer: String?
+    internal var initialMinutes: Int = 7
+    internal var minutes: [Int] = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15
+    ]
     
     public typealias ApiCompletionHandler = (_ result: FeatureCollection) -> Void
 
@@ -34,7 +52,6 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
         
         location = AppleLocationProvider()
         location.setDelegate(self)
-        location.requestWhenInUseAuthorization()
 
         mapView.mapboxMap.onNext(.mapLoaded) { _ in
             self.mapView.location.delegate = self
@@ -42,18 +59,24 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
             self.mapView.location.overrideLocationProvider(with: self.location)
             self.mapView.location.options.puckType = .puck2D()
             self.mapView.location.options.puckBearingSource = .course
+            
+            self.location.requestWhenInUseAuthorization()
         }
         
         view.addSubview(mapView)
 
+        setupSlider()
+    }
+
+    internal func setupSlider() {
         // Create Slider
         slider = UISlider()
         slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.minimumValue = 2
-        slider.maximumValue = 16
+        slider.minimumValue = 0
+        slider.maximumValue = 15
         slider.isContinuous = true
         slider.tintColor = UIColor.blue
-        slider.value = 500
+        slider.value = Float(initialMinutes)
         slider.addTarget(self, action: #selector(self.sliderValueDidChange(_:)), for: .valueChanged)
 
         view.addSubview(slider)
@@ -61,6 +84,7 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
             slider.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
             slider.heightAnchor.constraint(equalToConstant: 44),
             slider.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -44),
+            slider.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
         ])
     }
 
@@ -71,16 +95,7 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
     }
 
     @objc func sliderValueDidChange(_ sender:UISlider!) {
-        let opts = [
-            2,
-            4,
-            6,
-            8,
-            10,
-            12,
-            14,
-            16
-        ]
+        let opts = minutes
         var optsMap: [Int: Bool] = [:]
         for v in opts {
             optsMap[v] = true
@@ -90,7 +105,6 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
         guard let exists = optsMap[Int(value)] else {
             return
         }
-        print(value)
         let layerName = valueToLayerName(value)
         if let layer = currentLayer {
             if layer == layerName {
@@ -110,16 +124,16 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
     }
 
     func hideLayer(_ layer: String) {
-        try? mapView.mapboxMap.style.updateLayer(withId: layer, type: LineLayer.self) { layer in
+        try? mapView.mapboxMap.style.updateLayer(withId: layer, type: FillLayer.self) { layer in
                 print("hideLayer(lineLayer)", layer)
-                layer.lineOpacity = .constant(0.0)
+                layer.fillOpacity = .constant(0.0)
         }
     }
 
     func showLayer(_ layer: String) {
-        try? mapView.mapboxMap.style.updateLayer(withId: layer, type: LineLayer.self) { layer in
+        try? mapView.mapboxMap.style.updateLayer(withId: layer, type: FillLayer.self) { layer in
                 print("showLayer(lineLayer)", layer)
-                layer.lineOpacity = .constant(1.0)
+                layer.fillOpacity = .constant(1.0)
         }
     }
 
@@ -129,16 +143,7 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
         // Create a dispatch group
         let group = DispatchGroup()
 
-        let opts = [
-            2,
-            4,
-            6,
-            8,
-            10,
-            12,
-            14,
-            16
-        ]
+        let opts = minutes
 
         var results: [Int: GeoJSONSourceData] = [:]
         // Make multiple simultaneous requests
@@ -166,7 +171,7 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
                     shape: result,
                     layerName: self.valueToLayerName(key))
             }
-            self.showLayer("2")
+            self.showLayer(self.valueToLayerName(self.initialMinutes))
         }
     }
 
@@ -189,54 +194,20 @@ public class BasicMapExample: UIViewController, ExampleProtocol {
             // Create a GeoJSON data source.
             var geoJSONSource = GeoJSONSource()
             geoJSONSource.data = shape
-            var lineLayer = LineLayer(id: layerName)
+            
+            var lineLayer = FillLayer(id: layerName)
             lineLayer.source = layerName
-            lineLayer.lineColor = .constant(StyleColor(.black))
-            lineLayer.lineOpacity = .constant(0.0)
-//                .constant(1.0)
-//                .expression(
-//                Exp(.switchCase) {
-//                    Exp(.boolean) {
-//                        Exp(.featureState) { "selected" }
-//                    }
-//                    0.0
-//                    0.0
-//                    .constant(1.0)
-//                    .constant(0.0)
-//                    Exp(.interpolate) {
-//                        Exp(.linear)
-//                        Exp(.get) { "mag" }
-//                        1
-//                        8
-//                        1.5
-//                        10
-//                        2
-//                        12
-//                        2.5
-//                        14
-//                        3
-//                        16
-//                        3.5
-//                        18
-//                        4.5
-//                        20
-//                        6.5
-//                        22
-//                        8.5
-//                        24
-//                        10.5
-//                        26
-//                    }
-//                    5
-//                }
-//            )
-            lineLayer.lineOpacityTransition = .init(duration: 300, delay: 0)
-            lineLayer.lineWidth = .constant(4)
-//            var polygonLayer = FillLayer(id: layerName)
-//            polygonLayer.source = layerName
-//            polygonLayer.fillColor = .constant(StyleColor(.green))
-//            polygonLayer.fillOpacity = .constant(0)
-//            polygonLayer.fillOutlineColor = .constant(StyleColor(.purple))
+            lineLayer.fillColor = .constant(StyleColor(.orange))
+            lineLayer.fillOpacity = .constant(0.0)
+            lineLayer.fillOpacityTransition = .init(duration: 0.2, delay: 0)
+//            lineLayer. = .constant(4)
+            
+//            var lineLayer = LineLayer(id: layerName)
+//            lineLayer.source = layerName
+//            lineLayer.lineColor = .constant(StyleColor(.orange))
+//            lineLayer.lineOpacity = .constant(0.0)
+//            lineLayer.lineOpacityTransition = .init(duration: 0.2, delay: 0)
+//            lineLayer.lineWidth = .constant(4)
             try! mapView.mapboxMap.style.addSource(geoJSONSource, id: layerName)
             try! mapView.mapboxMap.style.addLayer(lineLayer)
             print("addLayer(lineLayer)", lineLayer)

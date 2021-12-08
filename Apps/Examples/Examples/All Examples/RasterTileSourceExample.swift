593b66b627c41f002bbbd32889e843a3858f66eb
Peng Liu <peng.liu@mapbox.com>
2021-12-08 14:02:48 +0200

    Update RasterTileSourceExample to use tileRequestsDelay property.

--- Apps/Examples/Examples/All Examples/RasterTileSourceExample.swift
+++ Apps/Examples/Examples/All Examples/RasterTileSourceExample.swift
@@ -1,8 +1,13 @@
 import MapboxMaps
+import UIKit
 
 @objc(RasterTileSourceExample)
 class RasterTileSourceExample: UIViewController, ExampleProtocol {
     var mapView: MapView!
+    var isTileRequestDelayEnabled = false
+
+    let button = UIButton(type: .system)
+    let sourceId = "raster-source"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,21 @@ class RasterTileSourceExample: UIViewController, ExampleProtocol {
         mapView.mapboxMap.onNext(.mapLoaded) { _ in
             self.addRasterSource()
         }
+
+        button.setTitle("Enable tile request delay", for: .normal)
+        button.backgroundColor = .white
+        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        NSLayoutConstraint.activate([button.centerXAnchor.constraint(equalTo: view.centerXAnchor), button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
+        button.addTarget(self, action: #selector(toggleTileRequestDelay), for: .touchUpInside)
+    }
+
+    @objc
+    func toggleTileRequestDelay() {
+        isTileRequestDelayEnabled.toggle()
+        try? mapView.mapboxMap.style.setSourceProperty(for: sourceId, property: "tile-requests-delay", value: isTileRequestDelayEnabled ? 5000 : 0)
+        button.setTitle(isTileRequestDelayEnabled ? "Disable tile request delay" : "Enable tile request delay", for: .normal)
     }
 
     func addRasterSource() {
@@ -39,7 +59,7 @@ class RasterTileSourceExample: UIViewController, ExampleProtocol {
 
         // Specify that the layer should use the source with the ID `raster-source`. This ID will be
         // assigned to the `RasterSource` when it is added to the style.
-        let sourceId = "raster-source"
+
         rasterLayer.source = sourceId
 
         do {

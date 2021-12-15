7787be80ce327c403b8072894b863b89f8a708be
Roman Gardukevich <roman.gardukevich@mapbox.com>
2021-12-15 18:15:36 +0300

    Release automation: open documentation pull requests (#917)
    * Open documetation pull requests
    
    * Codereview fixes

--- scripts/utilities/git.sh
+++ scripts/utilities/git.sh
@@ -6,3 +6,8 @@ git_recent_vtag() { git describe --tags --abbrev=0 "$(git_recent_vtag_hash)"; }
 git_recent_vtag_hash() { git rev-list --tags='v*' --max-count=1 ; }
 
 git_head_hash() { git rev-parse HEAD ; }
+
+git_configure_release_user() {
+    git config user.name "Release SDK bot"
+    git config user.email "release-bot@mapbox.com"
+}

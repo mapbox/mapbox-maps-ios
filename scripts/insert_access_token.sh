cb38a11265e31eb5fcc4f6169675105de63029ce
Andrew Hershberger <andrew.hershberger@mapbox.com>
2022-01-04 13:15:04 -0500

    Examples Bug Fixes (#974)
    * Fix layout and styling of Show/Hide Layer Example
    
    * Avoid missing token error hard-coded in plist
    
    Fixes #673

--- scripts/insert_access_token.sh
+++ scripts/insert_access_token.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+
 echo "Inserting Mapbox access token..."
 token_file=~/.mapbox
 token_file2=~/mapbox
 token="$(cat $token_file 2>/dev/null || cat $token_file2 2>/dev/null)"
+plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 if [ "$token" ]; then
-  plutil -replace MBXAccessToken -string $token "$TARGET_BUILD_DIR/$INFOPLIST_PATH"
+  plutil -replace MBXAccessToken -string "$token" "$plist"
   echo "Token insertion successful"
-else
+elif /usr/libexec/PlistBuddy -c "Print :MBXAccessToken" "$plist"; then
   echo \'error: Missing Mapbox access token\'
-  echo "error: Get an access token from <https://www.mapbox.com/studio/account/tokens/>, then create a new file at $token_file that contains the access token."
+  echo "error: Get an access token from <https://www.mapbox.com/studio/account/tokens/>, then create a new file at ~/.mapbox that contains the access token."
   exit 1
 fi

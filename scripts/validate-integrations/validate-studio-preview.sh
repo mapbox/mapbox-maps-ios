e28a92b1bc118a248703f351cb49a8a6fa0e5d7c
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-01-07 07:32:07 +0300

    Build Studio Preview for new release branch (#993)

---
+++ scripts/validate-integrations/validate-studio-preview.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+print_usage() {
+    cat <<HELP_USAGE
+Usage:
+        $0 branch_name
+HELP_USAGE
+}
+
+if [ $# != 1 ]; then
+    print_usage
+    exit 0
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+UTILS_PATH="$SCRIPT_DIR/../utils.sh"
+TMP_ROOT=$(mktemp -d)
+
+# shellcheck source=../utils.sh
+source "$UTILS_PATH"
+
+BRANCH_NAME=$1
+
+STUDIO_PREVIEW_DIR="$TMP_ROOT/studio-preview-ios"
+
+git clone "https://x-access-token:$(mbx-ci github reader private token)@github.com/mapbox/studio-preview-ios.git" \
+ "$STUDIO_PREVIEW_DIR" --depth=1 --quiet
+pushd "$STUDIO_PREVIEW_DIR" > /dev/null
+
+sed -i ''  -E "s/(pod 'MapboxMaps',).*/\1 :git => 'https:\/\/github.com\/mapbox\/mapbox-maps-ios.git', :branch => '${BRANCH_NAME//\//\\/}'/" Podfile
+bundle install
+bundle exec pod install
+
+time xcodebuild clean build -workspace StudioPreview.xcworkspace \
+           -scheme "StudioPreview" \
+           -destination "generic/platform=iOS" \
+           CODE_SIGNING_ALLOWED="NO"

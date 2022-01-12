a522b3861802fb39053480f24e43683cb792094e
Andrew Hershberger <andrew.hershberger@mapbox.com>
2022-01-12 01:05:15 -0600

    Revert changes to DebugApp

--- Apps/DebugApp/DebugApp/DebugViewController.swift
+++ Apps/DebugApp/DebugApp/DebugViewController.swift
@@ -1,6 +1,5 @@
 import UIKit
 import MapboxMaps
-import CoreLocation
 
 /**
  NOTE: This view controller should be used as a scratchpad
@@ -17,23 +16,5 @@ final class DebugViewController: UIViewController {
         mapView = MapView(frame: view.bounds)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(mapView)
-        mapView.location.options.puckType = .puck2D()
-
-        let followingState = mapView.viewport.makeFollowingViewportState(options: .init(bearing: .course))
-
-        let cupertino = CLLocationCoordinate2D(latitude: 37.3282643, longitude: -122.0733132)
-        let cupertinoPolygon = Polygon(center: cupertino, radius: 20000, vertices: 200)
-        let overviewState = mapView.viewport.makeOverviewViewportState(options: .init(geometry: cupertinoPolygon))
-
-        let immediateTransition = mapView.viewport.makeImmediateViewportTransition()
-        mapView.viewport.setTransition(immediateTransition, from: nil, to: followingState)
-
-        mapView.viewport.transition(to: followingState)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
-            self.mapView.viewport.transition(to: overviewState)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
-                self.mapView.viewport.transition(to: followingState)
-            }
-        }
     }
 }

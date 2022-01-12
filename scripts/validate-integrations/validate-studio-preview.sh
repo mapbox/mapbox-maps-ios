f108af88f9cfea084e3c540dc14d160bbb7a27f6
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-01-12 23:18:38 +0300

    Fix Studio preview validation script (#1013)

--- scripts/validate-integrations/validate-studio-preview.sh
+++ scripts/validate-integrations/validate-studio-preview.sh
@@ -23,16 +23,20 @@ source "$UTILS_PATH"
 BRANCH_NAME=$1
 
 STUDIO_PREVIEW_DIR="$TMP_ROOT/studio-preview-ios"
-
-git clone "https://x-access-token:$(mbx-ci github reader private token)@github.com/mapbox/studio-preview-ios.git" \
+step "Clone Studio Preview"
+git clone "https://x-access-token:$(mbx-ci github reader token)@github.com/mapbox/studio-preview-ios.git" \
  "$STUDIO_PREVIEW_DIR" --depth=1 --quiet
 pushd "$STUDIO_PREVIEW_DIR" > /dev/null
 
+step "Install dependencies"
 sed -i ''  -E "s/(pod 'MapboxMaps',).*/\1 :git => 'https:\/\/github.com\/mapbox\/mapbox-maps-ios.git', :branch => '${BRANCH_NAME//\//\\/}'/" Podfile
 bundle install
-bundle exec pod install
+bundle exec pod install --repo-update
 
-time xcodebuild clean build -workspace StudioPreview.xcworkspace \
+step "Build Studio Preview"
+xcodebuild clean build -workspace StudioPreview.xcworkspace \
            -scheme "StudioPreview" \
            -destination "generic/platform=iOS" \
            CODE_SIGNING_ALLOWED="NO"
+
+finish "Studio Preview builds successfully"

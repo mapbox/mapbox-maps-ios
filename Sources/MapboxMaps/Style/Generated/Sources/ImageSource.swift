fa4f4e47452b430a793b43a856b450a3d73dc6b9
Andrew Hershberger <andrew.hershberger@mapbox.com>
2022-02-09 14:33:40 -0600

    Improve indentation and docs in generated code

--- Sources/MapboxMaps/Style/Generated/Sources/ImageSource.swift
+++ Sources/MapboxMaps/Style/Generated/Sources/ImageSource.swift
@@ -1,34 +1,24 @@
 // This file is generated.
 import Foundation
 
-/**
- * An image data source.
- *
- * @see <a href="https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/#image">The online documentation</a>
- *
- */
+/// An image data source.
+///
+/// - SeeAlso: [Mapbox Style Specification](https://docs.mapbox.com/mapbox-gl-js/style-spec/sources/#image)
 public struct ImageSource: Source {
 
     public let type: SourceType
- 
-  
-    /** 
-     * URL that points to an image. 
-    */
+
+    /// URL that points to an image.
     public var url: String?
-      
-    /** 
-     * Corners of image specified in longitude, latitude pairs. 
-    */
+
+    /// Corners of image specified in longitude, latitude pairs.
     public var coordinates: [[Double]]?
-      
-    /** 
-     * When loading a map, if PrefetchZoomDelta is set to any number greater than 0, the map will first request a tile at zoom level lower than zoom - delta, but so that the zoom level is multiple of delta, in an attempt to display a full map at lower resolution as quick as possible. It will get clamped at the tile source minimum zoom. The default delta is 4. 
-    */
+
+    /// When loading a map, if PrefetchZoomDelta is set to any number greater than 0, the map will first request a tile at zoom level lower than zoom - delta, but so that the zoom level is multiple of delta, in an attempt to display a full map at lower resolution as quick as possible. It will get clamped at the tile source minimum zoom. The default delta is 4.
     public var prefetchZoomDelta: Double?
-     
+
     public init() {
-      self.type = .image
+        self.type = .image
     }
 }
 

0912bdc73f8b0e6e0688655839ec02f7c6bca72b
Andrew Hershberger <andrew.hershberger@mapbox.com>
2022-02-09 12:12:06 -0600

    Document Viewport API

--- Sources/MapboxMaps/Viewport/Viewport.swift
+++ Sources/MapboxMaps/Viewport/Viewport.swift
@@ -1,14 +1,14 @@
-// provides a structured approach to organizing
-// camera management logic into states and transitions between them
-//
-// at any given time, the viewport is either:
-//
-//  - idle (not updating the camera)
-//  - in a state (camera is being managed by a ViewportState)
-//  - transitioning (camera is being managed by a ViewportTransition)
-//
+/// `Viewport` provides a structured approach to organizing camera management logic into states and
+/// transitions between them.
+///
+/// At any given time, the viewport is either:
+///
+///  - idle (not updating the camera)
+///  - in a state (camera is being managed by a ``ViewportState``)
+///  - transitioning (camera is being managed by a ``ViewportTransition``)
 @_spi(Experimental) public final class Viewport {
 
+    /// Configuration options for adjusting the viewport's behavior.
     public var options: ViewportOptions {
         get { impl.options }
         set { impl.options = newValue }
@@ -29,53 +29,91 @@
         self.mapboxMap = mapboxMap
     }
 
-    // MARK: - Current State
-
-    // defaults to .idle
+    /// The current ``ViewportStatus``.
+    ///
+    /// `status` cannot be set directly. Use
+    /// ``Viewport/transition(to:transition:completion:)`` and ``Viewport/idle()`` to
+    /// transition to a state or to idle.
+    ///
+    /// Defaults to ``ViewportStatus/idle``.
+    ///
+    /// - SeeAlso:
+    ///   - ``Viewport/addStatusObserver(_:)``
+    ///   - ``Viewport/removeStatusObserver(_:)``
     public var status: ViewportStatus {
         impl.status
     }
 
+    /// Subscribes a ``ViewportStatusObserver`` to ``Viewport/status`` changes.
+    ///
+    /// Viewport keeps a strong reference to registered observers. Adding the same observer again while it is already subscribed has no effect.
+    ///
+    /// - Note: Observers are notified of status changes asynchronously on the main queue. This means that by
+    /// the time the notification is delivered, the status may have already changed again. This behavior is necessary to allow
+    /// observers to trigger further transitions while avoiding out-of-order delivery of status changed notifications.
+    /// - Parameter observer: An object that will be notified when the ``Viewport/status`` changes.
+    /// - SeeAlso: ``Viewport/removeStatusObserver(_:)``
     public func addStatusObserver(_ observer: ViewportStatusObserver) {
         impl.addStatusObserver(observer)
     }
 
+    /// Unsubscribes a ``ViewportStatusObserver`` from ``Viewport/status`` changes. This causes viewport
+    /// to release its strong reference to the observer. Removing an observer that is not subscribed has no effect.
+    ///
+    /// - Parameter observer: An object that should no longer be notified when the ``Viewport/status`` changes.
+    /// - SeeAlso: ``Viewport/addStatusObserver(_:)``
     public func removeStatusObserver(_ observer: ViewportStatusObserver) {
         impl.removeStatusObserver(observer)
     }
 
+    /// Sets ``Viewport/status`` to ``ViewportStatus/idle`` synchronously.
+    ///
+    /// This cancels any active ``ViewportState`` or ``ViewportTransition``.
     public func idle() {
         impl.idle()
     }
 
-    // the Bool in the completion block indicates whether the transition ran to
-    // completion (true) or was interrupted in some way (false). if the source
-    // of the interruption was because transition(to:completion:) or idle() was
-    // invoked, the next status is determined by those interrupting calls. if
-    // the source of the interruption was external (e.g. the ViewportTransition
-    // failed for some reason), the status will be set to .idle.
-    //
-    // transitioning to state x when status equals .state(x) just
-    // invokes completion synchronously with `true` and does not modify status
-    //
-    // transitioning to state x when status equals .transition(_, _, x) just
-    // invokes completion synchronously with `false` and does not modify status
+    /// Executes a transition to the requested state.
+    ///
+    /// If the transition fails, ``Viewport/status`` is set to ``ViewportStatus/idle``.
+    ///
+    /// Transitioning to state `x` when the status is `.state(x)` invokes `completion`
+    /// synchronously with `true` and does not modify ``Viewport/status``.
+    ///
+    /// Transitioning to state `x` when the status is `.transition(_, x)` invokes `completion`
+    /// synchronously with `false` and does not modify ``Viewport/status``.
+    ///
+    /// `Viewport` keeps a strong reference to active transitions and states. To reuse states and transitions,
+    /// keep strong references to them in the consuming project.
+    ///
+    /// - Parameters:
+    ///   - toState: The target ``ViewportState`` to transition to.
+    ///   - transition: The ``ViewportTransition`` that is used to transition to the target state.
+    ///                 If `nil`, ``Viewport/defaultTransition`` is used. Defaults to `nil`.
+    ///   - completion: A closure that is invoked when the transition ends. Defaults to `nil`.
+    ///   - success: Whether the transition ran to completion. Transitions may end early if they fail or
+    ///              are interrupted (e.g. by another call to
+    ///              `transition(to:transition:completion:)` or ``Viewport/idle()``)
     public func transition(to toState: ViewportState,
                            transition: ViewportTransition? = nil,
-                           completion: ((Bool) -> Void)? = nil) {
+                           completion: ((_ success: Bool) -> Void)? = nil) {
         impl.transition(to: toState, transition: transition, completion: completion)
     }
 
-    // MARK: - Transitions
-
-    // this transition is used unless overridden by one of the registered transitions
+    /// ``Viewport/transition(to:transition:completion:)`` uses this transition unless
+    /// some non-nil value is passed to its `transition` argument.
+    ///
+    /// Defaults to ``DefaultViewportTransition`` with default options.
     public var defaultTransition: ViewportTransition {
         get { impl.defaultTransition }
         set { impl.defaultTransition = newValue }
     }
 
-    // factory methods
-
+    /// Creates a new instance of ``FollowPuckViewportState`` with the specified options.
+    /// - Parameter options: configuration options used when creating ``FollowPuckViewportState``. Defaults to
+    ///                      ``FollowPuckViewportStateOptions/init(padding:zoom:bearing:pitch:animationDuration:)``
+    ///                      with the default value specified for all parameters.
+    /// - Returns: The newly-created ``FollowPuckViewportState``.
     public func makeFollowPuckViewportState(options: FollowPuckViewportStateOptions = .init()) -> FollowPuckViewportState {
         return FollowPuckViewportState(
             dataSource: FollowPuckViewportStateDataSource(
@@ -86,6 +124,9 @@
             mapboxMap: mapboxMap)
     }
 
+    /// Creates a new instance of ``OverviewViewportState`` with the specified options.
+    /// - Parameter options: configuration options used when creating ``OverviewViewportState``.
+    /// - Returns: The newly-created ``OverviewViewportState``.
     public func makeOverviewViewportState(options: OverviewViewportStateOptions) -> OverviewViewportState {
         return OverviewViewportState(
             options: options,
@@ -94,6 +135,10 @@
             observableCameraOptions: ObservableCameraOptions())
     }
 
+    /// Creates a new instance of ``DefaultViewportTransition``.
+    /// - Parameter options: configuration options used when creating ``DefaultViewportTransition``. Defaults to
+    ///                      ``DefaultViewportTransitionOptions/init(maxDuration:)`` with the default value specified for all parameters
+    /// - Returns: The newly-created ``DefaultViewportTransition``.
     public func makeDefaultViewportTransition(options: DefaultViewportTransitionOptions = .init()) -> DefaultViewportTransition {
         return DefaultViewportTransition(
             options: options,
@@ -102,6 +147,8 @@
                 cameraAnimationsManager: cameraAnimationsManager))
     }
 
+    /// Creates a new instance of ``ImmediateViewportTransition``.
+    /// - Returns: The newly-created ``ImmediateViewportTransition``.
     public func makeImmediateViewportTransition() -> ImmediateViewportTransition {
         return ImmediateViewportTransition(mapboxMap: mapboxMap)
     }

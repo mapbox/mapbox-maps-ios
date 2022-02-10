c95b14f16c9462f1a1241245ad6abd4d51d3b4f3
Andrew Hershberger <andrew.hershberger@mapbox.com>
2022-02-10 07:01:41 -0600

    Improve indentation and docs in generated code (#1105)

--- Tests/MapboxMapsTests/Style/Generated/Layers/HillshadeLayerTests.swift
+++ Tests/MapboxMapsTests/Style/Generated/Layers/HillshadeLayerTests.swift
@@ -6,81 +6,79 @@ final class HillshadeLayerTests: XCTestCase {
 
     func testLayerProtocolMembers() {
 
-       var layer = HillshadeLayer(id: "test-id")
-       layer.source = "some-source"
-       layer.sourceLayer = nil
-       layer.minZoom = 10.0
-       layer.maxZoom = 20.0
+        var layer = HillshadeLayer(id: "test-id")
+        layer.source = "some-source"
+        layer.sourceLayer = nil
+        layer.minZoom = 10.0
+        layer.maxZoom = 20.0
 
-       XCTAssert(layer.id == "test-id")
-       XCTAssert(layer.type == LayerType.hillshade)
-       XCTAssert(layer.filter == nil)
-       XCTAssert(layer.source == "some-source")
-       XCTAssertNil(layer.sourceLayer)
-       XCTAssert(layer.minZoom == 10.0)
-       XCTAssert(layer.maxZoom == 20.0)
+        XCTAssert(layer.id == "test-id")
+        XCTAssert(layer.type == LayerType.hillshade)
+        XCTAssert(layer.filter == nil)
+        XCTAssert(layer.source == "some-source")
+        XCTAssertNil(layer.sourceLayer)
+        XCTAssert(layer.minZoom == 10.0)
+        XCTAssert(layer.maxZoom == 20.0)
     }
 
     func testEncodingAndDecodingOfLayerProtocolProperties() {
-       var layer = HillshadeLayer(id: "test-id")
-       layer.source = "some-source"
-       layer.sourceLayer = nil
-       layer.minZoom = 10.0
-       layer.maxZoom = 20.0
+        var layer = HillshadeLayer(id: "test-id")
+        layer.source = "some-source"
+        layer.sourceLayer = nil
+        layer.minZoom = 10.0
+        layer.maxZoom = 20.0
 
-       var data: Data?
-       do {
-       	   data = try JSONEncoder().encode(layer)
-       } catch {
-           XCTFail("Failed to encode HillshadeLayer")
-       }
+        var data: Data?
+        do {
+            data = try JSONEncoder().encode(layer)
+        } catch {
+            XCTFail("Failed to encode HillshadeLayer")
+        }
 
-       guard let validData = data else {
-           XCTFail("Failed to encode HillshadeLayer")
-           return
-       }
+        guard let validData = data else {
+            XCTFail("Failed to encode HillshadeLayer")
+            return
+        }
 
-       do {
-           let decodedLayer = try JSONDecoder().decode(HillshadeLayer.self, from: validData)
-           XCTAssert(decodedLayer.id == "test-id")
-       	   XCTAssert(decodedLayer.type == LayerType.hillshade)
-           XCTAssert(decodedLayer.filter == nil)
-           XCTAssert(decodedLayer.source == "some-source")
-           XCTAssertNil(decodedLayer.sourceLayer)
-           XCTAssert(decodedLayer.minZoom == 10.0)
-           XCTAssert(decodedLayer.maxZoom == 20.0)
-       } catch {
-           XCTFail("Failed to decode HillshadeLayer")
-       }
+        do {
+            let decodedLayer = try JSONDecoder().decode(HillshadeLayer.self, from: validData)
+            XCTAssert(decodedLayer.id == "test-id")
+            XCTAssert(decodedLayer.type == LayerType.hillshade)
+            XCTAssert(decodedLayer.filter == nil)
+            XCTAssert(decodedLayer.source == "some-source")
+            XCTAssertNil(decodedLayer.sourceLayer)
+            XCTAssert(decodedLayer.minZoom == 10.0)
+            XCTAssert(decodedLayer.maxZoom == 20.0)
+        } catch {
+            XCTFail("Failed to decode HillshadeLayer")
+        }
     }
 
     func testEncodingAndDecodingOfLayoutProperties() {
+        var layer = HillshadeLayer(id: "test-id")
+        layer.visibility = .constant(.visible)
 
-       var layer = HillshadeLayer(id: "test-id")
-       layer.visibility = .constant(.visible)
+        var data: Data?
+        do {
+            data = try JSONEncoder().encode(layer)
+        } catch {
+            XCTFail("Failed to encode HillshadeLayer")
+        }
 
-       var data: Data?
-       do {
-       	   data = try JSONEncoder().encode(layer)
-       } catch {
-           XCTFail("Failed to encode HillshadeLayer")
-       }
+        guard let validData = data else {
+            XCTFail("Failed to encode HillshadeLayer")
+            return
+        }
 
-       guard let validData = data else {
-           XCTFail("Failed to encode HillshadeLayer")
-           return
-       }
-
-       do {
-           let decodedLayer = try JSONDecoder().decode(HillshadeLayer.self, from: validData)
-           XCTAssert(decodedLayer.visibility == .constant(.visible))
-       } catch {
-           XCTFail("Failed to decode HillshadeLayer")
-       }
+        do {
+            let decodedLayer = try JSONDecoder().decode(HillshadeLayer.self, from: validData)
+            XCTAssert(decodedLayer.visibility == .constant(.visible))
+        } catch {
+            XCTFail("Failed to decode HillshadeLayer")
+        }
     }
 
     func testEncodingAndDecodingOfPaintProperties() {
-
        var layer = HillshadeLayer(id: "test-id")
        layer.hillshadeAccentColor = Value<StyleColor>.testConstantValue()
        layer.hillshadeAccentColorTransition = StyleTransition(duration: 10.0, delay: 10.0)
@@ -95,7 +93,7 @@ final class HillshadeLayerTests: XCTestCase {
 
        var data: Data?
        do {
-       	   data = try JSONEncoder().encode(layer)
+           data = try JSONEncoder().encode(layer)
        } catch {
            XCTFail("Failed to encode HillshadeLayer")
        }
@@ -108,12 +106,12 @@ final class HillshadeLayerTests: XCTestCase {
        do {
            let decodedLayer = try JSONDecoder().decode(HillshadeLayer.self, from: validData)
            XCTAssert(decodedLayer.visibility == .constant(.visible))
-       	   XCTAssert(layer.hillshadeAccentColor == Value<StyleColor>.testConstantValue())
-       	   XCTAssert(layer.hillshadeExaggeration == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.hillshadeHighlightColor == Value<StyleColor>.testConstantValue())
-       	   XCTAssert(layer.hillshadeIlluminationAnchor == Value<HillshadeIlluminationAnchor>.testConstantValue())
-       	   XCTAssert(layer.hillshadeIlluminationDirection == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.hillshadeShadowColor == Value<StyleColor>.testConstantValue())
+           XCTAssert(layer.hillshadeAccentColor == Value<StyleColor>.testConstantValue())
+           XCTAssert(layer.hillshadeExaggeration == Value<Double>.testConstantValue())
+           XCTAssert(layer.hillshadeHighlightColor == Value<StyleColor>.testConstantValue())
+           XCTAssert(layer.hillshadeIlluminationAnchor == Value<HillshadeIlluminationAnchor>.testConstantValue())
+           XCTAssert(layer.hillshadeIlluminationDirection == Value<Double>.testConstantValue())
+           XCTAssert(layer.hillshadeShadowColor == Value<StyleColor>.testConstantValue())
        } catch {
            XCTFail("Failed to decode HillshadeLayer")
        }
--- Tests/MapboxMapsTests/Style/Generated/Layers/LineLayerTests.swift
+++ Tests/MapboxMapsTests/Style/Generated/Layers/LineLayerTests.swift
@@ -6,91 +6,89 @@ final class LineLayerTests: XCTestCase {
 
     func testLayerProtocolMembers() {
 
-       var layer = LineLayer(id: "test-id")
-       layer.source = "some-source"
-       layer.sourceLayer = nil
-       layer.minZoom = 10.0
-       layer.maxZoom = 20.0
+        var layer = LineLayer(id: "test-id")
+        layer.source = "some-source"
+        layer.sourceLayer = nil
+        layer.minZoom = 10.0
+        layer.maxZoom = 20.0
 
-       XCTAssert(layer.id == "test-id")
-       XCTAssert(layer.type == LayerType.line)
-       XCTAssert(layer.filter == nil)
-       XCTAssert(layer.source == "some-source")
-       XCTAssertNil(layer.sourceLayer)
-       XCTAssert(layer.minZoom == 10.0)
-       XCTAssert(layer.maxZoom == 20.0)
+        XCTAssert(layer.id == "test-id")
+        XCTAssert(layer.type == LayerType.line)
+        XCTAssert(layer.filter == nil)
+        XCTAssert(layer.source == "some-source")
+        XCTAssertNil(layer.sourceLayer)
+        XCTAssert(layer.minZoom == 10.0)
+        XCTAssert(layer.maxZoom == 20.0)
     }
 
     func testEncodingAndDecodingOfLayerProtocolProperties() {
-       var layer = LineLayer(id: "test-id")
-       layer.source = "some-source"
-       layer.sourceLayer = nil
-       layer.minZoom = 10.0
-       layer.maxZoom = 20.0
+        var layer = LineLayer(id: "test-id")
+        layer.source = "some-source"
+        layer.sourceLayer = nil
+        layer.minZoom = 10.0
+        layer.maxZoom = 20.0
 
-       var data: Data?
-       do {
-       	   data = try JSONEncoder().encode(layer)
-       } catch {
-           XCTFail("Failed to encode LineLayer")
-       }
+        var data: Data?
+        do {
+            data = try JSONEncoder().encode(layer)
+        } catch {
+            XCTFail("Failed to encode LineLayer")
+        }
 
-       guard let validData = data else {
-           XCTFail("Failed to encode LineLayer")
-           return
-       }
+        guard let validData = data else {
+            XCTFail("Failed to encode LineLayer")
+            return
+        }
 
-       do {
-           let decodedLayer = try JSONDecoder().decode(LineLayer.self, from: validData)
-           XCTAssert(decodedLayer.id == "test-id")
-       	   XCTAssert(decodedLayer.type == LayerType.line)
-           XCTAssert(decodedLayer.filter == nil)
-           XCTAssert(decodedLayer.source == "some-source")
-           XCTAssertNil(decodedLayer.sourceLayer)
-           XCTAssert(decodedLayer.minZoom == 10.0)
-           XCTAssert(decodedLayer.maxZoom == 20.0)
-       } catch {
-           XCTFail("Failed to decode LineLayer")
-       }
+        do {
+            let decodedLayer = try JSONDecoder().decode(LineLayer.self, from: validData)
+            XCTAssert(decodedLayer.id == "test-id")
+            XCTAssert(decodedLayer.type == LayerType.line)
+            XCTAssert(decodedLayer.filter == nil)
+            XCTAssert(decodedLayer.source == "some-source")
+            XCTAssertNil(decodedLayer.sourceLayer)
+            XCTAssert(decodedLayer.minZoom == 10.0)
+            XCTAssert(decodedLayer.maxZoom == 20.0)
+        } catch {
+            XCTFail("Failed to decode LineLayer")
+        }
     }
 
     func testEncodingAndDecodingOfLayoutProperties() {
+        var layer = LineLayer(id: "test-id")
+        layer.visibility = .constant(.visible)
+        layer.lineCap = Value<LineCap>.testConstantValue()
+        layer.lineJoin = Value<LineJoin>.testConstantValue()
+        layer.lineMiterLimit = Value<Double>.testConstantValue()
+        layer.lineRoundLimit = Value<Double>.testConstantValue()
+        layer.lineSortKey = Value<Double>.testConstantValue()
 
-       var layer = LineLayer(id: "test-id")
-       layer.visibility = .constant(.visible)
-       layer.lineCap = Value<LineCap>.testConstantValue()
-       layer.lineJoin = Value<LineJoin>.testConstantValue()
-       layer.lineMiterLimit = Value<Double>.testConstantValue()
-       layer.lineRoundLimit = Value<Double>.testConstantValue()
-       layer.lineSortKey = Value<Double>.testConstantValue()
+        var data: Data?
+        do {
+            data = try JSONEncoder().encode(layer)
+        } catch {
+            XCTFail("Failed to encode LineLayer")
+        }
 
-       var data: Data?
-       do {
-       	   data = try JSONEncoder().encode(layer)
-       } catch {
-           XCTFail("Failed to encode LineLayer")
-       }
+        guard let validData = data else {
+            XCTFail("Failed to encode LineLayer")
+            return
+        }
 
-       guard let validData = data else {
-           XCTFail("Failed to encode LineLayer")
-           return
-       }
-
-       do {
-           let decodedLayer = try JSONDecoder().decode(LineLayer.self, from: validData)
-           XCTAssert(decodedLayer.visibility == .constant(.visible))
-       	   XCTAssert(layer.lineCap == Value<LineCap>.testConstantValue())
-       	   XCTAssert(layer.lineJoin == Value<LineJoin>.testConstantValue())
-       	   XCTAssert(layer.lineMiterLimit == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.lineRoundLimit == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.lineSortKey == Value<Double>.testConstantValue())
-       } catch {
-           XCTFail("Failed to decode LineLayer")
-       }
+        do {
+            let decodedLayer = try JSONDecoder().decode(LineLayer.self, from: validData)
+            XCTAssert(decodedLayer.visibility == .constant(.visible))
+            XCTAssert(layer.lineCap == Value<LineCap>.testConstantValue())
+            XCTAssert(layer.lineJoin == Value<LineJoin>.testConstantValue())
+            XCTAssert(layer.lineMiterLimit == Value<Double>.testConstantValue())
+            XCTAssert(layer.lineRoundLimit == Value<Double>.testConstantValue())
+            XCTAssert(layer.lineSortKey == Value<Double>.testConstantValue())
+        } catch {
+            XCTFail("Failed to decode LineLayer")
+        }
     }
 
     func testEncodingAndDecodingOfPaintProperties() {
-
        var layer = LineLayer(id: "test-id")
        layer.lineBlur = Value<Double>.testConstantValue()
        layer.lineBlurTransition = StyleTransition(duration: 10.0, delay: 10.0)
@@ -115,7 +113,7 @@ final class LineLayerTests: XCTestCase {
 
        var data: Data?
        do {
-       	   data = try JSONEncoder().encode(layer)
+           data = try JSONEncoder().encode(layer)
        } catch {
            XCTFail("Failed to encode LineLayer")
        }
@@ -128,17 +126,17 @@ final class LineLayerTests: XCTestCase {
        do {
            let decodedLayer = try JSONDecoder().decode(LineLayer.self, from: validData)
            XCTAssert(decodedLayer.visibility == .constant(.visible))
-       	   XCTAssert(layer.lineBlur == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.lineColor == Value<StyleColor>.testConstantValue())
-       	   XCTAssert(layer.lineDasharray == Value<[Double]>.testConstantValue())
-       	   XCTAssert(layer.lineGapWidth == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.lineGradient == Value<StyleColor>.testConstantValue())
-       	   XCTAssert(layer.lineOffset == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.lineOpacity == Value<Double>.testConstantValue())
-       	   XCTAssert(layer.linePattern == Value<ResolvedImage>.testConstantValue())
-       	   XCTAssert(layer.lineTranslate == Value<[Double]>.testConstantValue())
-       	   XCTAssert(layer.lineTranslateAnchor == Value<LineTranslateAnchor>.testConstantValue())
-       	   XCTAssert(layer.lineWidth == Value<Double>.testConstantValue())
+           XCTAssert(layer.lineBlur == Value<Double>.testConstantValue())
+           XCTAssert(layer.lineColor == Value<StyleColor>.testConstantValue())
+           XCTAssert(layer.lineDasharray == Value<[Double]>.testConstantValue())
+           XCTAssert(layer.lineGapWidth == Value<Double>.testConstantValue())
+           XCTAssert(layer.lineGradient == Value<StyleColor>.testConstantValue())
+           XCTAssert(layer.lineOffset == Value<Double>.testConstantValue())
+           XCTAssert(layer.lineOpacity == Value<Double>.testConstantValue())
+           XCTAssert(layer.linePattern == Value<ResolvedImage>.testConstantValue())
+           XCTAssert(layer.lineTranslate == Value<[Double]>.testConstantValue())
+           XCTAssert(layer.lineTranslateAnchor == Value<LineTranslateAnchor>.testConstantValue())
+           XCTAssert(layer.lineWidth == Value<Double>.testConstantValue())
        } catch {
            XCTFail("Failed to decode LineLayer")
        }

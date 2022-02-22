10a96c07d1a304d2cea417d479abc04ec45a7750
Zizasaurus <zmiles@gwu.edu>
2022-02-22 17:47:29 -0500

    reworked example and added location and symbol layer accessibility elements

--- Apps/Examples/Examples/All Examples/VoiceOverAccessibilityExample.swift
+++ Apps/Examples/Examples/All Examples/VoiceOverAccessibilityExample.swift
@@ -1,153 +1,224 @@
 import UIKit
+import CoreLocation
 import MapboxMaps
 
-@objc(VoiceOverAccessibilityExample)
-public class VoiceOverAccessibilityExample: UIViewController, ExampleProtocol {
+struct MyData {
+    var id: Int
+    var coordinate: CLLocationCoordinate2D
+    var name: String
+}
 
-    var mapView: MapView!
+@objc(VoiceOverAccessibilityExample)
+class VoiceOverAccessibilityExample: UIViewController, ExampleProtocol {
+
+    let data: [MyData] = [
+        MyData(id: 0, coordinate: CLLocationCoordinate2D(latitude: 40.727405, longitude: -73.981926), name: "Tomkins Square Park"),
+        MyData(id: 1, coordinate: CLLocationCoordinate2D(latitude: 40.7308963, longitude: -73.998694), name: "Washington Square Park"),
+        MyData(id: 2, coordinate: CLLocationCoordinate2D(latitude: 40.715225, longitude: -74.000086), name: "Columbus Park"),
+        MyData(id: 3, coordinate: CLLocationCoordinate2D(latitude: 40.692813, longitude: -73.976161), name: "Fort Greene Park")
+    ]
+
+    var mapView: MapViewView!
+    var markerAccessibilityElements: [Int: UIAccessibilityElement] = [:]
+    var routeShields: [UIAccessibilityElement] = []
     var pointAnnotation: PointAnnotation?
     var pointAnnotationManager: PointAnnotationManager?
-    var pointAnnotationView: UIView! = nil
-    var pointAnnotationViews = [UIView]()
-    var i = 0
+    var visibleAnnotationArray: [PointAnnotation] = []
 
-    let coordinates = [
-        CLLocationCoordinate2D(latitude: 35.67514743608467, longitude: -86.220703125),
-        CLLocationCoordinate2D(latitude: 33.65120829920497, longitude: -80.771484375),
-        CLLocationCoordinate2D(latitude: 37.43997405227057, longitude: -78.662109375)
-    ]
-
-    let annotationProperties = [
-        ["Name": "Alabama"],
-        ["Name" : "South Carolina"],
-        ["Name" : "Virginia"]
-    ]
-
-    override public func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Set the center coordinate and zoom level.
-        let centerCoordinate = CLLocationCoordinate2D(latitude: 33.65120829920497, longitude: -80.771484375)
-        let options = MapInitOptions(cameraOptions: CameraOptions(center: centerCoordinate, zoom: 4), styleURI: .light)
-
-        mapView = MapView(frame: view.bounds, mapInitOptions: options)
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 40.7131854, longitude: -74.0165265)
+        let options = MapInitOptions(cameraOptions: CameraOptions(center: centerCoordinate, zoom: 10))
+        mapView = MapViewView(frame: view.frame, mapInitOptions: options)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        view.addSubview(mapView)
-        // add double tap gesture to map
-        self.addDoubleTapGesture(to: self.mapView)
+        mapView.isAccessibilityElement = true
 
-        // Allow the view controller to receive information about map events.
+        view.addSubview(mapView)
+
+        pointAnnotationManager =  mapView.annotations.makePointAnnotationManager(id: "annotation-manager")
+
         mapView.mapboxMap.onNext(.mapLoaded) { _ in
-            self.addAnnotations()
-            self.addAccesibility()
-            self.finish()
+            self.setUserLocation()
+            self.calculateVisibleAnnotations()
+            self.queryRenderedHighwayShields()
         }
 
         mapView.mapboxMap.onEvery(.cameraChanged) { _ in
-            if self.pointAnnotationManager!.annotations.isEmpty {
-                self.addAnnotations()
-                self.addAccesibility()
-            } else {
-                self.i = 0
-                for view in self.pointAnnotationViews {
-                    view.removeFromSuperview()
-                }
-                self.pointAnnotationManager?.annotations = []
-                self.mapView.annotations.removeAnnotationManager(withId: "annotation-manager")
+            for datum in self.data {
+                let element = self.markerAccessibilityElements[datum.id]
+                element?.accessibilityFrame = self.mapView.rect(for: datum.coordinate)
             }
+            self.queryRenderedHighwayShields()
         }
+        
+        mapView.coordinates = data.map(\.coordinate)
 
+        // create UIAccessibilityElements from data
+        for datum in data {
+            let element = UIAccessibilityElement(accessibilityContainer: view!)
+            element.accessibilityIdentifier = datum.id.description
+            element.accessibilityFrame = mapView.rect(for: datum.coordinate)
+            element.accessibilityLabel = datum.name
+            markerAccessibilityElements[datum.id] = element
 
-        if !UIAccessibility.isVoiceOverRunning {
-            let label = UILabel(frame: CGRect(x: view.frame.origin.x, y: view.frame.origin.y + 100, width: UIScreen.main.bounds.width, height: 50))
-            label.backgroundColor = .gray
-            label.textColor = .black
-            label.text = "Turn on VoiceOver to interact with the mapview annotations."
-            label.textAlignment = .center
-            label.lineBreakMode = .byWordWrapping
-            label.numberOfLines = 0
-            view.addSubview(label)
-        }
-    }
-
-    public func addDoubleTapGesture(to mapView: MapView) {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
-        tap.numberOfTapsRequired = 2
-        view.addGestureRecognizer(tap)
-    }
-
-    @objc func doubleTapped() {
-
-        if UIAccessibility.isVoiceOverRunning {
-            let focusedElement = UIAccessibility.focusedElement(using: UIAccessibility.AssistiveTechnologyIdentifier.notificationVoiceOver)! as! UIView
-            let index = focusedElement.tag
-            if let image = UIImage(named: "yellow_star") {
-                self.pointAnnotationManager!.annotations[index].image = .init(image: image, name: "yellow_star")
-                // unable to get voiceover for doubletapped element
-                //focusedElement.accessibilityLabel = "You've starred \(self.pointAnnotationManager!.annotations[index].userInfo?.first?.value)"
-            }
-        }
-    }
-
-    private func addAnnotations() {
-        pointAnnotationManager =  mapView.annotations.makePointAnnotationManager(id: "annotation-manager", layerPosition: .default)
-
-        for coordinate in coordinates {
-            let point = mapView.mapboxMap.point(for: coordinate)
-            pointAnnotation = PointAnnotation(id: "annotation" , coordinate: coordinate)
-            pointAnnotation?.userInfo = annotationProperties[i]
-
-            if let image = UIImage(named: "custom_marker") {
-                pointAnnotation?.image = .init(image: image, name: "custom_marker")
-            }
-
+            pointAnnotation = PointAnnotation(id: datum.name , coordinate: datum.coordinate)
+            pointAnnotation!.image = .init(image: UIImage(named: "custom_marker")!, name: "custom_marker")
             pointAnnotationManager?.annotations.append(pointAnnotation!)
-            pointAnnotationView = UIView(frame: CGRect(x: point.x - 20, y: point.y - 20, width: 40, height: 45))
-            pointAnnotationView?.layer.borderColor = UIColor.clear.cgColor
-            pointAnnotationView.layer.cornerRadius = 10
-            pointAnnotationView.layer.backgroundColor = UIColor.clear.cgColor
-            pointAnnotationView.layer.borderWidth = 2
-            pointAnnotationView.accessibilityFrame = self.pointAnnotationView.frame
-            pointAnnotationView.isAccessibilityElement = true
-            pointAnnotationView.accessibilityElements = [pointAnnotation]
-            pointAnnotationView.accessibilityLabel = "\(pointAnnotation!.userInfo!.first!.value) selected."
-
-            if i == 0 {
-                pointAnnotationView.tag = 0
-            } else {
-                pointAnnotationView.tag = i
-            }
-            //pointAnnotationView.accessibilityHint = " Double tap to change the icon color."
-            pointAnnotationView.accessibilityTraits = .allowsDirectInteraction
-
-            view.addSubview(pointAnnotationView)
-            pointAnnotationViews.append(pointAnnotationView)
-            i = i + 1
         }
+
+        // add shield layer elements to the array
+        view.accessibilityElements = [Array(self.markerAccessibilityElements.values), self.mapView]
+        calculateVisibleAnnotations()
     }
 
-    private func addAccesibility() {
-        let cameraView = mapView.mapboxMap.cameraBounds.bounds.northeast
-        let origin = mapView.mapboxMap.point(for: cameraView)
-        let mapviewAccessibilityFrame = CGRect(origin: origin, size: CGSize(width: UIScreen.main.bounds.width / 0.8, height: UIScreen.main.bounds.height / 0.9))
+    func setUserLocation() {
+        // set custom location to New York City
+        let customLocationProvider = CustomLocationProvider(currentLocation: CLLocation(latitude: 40.7131854, longitude: -74.0165265))
+        mapView.location.overrideLocationProvider(with: customLocationProvider)
+        mapView.location.options.puckType = .puck2D()
+        mapView.location.options.puckBearingSource = .course
+        mapView.location.isAccessibilityElement = true
+        mapView.location.accessibilityLabel = "Current location"
+        mapView.location.accessibilityFrame = self.mapView.rect(for: (self.mapView.location.latestLocation?.location.coordinate)!)
+        view.accessibilityElements?.append(self.mapView.location!)
+    }
 
-        var visibleAnnotationsInView = [PointAnnotation]()
+    func queryRenderedHighwayShields() {
+        routeShields = []
+
+        mapView.mapboxMap.queryRenderedFeatures(
+            in: mapView.bounds,
+            options: RenderedQueryOptions(layerIds: ["road-number-shield"], filter: nil)) { [weak self] result in
+                switch result {
+                case .success(let queriedfeatures):
+                    for queriedFeature in queriedfeatures {
+
+                        let shield = queriedFeature.feature.properties!["shield"]!!.rawValue as! String
+                        let shieldNumber = queriedFeature.feature.properties!["ref"]!!.rawValue as! String
+                        let geometry = queriedFeature.feature.geometry
+
+                        switch geometry {
+                        case .point(let point):
+                            // create the UIAccessibility element for each route
+                            // shield in the map view.
+                             let element = UIAccessibilityElement(accessibilityContainer: self?.mapView.mapboxMap.style)
+                            element.accessibilityIdentifier = queriedFeature.feature.identifier.debugDescription
+                            element.accessibilityLabel = "\(shield) \(shieldNumber)"
+                            element.accessibilityFrame = (self?.mapView.rect(for: point.coordinates))!
+                            self?.view.accessibilityElements?.append(element)
+
+                        case .geometryCollection(_):
+                            break
+                        case .lineString(_):
+                            break
+                        case .multiLineString(_):
+                            break
+                        case .multiPoint(_):
+                            break
+                        case .polygon(_):
+                            break
+                        case .multiPolygon(_):
+                            break
+                        case .none:
+                            break
+                        }
+                    }
+
+                case .failure(let error):
+                    print("Error:", error)
+                }
+            }
+    }
+
+    func calculateVisibleAnnotations() {
+        visibleAnnotationArray = []
         let cameraOptions = CameraOptions(cameraState: mapView.cameraState)
         for annotation in pointAnnotationManager!.annotations {
             if mapView.mapboxMap.coordinateBounds(for: cameraOptions).contains(forPoint: annotation.point.coordinates, wrappedCoordinates: true) {
-                visibleAnnotationsInView.append(annotation)
+                visibleAnnotationArray.append(annotation)
             }
         }
-
-        mapView.accessibilityFrame = mapviewAccessibilityFrame
-        if visibleAnnotationsInView.count > 1 {
-            mapView.accessibilityLabel = "Map view selected. There are \(visibleAnnotationsInView.count) visible annotations: \(visibleAnnotationsInView.map {$0.userInfo!.first!.value})."
-        } else {
-            mapView.accessibilityLabel = "Map view selected. There is \(visibleAnnotationsInView.count) visible annotation: \(visibleAnnotationsInView.first!.userInfo!.first!.value)."
+        if visibleAnnotationArray.count > 1 {
+            mapView.accessibilityLabel = "Map view selected. There are \(visibleAnnotationArray.count) visible point annotations: \(visibleAnnotationArray.map {$0.id}). There are also other accessibility elements, including a location component and interstate route shields"
+        } else if visibleAnnotationArray.count == 1 {
+            mapView.accessibilityLabel = "Map view selected. There is \(visibleAnnotationArray.count) visible annotation: \(visibleAnnotationArray.first?.id)."
+        } else if visibleAnnotationArray.count == 0 {
+            mapView.accessibilityLabel = "Map view selected. There are no visible annotations."
         }
-        mapView.isAccessibilityElement = true
-        mapView.accessibilityElements = [pointAnnotationViews]
-
     }
 }
 
+class MapViewView: MapView {
+    var coordinates: [CLLocationCoordinate2D] = []
+
+    func point(for coordinate: CLLocationCoordinate2D) -> CGPoint {
+        let point = mapboxMap.point(for: coordinate)
+        return point
+    }
+
+    func rect(for coordinate: CLLocationCoordinate2D) -> CGRect {
+        CGRect(origin: point(for: coordinate), size: .zero).insetBy(dx: -20, dy: -20)
+    }
+}
+
+class CustomLocationProvider: LocationProvider {
+    var locationProviderOptions: LocationOptions
+
+    let authorizationStatus: CLAuthorizationStatus
+
+    let accuracyAuthorization: CLAccuracyAuthorization
+
+    let heading: CLHeading?
+
+    private let currentLocation: CLLocation
+
+    private weak var delegate: LocationProviderDelegate?
+
+    func setDelegate(_ delegate: LocationProviderDelegate) {
+        self.delegate = delegate
+        delegate.locationProvider(self, didUpdateLocations: [currentLocation])
+    }
+
+    func requestAlwaysAuthorization() {
+        // not required for this example
+    }
+
+    func requestWhenInUseAuthorization() {
+        // not required for this example
+    }
+
+    func requestTemporaryFullAccuracyAuthorization(withPurposeKey purposeKey: String) {
+        // not required for this example
+    }
+
+    func startUpdatingLocation() {
+        // not required for this example
+    }
+
+    func stopUpdatingLocation() {
+        // not required for this example
+    }
+
+    var headingOrientation: CLDeviceOrientation
+
+    func startUpdatingHeading() {
+        // not required for this example
+    }
+
+    func stopUpdatingHeading() {
+        // not required for this example
+    }
+
+    func dismissHeadingCalibrationDisplay() {
+        // not required for this example
+    }
+
+    init(currentLocation: CLLocation) {
+        self.locationProviderOptions = .init()
+        self.authorizationStatus = .authorizedWhenInUse
+        self.accuracyAuthorization = .fullAccuracy
+        self.headingOrientation = .portrait
+        self.heading = nil
+        self.currentLocation = currentLocation
+    }
+}

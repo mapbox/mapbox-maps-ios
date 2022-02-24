ca27dd299024e84090cbdbd72d2d0bfbdd806232
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-02-24 16:15:40 +0300

    Take Xcode min version from CircleCI config

--- scripts/release/create-github-draft-release.sh
+++ scripts/release/create-github-draft-release.sh
@@ -27,6 +27,7 @@ main() {
 
     MAPBOX_COMMON_VERSION=$(jq -r .MapboxCommon "$VERSION_JSON_PATH")
     MAPBOX_COREMAPS_VERSION=$(jq -r .MapboxCoreMaps "$VERSION_JSON_PATH")
+    XCODE_MIN_VERSION=$(python3 -c "import yaml,sys;print(yaml.safe_load(sys.stdin)['executors']['xcode-sdk-min']['macos']['xcode'])" < "$SCRIPT_DIR/../../.circleci/config.yml")
 
     CHANGELOG=$( ([[ $(command -v parse-changelog) ]] && parse-changelog CHANGELOG.md) || echo "<Compose changelog here>" )
 
@@ -35,7 +36,7 @@ main() {
 
 * Compatible version of MapboxCoreMaps: \`$MAPBOX_COREMAPS_VERSION\`
 * Compatible version of MapboxCommon: \`$MAPBOX_COMMON_VERSION\`
-* Compatible version of Xcode: \`12.5.1\`
+* Compatible version of Xcode: \`$XCODE_MIN_VERSION\`
 
 ### Changes
 

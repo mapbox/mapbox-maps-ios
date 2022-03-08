b59269f7774c7105b88f9b3b354d2c9de0e372a8
Andrew Hershberger <andrew.hershberger@mapbox.com>
2022-03-08 12:12:15 -0600

    Offline limit updates

--- Sources/MapboxMaps/Offline/TileStore+MapboxMaps.swift
+++ Sources/MapboxMaps/Offline/TileStore+MapboxMaps.swift
@@ -66,9 +66,11 @@ extension TileStore {
     ///     user to dispatch to a user-controlled thread.
     ///
     /// - Important:
-    ///     By default, users may download up to 250MB of data for offline
-    ///     use without incurring additional charges. This limit is subject
-    ///     to change.
+    ///     By default, users may download up to 750 tile packs for offline
+    ///     use across all regions. If the limit is hit, any loadRegion call
+    ///     will fail until excess regions are deleted. This limit is subject
+    ///     to change. Please contact Mapbox if you require a higher limit.
+    ///     Additional charges may apply.
     @discardableResult
     public func loadTileRegion(forId id: String,
                                loadOptions: TileRegionLoadOptions,

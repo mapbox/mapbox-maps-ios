9be7c6c00c90ccd461e4f022bdc934b7bc0537c8
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-03-10 11:53:08 +0200

    Fix flaky attribution test for good

--- Tests/MapboxMapsTests/Helpers/Random/String+Random.swift
+++ Tests/MapboxMapsTests/Helpers/Random/String+Random.swift
@@ -1,4 +1,10 @@
+import Foundation
 extension String {
+    static func randomAlphanumeric(withLength length: UInt) -> Self {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return (0..<length).reduce(into: "") { s, _ in s.append(letters.randomElement()!) }
+    }
+
     static func randomASCII(withLength length: UInt) -> Self {
         return (0..<length).reduce(into: "") { s, _ in s.append(.randomASCII()) }
     }

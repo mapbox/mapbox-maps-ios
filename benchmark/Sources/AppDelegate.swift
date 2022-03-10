b4bdb00e0802535b41a4083cc2005914bf7c3dbd
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-03-10 20:11:30 +0300

    Make Benchmark iOS 11 compatible

--- benchmark/Sources/AppDelegate.swift
+++ benchmark/Sources/AppDelegate.swift
@@ -7,10 +7,5 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: - UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
 }
 

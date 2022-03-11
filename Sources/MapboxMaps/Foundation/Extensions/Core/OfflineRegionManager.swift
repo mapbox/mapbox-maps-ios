cb6f5049750cd0eb7e0e2702707d69ee50081a7e
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-03-11 13:14:34 +0200

    Add a new mergeOfflineDatabase method with correct signature

--- Sources/MapboxMaps/Foundation/Extensions/Core/OfflineRegionManager.swift
+++ Sources/MapboxMaps/Foundation/Extensions/Core/OfflineRegionManager.swift
@@ -27,10 +27,38 @@ extension OfflineRegionManager {
                                                                                         concreteErrorType: MapError.self))
     }
 
+    /// Merges data from the database at `filePath` into the main offline database.
+    ///
+    /// - Warning: This method passes only the first merged offline region to its completion block.
+    /// In case there are no merged offline regions the completion block is called with an error.
+    ///
+    /// - Parameters:
+    ///   - filePath: The path to the database to be merged
+    ///   - completion: The block to execute with the results. This block is executed on the database thread.
+    ///   The block has no return value and takes a `Result` case parameter that indicates the result of merging the offline database.
+    @available(iOS, deprecated, message: "use mergeOfflineDatabase(forPath:completion) instead")
     public func mergeOfflineDatabase(for filePath: String,
-                                     completion: @escaping (Result<OfflineRegion, Error>) -> Void) {
+                                     completion: @escaping (_ result: Result<OfflineRegion, Error>) -> Void) {
         mergeOfflineDatabase(forFilePath: filePath, callback: coreAPIClosureAdapter(for: completion,
-                                                                                    type: OfflineRegion.self,
+                                                                                    type: NSArray.self,
+                                                                                    concreteErrorType: MapError.self,
+                                                                                       converter: { result in
+            // returning the first region here for backwards compatibility
+            (result as? [OfflineRegion])?.first
+        }))
+    }
+
+    /// Merges data from the database at `filePath` into the main offline database.
+    ///
+    /// - Parameters:
+    ///   - filePath: The path to the database to be merged
+    ///   - completion: The block to execute with the results. This block is executed on the database thread.
+    ///   The block has no return value and takes a `Result` case parameter that indicates the result of merging the offline database.
+    public func mergeOfflineDatabase(forPath filePath: String,
+                                     completion: @escaping (Result<[OfflineRegion], Error>) -> Void) {
+        mergeOfflineDatabase(forFilePath: filePath, callback: coreAPIClosureAdapter(for: completion,
+                                                                                    type: NSArray.self,
                                                                                     concreteErrorType: MapError.self))
     }
+
 }

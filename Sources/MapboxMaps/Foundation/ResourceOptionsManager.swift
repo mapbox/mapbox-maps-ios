c7887e9bfee82a04396cc4f8098e5b16b69584cd
Mai Mai <mai.mai@mapbox.com>
2022-04-20 10:50:57 +0300

    Feature/improve tests resource options manager (#1280)
    * Remove redudant tileStore from ResourceOptionsManager
    * Update new test cases for ResourceOptionsManager

--- Sources/MapboxMaps/Foundation/ResourceOptionsManager.swift
+++ Sources/MapboxMaps/Foundation/ResourceOptionsManager.swift
@@ -54,7 +54,6 @@ public class ResourceOptionsManager {
     }
 
     private var _resourceOptions: ResourceOptions!
-    private var tileStore: TileStore?
     private var bundle: Bundle
 
     /// Initializes a `ResourceOptionsManager` with an optional access token.
@@ -99,11 +98,9 @@ public class ResourceOptionsManager {
 
         _resourceOptions = resourceOptions
         _resourceOptions.accessToken = token
-
-        tileStore = resourceOptions.tileStore
     }
 
-    internal func defaultAccessToken() -> String {
+    private func defaultAccessToken() -> String {
         // Check User defaults
         #if DEBUG
         if let accessToken = UserDefaults.standard.string(forKey: "MBXAccessToken") {

9554ac353170e0f45c213e4ed1f7e00b150cdc2a
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-05-23 12:14:04 +0300

    Generate model layer

---
+++ Tests/MapboxMapsTests/Style/Generated/IntegrationTests/Layers/ModelLayerIntegrationTests.swift
@@ -0,0 +1,64 @@
+// This file is generated
+import XCTest
+@testable import MapboxMaps
+
+final class ModelLayerIntegrationTests: MapViewIntegrationTestCase {
+
+    internal func testBaseClass() throws {
+        // Do nothing
+    }
+
+    internal func testWaitForIdle() throws {
+        let style = try XCTUnwrap(self.style)
+
+        let successfullyAddedLayerExpectation = XCTestExpectation(description: "Successfully added ModelLayer to Map")
+        successfullyAddedLayerExpectation.expectedFulfillmentCount = 1
+
+        let successfullyRetrievedLayerExpectation = XCTestExpectation(description: "Successfully retrieved ModelLayer from Map")
+        successfullyRetrievedLayerExpectation.expectedFulfillmentCount = 1
+
+        style.uri = .streets
+
+        didFinishLoadingStyle = { _ in
+
+            var layer = ModelLayer(id: "test-id")
+            layer.source = "some-source"
+            layer.sourceLayer = nil
+            layer.minZoom = 10.0
+            layer.maxZoom = 20.0
+            layer.visibility = .constant(.visible)
+            layer.modelId = Value<String>.testConstantValue()
+
+            layer.modelColor = Value<StyleColor>.testConstantValue()
+            layer.modelColorTransition = StyleTransition(duration: 10.0, delay: 10.0)
+            layer.modelColorMixIntensity = Value<Double>.testConstantValue()
+            layer.modelColorMixIntensityTransition = StyleTransition(duration: 10.0, delay: 10.0)
+            layer.modelOpacity = Value<Double>.testConstantValue()
+            layer.modelOpacityTransition = StyleTransition(duration: 10.0, delay: 10.0)
+            layer.modelRotationTransition = StyleTransition(duration: 10.0, delay: 10.0)
+            layer.modelScaleTransition = StyleTransition(duration: 10.0, delay: 10.0)
+            layer.modelTranslationTransition = StyleTransition(duration: 10.0, delay: 10.0)
+            layer.modelType = Value<ModelType>.testConstantValue()
+
+            // Add the layer
+            do {
+                try style.addLayer(layer)
+                successfullyAddedLayerExpectation.fulfill()
+            } catch {
+                XCTFail("Failed to add ModelLayer because of error: \(error)")
+            }
+
+            // Retrieve the layer
+            do {
+                _ = try style.layer(withId: "test-id", type: ModelLayer.self)
+                successfullyRetrievedLayerExpectation.fulfill()
+            } catch {
+                XCTFail("Failed to retrieve ModelLayer because of error: \(error)")
+            }
+        }
+
+        wait(for: [successfullyAddedLayerExpectation, successfullyRetrievedLayerExpectation], timeout: 5.0)
+    }
+}
+
+// End of generated file
---
+++ Tests/MapboxMapsTests/Style/Generated/Layers/ModelLayerTests.swift
@@ -0,0 +1,127 @@
+// This file is generated
+import XCTest
+@testable import MapboxMaps
+
+final class ModelLayerTests: XCTestCase {
+
+    func testLayerProtocolMembers() {
+
+        var layer = ModelLayer(id: "test-id")
+        layer.source = "some-source"
+        layer.sourceLayer = nil
+        layer.minZoom = 10.0
+        layer.maxZoom = 20.0
+
+        XCTAssert(layer.id == "test-id")
+        XCTAssert(layer.type == LayerType.model)
+        XCTAssert(layer.filter == nil)
+        XCTAssert(layer.source == "some-source")
+        XCTAssertNil(layer.sourceLayer)
+        XCTAssert(layer.minZoom == 10.0)
+        XCTAssert(layer.maxZoom == 20.0)
+    }
+
+    func testEncodingAndDecodingOfLayerProtocolProperties() {
+        var layer = ModelLayer(id: "test-id")
+        layer.source = "some-source"
+        layer.sourceLayer = nil
+        layer.minZoom = 10.0
+        layer.maxZoom = 20.0
+
+        var data: Data?
+        do {
+            data = try JSONEncoder().encode(layer)
+        } catch {
+            XCTFail("Failed to encode ModelLayer")
+        }
+
+        guard let validData = data else {
+            XCTFail("Failed to encode ModelLayer")
+            return
+        }
+
+        do {
+            let decodedLayer = try JSONDecoder().decode(ModelLayer.self, from: validData)
+            XCTAssert(decodedLayer.id == "test-id")
+            XCTAssert(decodedLayer.type == LayerType.model)
+            XCTAssert(decodedLayer.filter == nil)
+            XCTAssert(decodedLayer.source == "some-source")
+            XCTAssertNil(decodedLayer.sourceLayer)
+            XCTAssert(decodedLayer.minZoom == 10.0)
+            XCTAssert(decodedLayer.maxZoom == 20.0)
+        } catch {
+            XCTFail("Failed to decode ModelLayer")
+        }
+    }
+
+    func testEncodingAndDecodingOfLayoutProperties() {
+        var layer = ModelLayer(id: "test-id")
+        layer.visibility = .constant(.visible)
+        layer.modelId = Value<String>.testConstantValue()
+
+        var data: Data?
+        do {
+            data = try JSONEncoder().encode(layer)
+        } catch {
+            XCTFail("Failed to encode ModelLayer")
+        }
+
+        guard let validData = data else {
+            XCTFail("Failed to encode ModelLayer")
+            return
+        }
+
+        do {
+            let decodedLayer = try JSONDecoder().decode(ModelLayer.self, from: validData)
+            XCTAssert(decodedLayer.visibility == .constant(.visible))
+            XCTAssert(layer.modelId == Value<String>.testConstantValue())
+        } catch {
+            XCTFail("Failed to decode ModelLayer")
+        }
+    }
+
+    func testEncodingAndDecodingOfPaintProperties() {
+       var layer = ModelLayer(id: "test-id")
+       layer.modelColor = Value<StyleColor>.testConstantValue()
+       layer.modelColorTransition = StyleTransition(duration: 10.0, delay: 10.0)
+       layer.modelColorMixIntensity = Value<Double>.testConstantValue()
+       layer.modelColorMixIntensityTransition = StyleTransition(duration: 10.0, delay: 10.0)
+       layer.modelOpacity = Value<Double>.testConstantValue()
+       layer.modelOpacityTransition = StyleTransition(duration: 10.0, delay: 10.0)
+       layer.modelRotation = Value<[Double]>.testConstantValue()
+       layer.modelRotationTransition = StyleTransition(duration: 10.0, delay: 10.0)
+       layer.modelScale = Value<[Double]>.testConstantValue()
+       layer.modelScaleTransition = StyleTransition(duration: 10.0, delay: 10.0)
+       layer.modelTranslation = Value<[Double]>.testConstantValue()
+       layer.modelTranslationTransition = StyleTransition(duration: 10.0, delay: 10.0)
+       layer.modelType = Value<ModelType>.testConstantValue()
+
+       var data: Data?
+       do {
+           data = try JSONEncoder().encode(layer)
+       } catch {
+           XCTFail("Failed to encode ModelLayer")
+       }
+
+       guard let validData = data else {
+           XCTFail("Failed to encode ModelLayer")
+           return
+       }
+
+       do {
+           let decodedLayer = try JSONDecoder().decode(ModelLayer.self, from: validData)
+           XCTAssert(decodedLayer.visibility == .constant(.visible))
+           XCTAssert(layer.modelColor == Value<StyleColor>.testConstantValue())
+           XCTAssert(layer.modelColorMixIntensity == Value<Double>.testConstantValue())
+           XCTAssert(layer.modelOpacity == Value<Double>.testConstantValue())
+           XCTAssert(layer.modelRotation == Value<[Double]>.testConstantValue())
+           XCTAssert(layer.modelScale == Value<[Double]>.testConstantValue())
+           XCTAssert(layer.modelTranslation == Value<[Double]>.testConstantValue())
+           XCTAssert(layer.modelType == Value<ModelType>.testConstantValue())
+       } catch {
+           XCTFail("Failed to decode ModelLayer")
+       }
+    }
+}
+
+// End of generated file

231773d11d040ac0e276a23372e3b8c9ff77f63b
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-06-10 23:15:48 +0300

    Add token helper for tests

--- Apps/ci_scripts/ci_post_clone.sh
+++ Apps/ci_scripts/ci_post_clone.sh
@@ -2,3 +2,6 @@
 
 echo "machine api.mapbox.com login mapbox password $SDK_REGISTRY_TOKEN" >> ~/.netrc
 chmod 0600 ~/.netrc
+
+pwd
+echo "$SDK_REGISTRY_TOKEN" > "../Tests/MapboxMapsTests/Helpers/MapboxAccessToken"

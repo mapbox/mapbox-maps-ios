f5eb6185452a7a194165850a2dfde5d8afbfd78d
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-06-10 23:16:07 +0300

    Use recommended settings for ci scripts

--- Apps/ci_scripts/ci_post_clone.sh
+++ Apps/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/zsh
 
 echo "machine api.mapbox.com login mapbox password $SDK_REGISTRY_TOKEN" >> ~/.netrc
 chmod 0600 ~/.netrc
 
 pwd
-echo "$SDK_REGISTRY_TOKEN" > "../Tests/MapboxMapsTests/Helpers/MapboxAccessToken"
+echo "$SDK_REGISTRY_TOKEN" > "$CI_WORKSPACE/Tests/MapboxMapsTests/Helpers/MapboxAccessToken"

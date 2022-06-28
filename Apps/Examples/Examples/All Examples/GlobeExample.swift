0d8d1205414585de2b40be077502c89394323319
ZiZi <44972592+ZiZasaurus@users.noreply.github.com>
2022-06-28 11:36:33 -0400

    Update Apps/Examples/Examples/All Examples/GlobeExample.swift
    Co-authored-by: Roman Laitarenko <roman.laitarenko@mapbox.com>

--- Apps/Examples/Examples/All Examples/GlobeExample.swift
+++ Apps/Examples/Examples/All Examples/GlobeExample.swift
@@ -14,7 +14,8 @@ class GlobeExample: UIViewController, ExampleProtocol {
         mapView = MapView(frame: view.bounds, mapInitOptions: .init(styleURI: .satelliteStreets))
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapView.mapboxMap.setCamera(to: .init(center: CLLocationCoordinate2D(latitude: 50, longitude: 30), zoom: 0.45))
-
+        try! self.mapView.mapboxMap.style.setProjection(StyleProjection(name: .globe))
+        
         mapView.mapboxMap.onNext(event: .styleLoaded) { _ in
             try! self.mapView.mapboxMap.style.setAtmosphere(Atmosphere())
             self.finish()

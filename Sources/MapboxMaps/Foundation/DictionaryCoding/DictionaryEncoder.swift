48ecfd6f8dbf9b6f1afda8a21524e207504d8afa
Mai Mai <mai.mai@mapbox.com>
2022-08-01 21:06:45 +0700

    Use encodeNilIfNeeded instead of wrapper struct

--- Sources/MapboxMaps/Foundation/DictionaryCoding/DictionaryEncoder.swift
+++ Sources/MapboxMaps/Foundation/DictionaryCoding/DictionaryEncoder.swift
@@ -211,64 +211,68 @@ private extension DictionaryEncoder {
         }
 
         func encodeIfPresent<T: Encodable>(_ value: T?, forKey key: Key) throws {
-            guard value != nil || shouldEncodeNilValues else { return }
-            try encode(value, forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Bool?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: String?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Double?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Float?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Int?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Int8?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Int16?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Int32?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: Int64?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: UInt?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: UInt8?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: UInt16?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: UInt32?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
         }
 
         func encodeIfPresent(_ value: UInt64?, forKey key: Key) throws {
-            try encodeIfPresent(NilEncodable(value), forKey: key)
+            try encodeNilIfNeeded(value, forKey: key)
+        }
+
+        private func encodeNilIfNeeded<T: Encodable>(_ value: T?, forKey key: Key) throws {
+            guard value != nil || shouldEncodeNilValues else { return }
+            try encode(value, forKey: key)
         }
 
         func nestedContainer<NestedKey>(

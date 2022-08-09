fb2b83861485705efbeb8522a9cd9233bdc68a70
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-08-09 13:38:58 +0300

    Remove TimingCurve.linear

--- Sources/MapboxMaps/Foundation/Interpolators/TimingCurve.swift
+++ Sources/MapboxMaps/Foundation/Interpolators/TimingCurve.swift
@@ -8,8 +8,4 @@ internal struct TimingCurve: Equatable {
         p1: CGPoint(x: 0.42, y: 0),
         p2: CGPoint(x: 0.58, y: 1)
     )
-    internal static let linear = TimingCurve(
-        p1: .zero,
-        p2: CGPoint(x: 1, y: 1)
-    )
 }

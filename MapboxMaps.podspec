45df908c7063bf982513ecc4cd2cc61f0599a794
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-08-24 18:04:12 +0300

    Update CoreMaps to 10.8.0-rc.1 (#1542)
    * Update CoreMaps to 10.8.0-rc.1
    
    * Update changelog

--- MapboxMaps.podspec
+++ MapboxMaps.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |m|
 
-  maps_version = '10.8.0-beta.1'
+  maps_version = '10.8.0-rc.1'
 
   m.name = 'MapboxMaps'
   m.version = maps_version
@@ -21,8 +21,8 @@ Pod::Spec.new do |m|
   m.source_files = 'Sources/MapboxMaps/**/*.{swift,h}'
   m.resources = ['Sources/**/*.{xcassets,strings}', 'Sources/MapboxMaps/MapboxMaps.json']
 
-  m.dependency 'MapboxCoreMaps', '10.8.0-beta.2'
-  m.dependency 'MapboxCommon', '23.0.0-beta.1'
+  m.dependency 'MapboxCoreMaps', '10.8.0-rc.1'
+  m.dependency 'MapboxCommon', '23.0.0-rc.2'
   m.dependency 'MapboxMobileEvents', '1.0.8'
   m.dependency 'Turf', '~> 2.0'
 
--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["MapboxMaps"]),
     ],
     dependencies: [
-        .package(name: "MapboxCoreMaps", url: "https://github.com/mapbox/mapbox-core-maps-ios.git", .exact("10.8.0-beta.2")),
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("23.0.0-beta.1")),
+        .package(name: "MapboxCoreMaps", url: "https://github.com/mapbox/mapbox-core-maps-ios.git", .exact("10.8.0-rc.1")),
+        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("23.0.0-rc.2")),
         .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", .exact("1.0.8")),
         .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", from: "2.0.0"),
         .package(name: "CocoaImageHashing", url: "https://github.com/ameingast/cocoaimagehashing", .exact("1.9.0"))

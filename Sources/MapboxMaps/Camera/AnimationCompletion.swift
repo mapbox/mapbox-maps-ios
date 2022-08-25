acc3e52325a0f6d3a8986a261e5eb722928fc652
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-08-25 14:15:56 +0300

    UIViewAnimatingPosition support

--- Sources/MapboxMaps/Camera/AnimationCompletion.swift
+++ Sources/MapboxMaps/Camera/AnimationCompletion.swift
@@ -4,4 +4,19 @@ import AppKit
 import UIKit
 #endif
 
+
+#if os(OSX)
+public enum UIViewAnimatingState: Int {
+    case inactive
+    case active
+    case stopped
+}
+
+public enum UIViewAnimatingPosition: Int {
+    case end
+    case start
+    case current
+}
+#endif
+
 public typealias AnimationCompletion = (UIViewAnimatingPosition) -> Void

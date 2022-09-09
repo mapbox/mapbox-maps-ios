4ab7f15fd1239d8e8ff46167ad936550e86a26e5
Patrick Leonard <pjleonard37@users.noreply.github.com>
2022-09-09 10:02:49 -0400

    Mapsios 247 sync location examples with android (#1553)
    * Change puck for Navigation Simulator
    
    * Update Location component example to enable customization
    
    * Finishes puck example
    
    * Update Apps/Examples/Examples/All Examples/NavigationSimulatorExample.swift
    
    Co-authored-by: Roman Gardukevich <roman.gardukevich@mapbox.com>
    
    * Update example based on review
    
    * Apply suggestions from code review
    
    Co-authored-by: Roman Gardukevich <roman.gardukevich@mapbox.com>
    
    * Restructure UI logic into enums
    
    * Apply suggestions from code review
    
    Co-authored-by: Roman Gardukevich <roman.gardukevich@mapbox.com>
    
    * Extract logic to updatePuckUI, make didSet lightweight
    
    * Update Apps/Examples/Examples/All Examples/Custom2DPuckExample.swift
    
    Co-authored-by: Roman Gardukevich <roman.gardukevich@mapbox.com>
    
    * Fix Swiftlint
    
    Co-authored-by: Roman Gardukevich <roman.gardukevich@mapbox.com>

--- Apps/Examples/Examples/All Examples/NavigationSimulatorExample.swift
+++ Apps/Examples/Examples/All Examples/NavigationSimulatorExample.swift
@@ -41,7 +41,8 @@ final class NavigationSimulatorExample: UIViewController, ExampleProtocol {
     private func configureMap() {
         navigationSimulator = NavigationSimulator(viewport: mapView.viewport, route: sampleRouteLine)
 
-        mapView.location.options.puckType = .puck2D(.makeDefault(showBearing: true))
+        let configuration = Puck2DConfiguration(topImage: UIImage(named: "user_puck_icon")!)
+        mapView.location.options.puckType = .puck2D(configuration)
         mapView.location.options.puckBearingSource = .course
         mapView.location.overrideLocationProvider(with: navigationSimulator)
         mapView.location.addPuckLocationConsumer(self)

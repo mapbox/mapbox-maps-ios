77b270abfe1c6f0f24c83c941eb3f22f636bf186
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-09-21 11:31:46 +0300

    Add a RasterDemSource when executing EnableTerrain command

--- benchmark/Tests/Commands/EnableTerrainCommand.swift
+++ benchmark/Tests/Commands/EnableTerrainCommand.swift
@@ -10,6 +10,10 @@ struct EnableTerrainCommand: AsyncCommand {
             throw ExecutionError.cannotFindMapboxMap
         }
 
+        if let source = terrain.rasterDemSource {
+            try mapView.mapboxMap.style.addSource(source, id: terrain.source)
+        }
+
         try mapView.mapboxMap.style.setTerrain(terrain)
     }
 }
@@ -18,5 +22,25 @@ extension EnableTerrainCommand: Decodable {
 
     init(from decoder: Decoder) throws {
         terrain = try Terrain(from: decoder)
+    }
+}
+
+private extension Terrain {
+    var rasterDemSource: RasterDemSource? {
+        var source = RasterDemSource()
+        source.maxzoom = 14
+
+        switch self.source {
+        case "mapbox-dem":
+            source.url = "mapbox://mapbox.terrain-rgb"
+            source.tileSize = 512
+        case "mapbox-dem-padded":
+            source.url = "mapbox://mapbox.mapbox-terrain-dem-v1"
+            source.tileSize = 514
+        default:
+            return nil
+        }
+
+        return source
     }
 }

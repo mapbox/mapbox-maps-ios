52db40491da2dca79b791ee3287b087f72065488
Patrick Leonard <patrick.leonard@mapbox.com>
2022-09-27 16:43:00 -0400

    Add additional tests based on review

--- Tests/MapboxMapsTests/Annotations/Generated/CircleAnnotationManagerTests.swift
+++ Tests/MapboxMapsTests/Annotations/Generated/CircleAnnotationManagerTests.swift
@@ -1,12 +1,13 @@
 import Foundation
 import XCTest
 @testable import MapboxMaps
+import Turf
 
 final class CircleAnnotationManagerTests: XCTestCase {
 
     var manager: CircleAnnotationManager!
     var style: MockStyle!
-    var displayLinkCoordinator: DisplayLinkCoordinator!
+    var displayLinkCoordinator: MockDisplayLinkCoordinator!
     var id = UUID().uuidString
     var annotations = [CircleAnnotation]()
 
@@ -38,24 +39,30 @@ final class CircleAnnotationManagerTests: XCTestCase {
 
     func testAddSource() {
         // when
-        manager.annotations = annotations
+        _ = CircleAnnotationManager(id: id,
+                                 style: style,
+                                 layerPosition: nil,
+                                 displayLinkCoordinator: displayLinkCoordinator)
 
         // then
-        XCTAssertEqual(style.addSourceStub.invocations.count, 1)
+        XCTAssertEqual(style.addSourceStub.invocations.count, 2) // once for init in setUp and once here
         XCTAssertEqual(style.addSourceStub.invocations.last?.parameters.source.type, SourceType.geoJson)
         XCTAssertEqual(style.addSourceStub.invocations.last?.parameters.id, manager.id)
     }
 
     func testAddLayer() {
         // when
-        manager.annotations = annotations
+        let initializedManager = CircleAnnotationManager(id: id,
+                                                         style: style,
+                                                         layerPosition: nil,
+                                                         displayLinkCoordinator: displayLinkCoordinator)
 
         // then
-        XCTAssertEqual(style.addPersistentLayerStub.invocations.count, 1)
+        XCTAssertEqual(style.addSourceStub.invocations.count, 2) // once for init in setUp and once here
         XCTAssertEqual(style.addPersistentLayerWithPropertiesStub.invocations.count, 0)
         XCTAssertEqual(style.addPersistentLayerStub.invocations.last?.parameters.layer.type, LayerType.circle)
-        XCTAssertEqual(style.addPersistentLayerStub.invocations.last?.parameters.layer.id, manager.id)
-        XCTAssertEqual(style.addPersistentLayerStub.invocations.last?.parameters.layer.source, manager.sourceId)
+        XCTAssertEqual(style.addPersistentLayerStub.invocations.last?.parameters.layer.id, initializedManager.id)
+        XCTAssertEqual(style.addPersistentLayerStub.invocations.last?.parameters.layer.source, initializedManager.sourceId)
         XCTAssertNil(style.addPersistentLayerStub.invocations.last?.parameters.layerPosition)
     }
 
@@ -191,10 +198,127 @@ final class CircleAnnotationManagerTests: XCTestCase {
     }
 
     func testSetCircleTranslateAnchor() {
+        // given
+        let newCircleTranslateAnchorProperty = CircleTranslateAnchor(rawValue: "map")
+
         // when
-        manager.circleTranslateAnchor = CircleTranslateAnchor(rawValue: "map")
+        manager.circleTranslateAnchor = newCircleTranslateAnchorProperty
 
         // then
         XCTAssertEqual(manager.circleTranslateAnchor, CircleTranslateAnchor(rawValue: "map"))
+    }
+
+    func testManagerSubscribestoDisplayLinkCoordinator() {
+        // then
+        XCTAssertEqual(displayLinkCoordinator.addStub.invocations.count, 1)
+        XCTAssertEqual(displayLinkCoordinator.removeStub.invocations.count, 0)
+    }
+
+    func testDestroyManagerRemovesDisplayLinkParticipant() {
+        // when
+        manager.destroy()
+
+        // then
+        XCTAssertEqual(displayLinkCoordinator.removeStub.invocations.count, 1)
+    }
+
+    func testSyncSourceAndLayerWhenNewPropertiesSet() {
+        // when
+        manager.circleTranslate = [23.34, 234.5]
+        manager.syncSourceAndLayerIfNeeded()
+
+        // then
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.count, 1)
+        XCTAssertEqual(style.updateGeoJSONSourceStub.invocations.count, 1)
+    }
+
+    func testAnnotationPropertiesAdded() {
+        // given
+        let newCircleTranslateProperty = [23.34, 234.5]
+
+        // when
+        manager.circleTranslate = newCircleTranslateProperty
+        manager.syncSourceAndLayerIfNeeded()
+
+        // then
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.last?.parameters.layerId, manager.id)
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-translate"] as! [Double], newCircleTranslateProperty)
+    }
+
+    func testAnnotationPropertiesAddedWithoutDuplicate() {
+        // given
+        let newCircleTranslateProperty = [23.34, 234.5]
+        let secondNewCircleTranslateProperty = [324.432, 324.432]
+
+        // when
+        manager.circleTranslate = newCircleTranslateProperty
+        manager.syncSourceAndLayerIfNeeded()
+        manager.circleTranslate = secondNewCircleTranslateProperty
+        manager.syncSourceAndLayerIfNeeded()
+
+        // then
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.last?.parameters.layerId, manager.id)
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.count, 2)
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-translate"] as! [Double], secondNewCircleTranslateProperty)
+    }
+
+    func testAnnotationPropertiesMergedWithManagersLayerProperties() {
+        // given
+        var annotations = [CircleAnnotation]()
+        for _ in 0...5 {
+            var annotation = CircleAnnotation(centerCoordinate: CLLocationCoordinate2D(latitude: 38.9, longitude: -77.1))
+            annotation.circleColor = StyleColor(UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0))
+            annotation.circleRadius = 12
+            annotations.append(annotation)
+        }
+        let newCircleTranslateProperty = [23.34, 234.5]
+
+        // when
+        manager.annotations = annotations
+        manager.circleTranslate = newCircleTranslateProperty
+        manager.syncSourceAndLayerIfNeeded()
+
+        // then
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.count, 1)
+        XCTAssertNotNil(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-color"])
+        XCTAssertNotNil(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-radius"])
+        XCTAssertNil(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-blur"])
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-translate"] as! [Double], newCircleTranslateProperty)
+    }
+
+    func testAnnotationPropertiesResetProperly() {
+        // given
+        let newCircleTranslateProperty = [23.34, 234.5]
+
+        // when
+        manager.circleTranslate = newCircleTranslateProperty
+        manager.syncSourceAndLayerIfNeeded()
+        manager.circleTranslate = nil
+        manager.syncSourceAndLayerIfNeeded()
+
+        // then
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.last?.parameters.properties["circle-translate"] as! [Double], [0, 0])
+        XCTAssertEqual(style.setLayerPropertiesStub.invocations.count, 2)
+    }
+
+    func testfeatureCollectionPassedtoGeoJSON() {
+        // given
+        var annotations = [CircleAnnotation]()
+        for _ in 0...5 {
+            var annotation = CircleAnnotation(centerCoordinate: CLLocationCoordinate2D(latitude: 38.9, longitude: -77.1))
+            annotation.circleColor = StyleColor(UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0))
+            annotation.circleRadius = 12
+            annotations.append(annotation)
+        }
+        let featureCollection = FeatureCollection(features: annotations.map(\.feature))
+
+        // when
+        manager.annotations = annotations
+        manager.syncSourceAndLayerIfNeeded()
+
+        // then
+        XCTAssertEqual(style.updateGeoJSONSourceStub.invocations.count, 1)
+        XCTAssertEqual(style.updateGeoJSONSourceStub.invocations.last?.parameters.id, manager.id)
+        XCTAssertEqual(style.updateGeoJSONSourceStub.invocations.last?.parameters.geojson, .featureCollection(featureCollection))
     }
 }

7c33fec60d760ef1da25124e02c5bed9a39685e8
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-10-14 16:36:05 +0300

    Drop pre-Xcode 13 compilation conditions

--- Sources/MapboxMaps/Foundation/RetroactiveConformances/DisplayLinkProtocol.swift
+++ Sources/MapboxMaps/Foundation/RetroactiveConformances/DisplayLinkProtocol.swift
@@ -4,12 +4,8 @@ internal protocol DisplayLinkProtocol: AnyObject {
     var timestamp: CFTimeInterval { get }
     var duration: CFTimeInterval { get }
     var preferredFramesPerSecond: Int { get set }
-    // Checking Swift version as a proxy for iOS SDK version to enable
-    // building with iOS SDKs < 15
-    #if swift(>=5.5)
     @available(iOS 15.0, *)
     var preferredFrameRateRange: CAFrameRateRange { get set }
-    #endif
     var isPaused: Bool { get set }
     func add(to runloop: RunLoop, forMode mode: RunLoop.Mode)
     func invalidate()

145eb3288b2fbd0852ba983f4408de608094d792
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-10-17 18:55:50 +0300

    Fix comparison

--- Sources/MapboxMaps/Foundation/Extensions/Core/StyleObjectInfo+3DPuck.swift
+++ Sources/MapboxMaps/Foundation/Extensions/Core/StyleObjectInfo+3DPuck.swift
@@ -2,5 +2,5 @@ import Foundation
 @_implementationOnly import MapboxCoreMaps_Private
 
 internal extension StyleObjectInfo {
-    var is3DPuckLayer: Bool { type == "model" && "id" == Puck3D.layerID }
+    var is3DPuckLayer: Bool { type == "model" && id == Puck3D.layerID }
 }

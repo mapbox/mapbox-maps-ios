e02ee1f55652357cc3d596913b9821bd0193b66e
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-10-19 17:30:39 +0300

    Run a single test

--- Apps/Examples/ExamplesTests/TestableExampleTests.swift
+++ Apps/Examples/ExamplesTests/TestableExampleTests.swift
@@ -29,18 +29,19 @@ class TestableExampleTests: XCTestCase {
 
         let existingImpl = method_getImplementation(method)
 
-        for category in Examples.all {
-            for example in category["examples"] as! [Example] {
-                // Add a method for this test, but using the same implementation
-                let selectorName = "test\(example.type)"
-                let testSelector = Selector((selectorName))
-                class_addMethod(Self.self, testSelector, existingImpl, "v@:f")
+//        for category in Examples.all {
+        //            for example in category["examples"] as! [Example] {
+        // Add a method for this test, but using the same implementation
+        let example = Examples.annotationExamples.last!
+        let selectorName = "test\(example.type)"
+        let testSelector = Selector((selectorName))
+        class_addMethod(Self.self, testSelector, existingImpl, "v@:f")
 
-                let test = TestableExampleTests(selector: testSelector)
-                test.example = example
-                newTestSuite.addTest(test)
-            }
-        }
+        let test = TestableExampleTests(selector: testSelector)
+        test.example = example
+        newTestSuite.addTest(test)
+//            }
+//        }
         return newTestSuite
     }
 

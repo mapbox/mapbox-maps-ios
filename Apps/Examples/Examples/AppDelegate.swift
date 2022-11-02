8ece838bcee42ff5391f344367ae0babaa3b3480
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-11-02 14:27:46 +0200

    Prevent map from being rendered on background (#1675)

--- Apps/Examples/Examples/AppDelegate.swift
+++ Apps/Examples/Examples/AppDelegate.swift
@@ -11,9 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        let examplesTableViewController = ExampleTableViewController()
-        let navigationController = UINavigationController(rootViewController: examplesTableViewController)
-
         let appearance = UINavigationBar.appearance()
         appearance.prefersLargeTitles = true
 
@@ -21,9 +18,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             appearance.scrollEdgeAppearance = UINavigationBarAppearance()
         }
 
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
+        if #unavailable(iOS 13.0) {
+            let examplesTableViewController = ExampleTableViewController()
+            let navigationController = UINavigationController(rootViewController: examplesTableViewController)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
 
         return true
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let config = UISceneConfiguration(name: "Default configuration", sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        config.sceneClass = UIWindowScene.self
+        return config
+    }
+}
+
+@available(iOS 13.0, *)
+final class SceneDelegate: NSObject, UISceneDelegate {
+    var windows: [UIWindow] = []
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+
+        let examplesTableViewController = ExampleTableViewController()
+        let navigationController = UINavigationController(rootViewController: examplesTableViewController)
+
+        let appearance = UINavigationBar.appearance()
+        appearance.prefersLargeTitles = true
+
+        appearance.scrollEdgeAppearance = UINavigationBarAppearance()
+
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+
+        windows.append(window)
     }
 }

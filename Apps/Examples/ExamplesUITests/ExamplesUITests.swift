58d707937bd2f5a9b80e676bd6ef866ce2de5572
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-11-03 17:35:47 +0200

    Restore the old UI Test

--- Apps/Examples/ExamplesUITests/ExamplesUITests.swift
+++ Apps/Examples/ExamplesUITests/ExamplesUITests.swift
@@ -45,4 +45,18 @@ final class ExamplesUITests: XCTestCase {
 
         wait(for: [locationPermissionGrantedExpectation], timeout: 5)
     }
+
+    func testEveryExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Tap every example cell in the table view, and then dismiss the example.
+        for cell in app.tables.element(boundBy: 0).cells.allElementsBoundByIndex {
+            // Open the example
+            cell.tap()
+            // Navigate back to the table view
+            app.navigationBars.buttons.element(boundBy: 0).tap()
+        }
+    }
 }

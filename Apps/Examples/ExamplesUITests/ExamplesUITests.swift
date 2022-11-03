69b8b298b85de6b4e3b40f12bae0ed2c88a4ae81
Roman Laitarenko <roman.laitarenko@mapbox.com>
2022-11-03 17:51:10 +0200

    Add UI test to grant location permission before running examples tests (#1685)

--- Apps/Examples/ExamplesUITests/ExamplesUITests.swift
+++ Apps/Examples/ExamplesUITests/ExamplesUITests.swift
@@ -1,6 +1,7 @@
 import XCTest
 
-class ExamplesUITests: XCTestCase {
+final class ExamplesUITests: XCTestCase {
+    private var locationAuthorizationAlertMonitor: NSObjectProtocol?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -11,6 +12,38 @@ class ExamplesUITests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_GrantLocationPermission() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let locationPermissionGrantedExpectation = expectation(description: "Location permission granted")
+        locationAuthorizationAlertMonitor = addUIInterruptionMonitor(withDescription: "", handler: { alert in
+            let allowButton = alert.buttons["Allow While Using App"]
+
+            guard allowButton.exists else {
+                XCTFail("Can't find the allow button")
+                return false
+            }
+            locationPermissionGrantedExpectation.fulfill()
+            allowButton.tap()
+            return true
+        })
+
+        // Navigate to an example that should trigger location permissoon alert to be shown
+        let searchField = app.navigationBars.firstMatch.searchFields.firstMatch
+        searchField.tap()
+        searchField.typeText("Location")
+        app.tables.firstMatch.cells.firstMatch.tap()
+
+        // wait for the alert to appear
+        sleep(2)
+
+        // interact with the app so that UI interruption monitor gets triggered
+        app.swipeUp()
+
+        wait(for: [locationPermissionGrantedExpectation], timeout: 5)
     }
 
     func testEveryExample() throws {

e7bf187c8ee9b8c1a722cb84796f07de03309820
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-11-10 13:51:19 +0200

    Small refactoring

--- Tests/MapboxMapsTests/Helpers/Stubbed.swift
+++ Tests/MapboxMapsTests/Helpers/Stubbed.swift
@@ -1,8 +1,28 @@
-//
-//  File.swift
-//  
-//
-//  Created by odnairy on 10.11.22.
-//
+@propertyWrapper
+final class Stubbed<T> {
+    let getStub: Stub<Void, T>
+    let setStub = Stub<T, Void>()
 
-import Foundation
+    var projectedValue: Stubbed<T> {
+        return self
+    }
+
+    init(wrappedValue: T) {
+        getStub = Stub(defaultReturnValue: wrappedValue)
+    }
+
+    var wrappedValue: T {
+        get {
+            getStub.call()
+        }
+        set {
+            setStub.call(with: newValue)
+            getStub.defaultReturnValue = newValue
+        }
+    }
+
+    func reset() {
+        getStub.reset()
+        setStub.reset()
+    }
+}

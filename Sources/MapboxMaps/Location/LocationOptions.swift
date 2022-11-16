95bee4a7ef750acb91e6af8483d5d77d1e42f99b
James Hurst <james@jameshurst.ca>
2022-11-16 17:45:19 -0500

    Allow external initialization of options

--- Sources/MapboxMaps/Location/LocationOptions.swift
+++ Sources/MapboxMaps/Location/LocationOptions.swift
@@ -3,32 +3,46 @@ import CoreLocation
 
 /// A struct to configure a `LocationManager`
 public struct LocationOptions: Equatable {
-
     /// Specifies the minimum distance (measured in meters) a device must move horizontally
     /// before a location update is generated.
 
     /// The default value of this property is `kCLDistanceFilterNone`.
-    public var distanceFilter: CLLocationDistance = kCLDistanceFilterNone
+    public var distanceFilter: CLLocationDistance
 
     /// Specifies the accuracy of the location data.
     /// The default value is `kCLLocationAccuracyBest`.
-    public var desiredAccuracy: CLLocationAccuracy = kCLLocationAccuracyBest
+    public var desiredAccuracy: CLLocationAccuracy
 
     /// Sets the type of user activity associated with the location updates.
     /// The default value is `CLActivityType.other`.
-    public var activityType: CLActivityType = .other
+    public var activityType: CLActivityType
 
     /// Sets the type of puck that should be used
     public var puckType: PuckType?
 
     /// Specifies if a `Puck` should use `Heading` or `Course` for the bearing
     /// This is an experimental option. The default value is `PuckBearingSource.heading`.
-    public var puckBearingSource: PuckBearingSource = .heading
+    public var puckBearingSource: PuckBearingSource
 
     /// Whether the puck rotates to track the bearing source.
-    public var puckBearingEnabled: Bool = true
+    public var puckBearingEnabled: Bool
 
-    public init() {}
+    /// Initializes a `LocationOptions`.
+    public init(
+        distanceFilter: CLLocationDistance = kCLDistanceFilterNone,
+        desiredAccuracy: CLLocationAccuracy = kCLLocationAccuracyBest,
+        activityType: CLActivityType = .other,
+        puckType: PuckType? = nil,
+        puckBearingSource: PuckBearingSource = .heading,
+        puckBearingEnabled: Bool = true
+    ) {
+        self.distanceFilter = distanceFilter
+        self.desiredAccuracy = desiredAccuracy
+        self.activityType = activityType
+        self.puckType = puckType
+        self.puckBearingSource = puckBearingSource
+        self.puckBearingEnabled = puckBearingEnabled
+    }
 }
 
 /// Controls how the puck is oriented

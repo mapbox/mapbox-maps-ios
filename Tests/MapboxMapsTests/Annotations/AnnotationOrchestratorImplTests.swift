e662bb39a2239127830af5ada127e7c4da6f9191
ZiZi <44972592+ZiZasaurus@users.noreply.github.com>
2022-11-24 10:35:33 -0500

    Update Tests/MapboxMapsTests/Annotations/AnnotationOrchestratorImplTests.swift
    Co-authored-by: Roman Laitarenko <roman.laitarenko@mapbox.com>

--- Tests/MapboxMapsTests/Annotations/AnnotationOrchestratorImplTests.swift
+++ Tests/MapboxMapsTests/Annotations/AnnotationOrchestratorImplTests.swift
@@ -89,8 +89,8 @@ final class AnnotationOrchestratorImplTests: XCTestCase {
 
         impl.removeAnnotationManager(withId: managerId)
 
-        XCTAssertTrue(!tapGestureRecognizer.isEnabled)
-        XCTAssertTrue(!longPressGestureRecognizer.isEnabled)
+        XCTAssertFalse(tapGestureRecognizer.isEnabled)
+        XCTAssertFalse(longPressGestureRecognizer.isEnabled)
     }
 
     func testSingleTapRecognizesSimultaneouslyWithOtherTapRecognizer() {

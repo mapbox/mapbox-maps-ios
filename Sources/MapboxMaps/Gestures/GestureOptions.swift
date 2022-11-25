0bdf5c7818d4bbd61600fd26ece1d069f96f49fa
James Hurst <james@jameshurst.ca>
2022-11-25 09:08:57 -0500

    Allow external initialization of options (#1732)
    * Allow external initialization of options
    
    * Tidy up documentation for options
    
    * Add an allowlist entry for options initializer
    
    Co-authored-by: Roman Laitarenko <roman.laitarenko@mapbox.com>

--- Sources/MapboxMaps/Gestures/GestureOptions.swift
+++ Sources/MapboxMaps/Gestures/GestureOptions.swift
@@ -17,14 +17,18 @@ public enum PanMode: String, Equatable, CaseIterable {
 /// Configuration options for the built-in gestures
 public struct GestureOptions: Equatable {
 
-    /// Whether the single-touch pan gesture is enabled. Defaults to `true`.
-    public var panEnabled: Bool = true
+    /// Whether the single-touch pan gesture is enabled.
+    ///
+    /// Defaults to `true`.
+    public var panEnabled: Bool
 
     /// Whether the pinch gesture is enabled. Allows panning, rotating, and zooming.
+    ///
     /// Defaults to `true`.
-    public var pinchEnabled: Bool = true
+    public var pinchEnabled: Bool
 
     /// Whether rotation is enabled for the pinch gesture.
+    ///
     /// Defaults to `true`.
     @available(*, deprecated, renamed: "rotateEnabled")
     public var pinchRotateEnabled: Bool {
@@ -33,49 +37,104 @@ public struct GestureOptions: Equatable {
     }
 
     /// Whether rotation gesture is enabled.
+    ///
     /// Defaults to `true`.
-    public var rotateEnabled: Bool = true
+    public var rotateEnabled: Bool
 
     /// Whether rotation is enabled for the pinch to zoom gesture.
+    ///
     /// Defaults to `true`.
-    public var simultaneousRotateAndPinchZoomEnabled: Bool = true
+    public var simultaneousRotateAndPinchZoomEnabled: Bool
 
     /// Whether zoom is enabled for the pinch gesture.
+    ///
     /// Defaults to `true`.
-    public var pinchZoomEnabled: Bool = true
+    public var pinchZoomEnabled: Bool
 
     /// Whether pan is enabled for the pinch gesture.
+    ///
     /// Defaults to `true`.
-    public var pinchPanEnabled: Bool = true
+    public var pinchPanEnabled: Bool
 
-    /// Whether the pitch gesture is enabled. Defaults to `true`.
-    public var pitchEnabled: Bool = true
+    /// Whether the pitch gesture is enabled.
+    ///
+    /// Defaults to `true`.
+    public var pitchEnabled: Bool
 
     /// Whether double tapping the map with one touch results in a zoom-in animation.
+    ///
     /// Defaults to `true`.
-    public var doubleTapToZoomInEnabled: Bool = true
+    public var doubleTapToZoomInEnabled: Bool
 
     /// Whether single tapping the map with two touches results in a zoom-out animation.
+    ///
     /// Defaults to `true`.
-    public var doubleTouchToZoomOutEnabled: Bool = true
+    public var doubleTouchToZoomOutEnabled: Bool
 
-    /// Whether the quick zoom gesture is enabled. Defaults to `true`.
-    public var quickZoomEnabled: Bool = true
+    /// Whether the quick zoom gesture is enabled.
+    ///
+    /// Defaults to `true`.
+    public var quickZoomEnabled: Bool
 
     /// Configures the directions in which the map is allowed to move during a pan gesture.
+    ///
     /// Defaults to `PanMode.horizontalAndVertical`. Called `scrollMode` in
     /// the Android SDK for consistency with platform conventions.
-    public var panMode: PanMode = .horizontalAndVertical
+    public var panMode: PanMode
 
     /// A constant factor that determines how quickly pan deceleration animations happen.
     /// Multiplied with the velocity vector once per millisecond during deceleration animations.
+    /// 
     /// Defaults to `UIScrollView.DecelerationRate.normal.rawValue`
-    public var panDecelerationFactor: CGFloat = UIScrollView.DecelerationRate.normal.rawValue
+    public var panDecelerationFactor: CGFloat
 
     /// By default, gestures rotate and zoom around the center of the gesture. Set this property to rotate and zoom around a fixed point instead.
     ///
     /// This property will be ignored by the pinch gesture if ``GestureOptions/pinchPanEnabled`` is set to `true`.
     public var focalPoint: CGPoint?
 
-    public init() {}
+    /// Initializes a `GestureOptions`.
+    /// - Parameters:
+    ///   - panEnabled: Whether the single-touch pan gesture is enabled.
+    ///   - pinchEnabled: Whether the pinch gesture is enabled.
+    ///   - rotateEnabled: Whether rotation gesture is enabled.
+    ///   - simultaneousRotateAndPinchZoomEnabled: Whether rotation is enabled for the pinch to zoom gesture.
+    ///   - pinchZoomEnabled: Whether zoom is enabled for the pinch gesture.
+    ///   - pinchPanEnabled: Whether pan is enabled for the pinch gesture.
+    ///   - pitchEnabled: Whether the pitch gesture is enabled.
+    ///   - doubleTapToZoomInEnabled: Whether double tapping the map with one touch results in a zoom-in animation.
+    ///   - doubleTouchToZoomOutEnabled: Whether single tapping the map with two touches results in a zoom-out animation.
+    ///   - quickZoomEnabled: Whether the quick zoom gesture is enabled.
+    ///   - panMode: The directions in which the map is allowed to move during a pan gesture.
+    ///   - panDecelerationFactor: The constant factor that determines how quickly pan deceleration animations happen.
+    ///   - focalPoint: The centerpoint for rotating and zooming the map.
+    public init(
+        panEnabled: Bool = true,
+        pinchEnabled: Bool = true,
+        rotateEnabled: Bool = true,
+        simultaneousRotateAndPinchZoomEnabled: Bool = true,
+        pinchZoomEnabled: Bool = true,
+        pinchPanEnabled: Bool = true,
+        pitchEnabled: Bool = true,
+        doubleTapToZoomInEnabled: Bool = true,
+        doubleTouchToZoomOutEnabled: Bool = true,
+        quickZoomEnabled: Bool = true,
+        panMode: PanMode = .horizontalAndVertical,
+        panDecelerationFactor: CGFloat = UIScrollView.DecelerationRate.normal.rawValue,
+        focalPoint: CGPoint? = nil
+    ) {
+        self.panEnabled = panEnabled
+        self.pinchEnabled = pinchEnabled
+        self.rotateEnabled = rotateEnabled
+        self.simultaneousRotateAndPinchZoomEnabled = simultaneousRotateAndPinchZoomEnabled
+        self.pinchZoomEnabled = pinchZoomEnabled
+        self.pinchPanEnabled = pinchPanEnabled
+        self.pitchEnabled = pitchEnabled
+        self.doubleTapToZoomInEnabled = doubleTapToZoomInEnabled
+        self.doubleTouchToZoomOutEnabled = doubleTouchToZoomOutEnabled
+        self.quickZoomEnabled = quickZoomEnabled
+        self.panMode = panMode
+        self.panDecelerationFactor = panDecelerationFactor
+        self.focalPoint = focalPoint
+    }
 }
--- Sources/MapboxMaps/Ornaments/OrnamentOptions.swift
+++ Sources/MapboxMaps/Ornaments/OrnamentOptions.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-private let defaultOrnamentsMargin = CGPoint(x: 8.0, y: 8.0)
-
 /// Used to configure Ornament-specific capabilities of the map
 ///
 /// All margin values are relative to the ``MapView``'s safe area. To allow the safe area
@@ -10,29 +8,47 @@ private let defaultOrnamentsMargin = CGPoint(x: 8.0, y: 8.0)
 public struct OrnamentOptions: Equatable {
 
     // MARK: - Scale Bar
+
     /// The ornament options for the map's scale bar.
-    public var scaleBar = ScaleBarViewOptions()
+    public var scaleBar: ScaleBarViewOptions
 
     // MARK: - Compass
 
     /// The ornament options for the map's compass view.
-    public var compass = CompassViewOptions()
+    public var compass: CompassViewOptions
 
     // MARK: - Logo View
-    /**
-     Per our terms of service, a Mapbox map is required to display both
-     a Mapbox logo as well as an information icon that contains attribution
-     information. See https://docs.mapbox.com/help/how-mapbox-works/attribution/
-     for details.
-     */
 
     /// The ornament options for the map's logo view.
-    public var logo = LogoViewOptions()
+    ///
+    /// Per our terms of service, a Mapbox map is required to display both
+    /// a Mapbox logo as well as an information icon that contains attribution
+    /// information. See https://docs.mapbox.com/help/how-mapbox-works/attribution/
+    /// for details.
+    public var logo: LogoViewOptions
 
     // MARK: - Attribution Button
 
     /// The ornament options for the map's attribution button.
-    public var attributionButton = AttributionButtonOptions()
+    public var attributionButton: AttributionButtonOptions
+
+    /// Initializes an `OrnamentOptions`.
+    /// - Parameters:
+    ///   - scaleBar: The ornament options for the map's scale bar.
+    ///   - compass: The ornament options for the map's compass view.
+    ///   - logo: The ornament options for the map's logo view.
+    ///   - attributionButton: The ornament options for the map's attribution button.
+    public init(
+        scaleBar: ScaleBarViewOptions = .init(),
+        compass: CompassViewOptions = .init(),
+        logo: LogoViewOptions = .init(),
+        attributionButton: AttributionButtonOptions = .init()
+    ) {
+        self.scaleBar = scaleBar
+        self.compass = compass
+        self.logo = logo
+        self.attributionButton = attributionButton
+    }
 }
 
 /// :nodoc:
@@ -46,53 +62,151 @@ public protocol OrnamentOptionsProtocol {
 
 /// Used to configure position, margin, and visibility for the map's scale bar view.
 public struct ScaleBarViewOptions: OrnamentOptionsProtocol, Equatable {
-    /// The default value for this property is `.topLeft`.
-    public var position: OrnamentPosition = .topLeading
+
+    /// The position of the scale bar view.
+    ///
+    /// The default value for this property is `.topLeading`.
+    public var position: OrnamentPosition
+
+    /// The margins of the scale bar view.
+    ///
     /// The default value for this property is `CGPoint(x: 8.0, y: 8.0)`.
-    public var margins: CGPoint = defaultOrnamentsMargin
+    public var margins: CGPoint
+
+    /// The visibility of the scale bar view.
+    ///
     /// The default value for this property is `.adaptive`.
-    public var visibility: OrnamentVisibility = .adaptive
+    public var visibility: OrnamentVisibility
+
     /// Specifies the whether the scale bar uses the metric system.
     /// True if the scale bar is using metric units, false if the scale bar is using imperial units.
-    public var useMetricUnits: Bool = Locale.current.usesMetricSystem
+    ///
+    /// The default value for this property is `Locale.current.usesMetricSystem`.
+    public var useMetricUnits: Bool
+
+    /// Initializes a `ScaleBarViewOptions`.
+    /// - Parameters:
+    ///   - position: The position of the scale bar view.
+    ///   - margins: The margins of the scale bar view.
+    ///   - visibility: The visibility of the scale bar view.
+    ///   - useMetricUnits: Whether the scale bar uses the metric system.
+    public init(
+        position: OrnamentPosition = .topLeading,
+        margins: CGPoint = .init(x: 8.0, y: 8.0),
+        visibility: OrnamentVisibility = .adaptive,
+        useMetricUnits: Bool = Locale.current.usesMetricSystem
+    ) {
+        self.position = position
+        self.margins = margins
+        self.visibility = visibility
+        self.useMetricUnits = useMetricUnits
+    }
 }
 
 /// Used to configure position, margin, image, and visibility for the map's compass view.
 public struct CompassViewOptions: OrnamentOptionsProtocol, Equatable {
-    /// The default value for this property is `.topRight`.
-    public var position: OrnamentPosition = .topTrailing
+
+    /// The position of the compass view.
+    ///
+    /// The default value for this property is `.topTrailing`.
+    public var position: OrnamentPosition
+
+    /// The margins of the compass view.
+    ///
     /// The default value for this property is `CGPoint(x: 8.0, y: 8.0)`.
-    public var margins: CGPoint = defaultOrnamentsMargin
+    public var margins: CGPoint
+
+    /// The image used for displaying the compass.
+    ///
     /// The default value for this property is nil, default compass image will be drawn.
     public var image: UIImage?
+
+    /// The visibility of the compass view.
+    ///
     /// The default value for this property is `.adaptive`.
-    public var visibility: OrnamentVisibility = .adaptive
+    public var visibility: OrnamentVisibility
+
+    /// Initializes a `CompassViewOptions`.
+    /// - Parameters:
+    ///   - position: The position of the compass view.
+    ///   - margins: The margins of the compass view.
+    ///   - image: The image used for displaying the compass.
+    ///   - visibility: The visibility of the compass view.
+    public init(
+        position: OrnamentPosition = .topTrailing,
+        margins: CGPoint = .init(x: 8.0, y: 8.0),
+        image: UIImage? = nil,
+        visibility: OrnamentVisibility = .adaptive
+    ) {
+        self.position = position
+        self.margins = margins
+        self.image = image
+        self.visibility = visibility
+    }
 }
 
 /// Used to configure position, margin, and visibility for the map's attribution button.
 public struct AttributionButtonOptions: OrnamentOptionsProtocol, Equatable {
-    /// The default value for this property is `.bottomRight`.
-    public var position: OrnamentPosition = .bottomTrailing
+
+    /// The position of the attribution button.
+    ///
+    /// The default value for this property is `.bottomTrailing`.
+    public var position: OrnamentPosition
+
+    /// The margins of the attribution button.
+    ///
     /// The default value for this property is `CGPoint(x: 8.0, y: 8.0)`.
-    public var margins: CGPoint = defaultOrnamentsMargin
+    public var margins: CGPoint
+
     /// The default value for this property is `visible`. Setting this property to `.adaptive`
     /// will lead to the same behavior as `.visible`. The attribution button will be visible
     /// as long as the map view is visible.
     /// :nodoc:
     /// Restricted API. Please contact Mapbox to discuss your use case if you intend to use this property.
     @_spi(Restricted) public var visibility: OrnamentVisibility = .visible
+
+    /// Initializes an `AttributionButtonOptions`.
+    /// - Parameters:
+    ///   - position: The position of the attribution button.
+    ///   - margins: The margins of the attribution button.
+    public init(
+        position: OrnamentPosition = .bottomTrailing,
+        margins: CGPoint = .init(x: 8.0, y: 8.0)
+    ) {
+        self.position = position
+        self.margins = margins
+    }
 }
 
 /// Used to configure position, margin, and visibility for the map's logo view.
 public struct LogoViewOptions: OrnamentOptionsProtocol, Equatable {
-    /// The default value for this property is `.bottomLeft`.
-    public var position: OrnamentPosition = .bottomLeading
+
+    /// The position of the logo view.
+    ///
+    /// The default value for this property is `.bottomLeading`.
+    public var position: OrnamentPosition
+
+    /// The margins of the logo view.
+    ///
     /// The default value for this property is `CGPoint(x: 8.0, y: 8.0)`.
-    public var margins: CGPoint = defaultOrnamentsMargin
+    public var margins: CGPoint
+
     /// The default value for this property is `visible`. Setting this property to `.adaptive`
     /// willl lead to the same behavior as `.visible`. The logo view will be visible as long
     /// as the map view is visible.
     /// :nodoc:
     /// Restricted API. Please contact Mapbox to discuss your use case if you intend to use this property.
     @_spi(Restricted) public var visibility: OrnamentVisibility = .visible
+
+    /// Initializes a `LogoViewOptions`.
+    /// - Parameters:
+    ///   - position: The position of the logo view.
+    ///   - margins: The margins of the logo view.
+    public init(
+        position: OrnamentPosition = .bottomLeading,
+        margins: CGPoint = .init(x: 8.0, y: 8.0)
+    ) {
+        self.position = position
+        self.margins = margins
+    }
 }

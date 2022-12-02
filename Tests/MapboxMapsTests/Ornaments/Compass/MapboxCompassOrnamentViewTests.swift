ee1c54dd9bc6fd50e8d42d5862139ffa2622c882
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-12-02 12:13:06 +0200

    Extract test method

--- Tests/MapboxMapsTests/Ornaments/Compass/MapboxCompassOrnamentViewTests.swift
+++ Tests/MapboxMapsTests/Ornaments/Compass/MapboxCompassOrnamentViewTests.swift
@@ -113,14 +113,29 @@ class MapboxCompassOrnamentViewTests: XCTestCase {
         // And
         XCTAssertFalse(compass.containerView.image!.isEqual(originalImage))
 
-        //Given
-        let nilImage: UIImage? = nil
-        //When
-        compass.updateImage(image: nilImage)
-        //Then
-        XCTAssertFalse(compass.containerView.image!.isEqual(expectedImage))
-        //And
-        XCTAssertTrue(compass.containerView.image!.isEqual(originalImage))
+    }
+
+    func testReturningDefaultCompassImage() {
+        // Given
+        let compass = MapboxCompassOrnamentView()
+        let originalImage = compass.containerView.image
+        let customImage = emptyImage(with: CGSize(width: 25, height: 25))
+
+        // When
+        compass.updateImage(image: customImage)
+
+        // Then
+        XCTAssertEqual(compass.containerView.image, customImage)
+
+        // When
+        compass.updateImage(image: nil)
+
+        // Then
+        XCTAssertNotEqual(compass.containerView.image, customImage)
+        // And
+        XCTAssertNotNil(compass.containerView.image)
+        // And
+        XCTAssertEqual(compass.containerView.image, originalImage)
     }
 
     private func emptyImage(with size: CGSize) -> UIImage? {

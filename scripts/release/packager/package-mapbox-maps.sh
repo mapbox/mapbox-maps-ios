18c0c9d50e78ef1b28d983e93bff7c2936a9927c
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-12-05 14:49:57 +0200

    Update CoreMaps (#1777)
    * Update dependencies
    
    * Changelog
    
    * Update Turf dependency
    
    * Update MME
    
    * Update podspec

--- scripts/release/packager/package-mapbox-maps.sh
+++ scripts/release/packager/package-mapbox-maps.sh
@@ -39,7 +39,7 @@ fi
 ../download-dependency.sh mapbox-common "$COMMON_ARTIFACT" "$COMMON_VERSION"
 ../download-dependency.sh mobile-maps-core "$CORE_ARTIFACT" "$CORE_VERSION"
 ../build-dependency.sh MapboxMobileEvents 'https://github.com/mapbox/mapbox-events-ios.git' "$MME_VERSION" "$LINK_TYPE"
-../build-dependency.sh Turf 'https://github.com/mapbox/turf-swift.git' "$TURF_VERSION" "$LINK_TYPE" "Turf iOS"
+../build-dependency.sh Turf 'https://github.com/mapbox/turf-swift.git' "$TURF_VERSION" "$LINK_TYPE"
 
 step 'Creating MapboxMaps.xcodeproj'
 mkdir .xcode

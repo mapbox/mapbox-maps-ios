b4b30d3a33e989afaddb9efb17fe17dab6f1f915
Roman Laitarenko <roman.laitarenko@mapbox.com>
2023-01-26 17:25:15 +0200

    Add a script to generate license

---
+++ scripts/license/generate-license.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+COREMAPS_VERSION=$(jq -r .MapboxCoreMaps scripts/release/packager/versions.json)
+TURF_VERSION=$(jq -r .Turf scripts/release/packager/versions.json)
+LICENSE_HEADER=$(<scripts/license/LICENSE-header.md)
+#export GITHUB_TOKEN=$(mbx-ci github reader token)
+
+TURF_LICENSE_CONTENT=$(gh api -H "Accept: application/vnd.github+json" "/repos/mapbox/turf-swift/contents/LICENSE.md?ref=$TURF_VERSION" --jq ".content" | base64 --decode)
+CORE_LICENSE_CONTENT=$(gh api -H "Accept: application/vnd.github+json" "/repos/mapbox/mapbox-gl-native-internal/contents/LICENSE-iOS.md?ref=maps-v$COREMAPS_VERSION" --jq ".content" | base64 --decode)
+
+
+LICENSE_CONTENT="// This file is generated.
+"$LICENSE_HEADER"
+### turf-swift, https://github.com/mapbox/turf-swift
+
+\`\`\`
+"$TURF_LICENSE_CONTENT"
+\`\`\`
+
+---
+
+"$CORE_LICENSE_CONTENT"
+// End of generated file.
+"
+
+echo "$LICENSE_CONTENT"

713160d8dc1c54991162c0b76363d2ef985d9299
Roman Gardukevich <roman.gardukevich@mapbox.com>
2023-02-10 15:05:48 +0200

    Fix tests

--- Tests/MapboxMapsTests/Camera/Mocks/MockCameraAnimatorsRunner.swift
+++ Tests/MapboxMapsTests/Camera/Mocks/MockCameraAnimatorsRunner.swift
@@ -20,6 +20,11 @@ final class MockCameraAnimatorsRunner: CameraAnimatorsRunnerProtocol {
         cancelAnimationsWithOwnersStub.call(with: owners)
     }
 
+    let cancelAnimationsOwnersTypesStub = Stub<(owners: [AnimationOwner], types: [AnimationType]), Void>()
+    func cancelAnimations(withOwners owners: [AnimationOwner], andTypes types: [AnimationType]) {
+        cancelAnimationsOwnersTypesStub.call(with: (owners: owners, types: types))
+    }
+
     let addStub = Stub<CameraAnimatorProtocol, Void>()
     func add(_ animator: CameraAnimatorProtocol) {
         addStub.call(with: animator)

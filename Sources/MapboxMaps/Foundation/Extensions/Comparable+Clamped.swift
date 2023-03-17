559942acf43d2fd0a28618e985712275b282e689
Ivan Persidsky <ivan.persidskii@mapbox.com>
2023-03-17 17:06:47 +0200

    Get rid of fatalErrors in StyleColor

--- Sources/MapboxMaps/Foundation/Extensions/Comparable+Clamped.swift
+++ Sources/MapboxMaps/Foundation/Extensions/Comparable+Clamped.swift
@@ -1,11 +1,24 @@
 internal extension Comparable {
+    /// Returns new value clamped to the specified range.
     func clamped(to limits: ClosedRange<Self>) -> Self {
-        if self > limits.upperBound {
-            return limits.upperBound
-        } else if self < limits.lowerBound {
-            return limits.lowerBound
-        } else {
-            return self
+        var newValue = self
+        newValue.clamp(to: limits)
+        return newValue
+    }
+
+    /// Clamps `self` value to the specified range. Returns true if actual clamping was made.
+    @discardableResult
+    mutating func clamp(to limits: ClosedRange<Self>) -> Bool {
+        guard self <= limits.upperBound else {
+            self = limits.upperBound
+            return true
         }
+
+        guard self >= limits.lowerBound else {
+            self = limits.lowerBound
+            return true
+        }
+
+        return false
     }
 }

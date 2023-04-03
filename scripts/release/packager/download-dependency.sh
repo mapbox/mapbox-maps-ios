e151e292f29c52acb6eeb175dbd135703ae8bc50
Roman Gardukevich <roman.gardukevich@mapbox.com>
2023-04-03 13:44:40 +0300

    [packager] Detect release type based on version naming (#1456)
    * [packager] Detect release type based on version naming
    
    * Detect snapshots

--- scripts/release/packager/download-dependency.sh
+++ scripts/release/packager/download-dependency.sh
@@ -9,10 +9,14 @@ trap finish EXIT
 SDK_REGISTRY_NAME=${1}
 SDK_REGISTRY_ARTIFACT=${2}
 VERSION=${3}
-RELEASE_FOLDER=${4}
+RELEASE_FOLDER="releases"
 
 mkdir .download
 pushd .download
+
+if [[ ${VERSION} = *"SNAPSHOT"* ]]; then
+    RELEASE_FOLDER="snapshots"
+fi
 
 step "Download dependency at https://api.mapbox.com/downloads/v2/$SDK_REGISTRY_NAME/$RELEASE_FOLDER/ios/packages/$VERSION/$SDK_REGISTRY_ARTIFACT.zip"
 curl -n "https://api.mapbox.com/downloads/v2/$SDK_REGISTRY_NAME/$RELEASE_FOLDER/ios/packages/$VERSION/$SDK_REGISTRY_ARTIFACT.zip" --output tmp.zip

b39a95f0f5cd48ed4bbb953eb2f0617f5e0dcc9a
Roman Gardukevich <roman.gardukevich@mapbox.com>
2023-04-24 14:09:22 +0300

    Update CoreStyles (#1496)
    * update core styles
    
    * update attribute test to use style-v12
    
    * Update changelog
    
    ---------
    
    Co-authored-by: ank27 <ankur.khandelwal@mapbox.com>

--- mapbox-maps-ios/Tests/MapboxMapsTests/Style/AttributionTests.swift
+++ mapbox-maps-ios/Tests/MapboxMapsTests/Style/AttributionTests.swift
@@ -177,7 +177,7 @@ class AttributionTests: XCTestCase {
         let metadataPath = Bundle.mapboxMaps.url(forResource: "MapboxMaps", withExtension: "json")!
         let data = try! Data(contentsOf: metadataPath)
         let metadata = try! JSONDecoder().decode(MapboxMapsMetadata.self, from: data)
-        let expectedURL = try XCTUnwrap(URL(string: "https://apps.mapbox.com/feedback/?referrer=\(Bundle.main.bundleIdentifier!)&owner=mapbox&id=streets-v11&access_token=test-token&map_sdk_version=\(metadata.version)#/2.00000/1.00000/3.00/4.0/5"))
+        let expectedURL = try XCTUnwrap(URL(string: "https://apps.mapbox.com/feedback/?referrer=\(Bundle.main.bundleIdentifier!)&owner=mapbox&id=streets-v12&access_token=test-token&map_sdk_version=\(metadata.version)#/2.00000/1.00000/3.00/4.0/5"))
 
         let mapView = MapView(frame: .zero, mapInitOptions: mapInitOptions)
         let url = mapView.mapboxFeedbackURL()

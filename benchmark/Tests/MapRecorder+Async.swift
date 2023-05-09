0bb8d47f4e53da1cf2c3fa8b60f758a8a757905e
Mai Mai <mai.mai@mapbox.com>
2023-05-09 18:50:38 +0300

    Mark async extension methods as MainActor (#1533)

--- benchmark/Tests/MapRecorder+Async.swift
+++ benchmark/Tests/MapRecorder+Async.swift
@@ -1,6 +1,8 @@
 @_spi(Internal) import MapboxMaps
 
 extension MapRecorder {
+
+    @MainActor
     func replay(content: String, playbackCount: Int = 1, playbackSpeedMultiplier: Double = 1.0) async {
         return await withCheckedContinuation { continuation in
             replay(content: content, playbackCount: playbackCount, playbackSpeedMultiplier: playbackSpeedMultiplier) {
@@ -9,4 +11,3 @@ extension MapRecorder {
         }
     }
 }
-

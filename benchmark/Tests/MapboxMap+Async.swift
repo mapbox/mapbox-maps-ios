e65c087fb8a603579a23f640dc184ceb934a0b20
Mai Mai <mai.mai@mapbox.com>
2023-05-10 11:03:45 +0300

    Mark async extension methods as MainActor (#1533) (#1535)

--- benchmark/Tests/MapboxMap+Async.swift
+++ benchmark/Tests/MapboxMap+Async.swift
@@ -2,6 +2,8 @@ import Foundation
 import MapboxMaps
 
 extension MapboxMap {
+
+    @MainActor
     public static func clearData(for resourceOptions: ResourceOptions) async throws {
         return try await withCheckedThrowingContinuation { continuation in
             clearData(for: resourceOptions) { error in

14d1b1d1bef6c340c219145d14f21646ac253d31
Roman Gardukevich <roman.gardukevich@mapbox.com>
2023-05-15 13:46:16 +0300

    Add Fingertips support in the Examples and DebugApp (#1548)
    * Add Fingertips support in the Examples
    * Add Fingertips support in the DebugApp

--- mapbox-maps-ios/Apps/Examples/Examples/AppDelegate.swift
+++ mapbox-maps-ios/Apps/Examples/Examples/AppDelegate.swift
@@ -1,11 +1,12 @@
 import UIKit
+import Fingertips
 
 @main
 //swiftlint:disable explicit_top_level_acl explicit_acl
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     lazy var window: UIWindow? = {
-        return UIWindow(frame: UIScreen.main.bounds)
+        return FingerTipWindow(frame: UIScreen.main.bounds)
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -44,7 +45,7 @@ final class SceneDelegate: NSObject, UISceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
 
-        let window = UIWindow(windowScene: windowScene)
+        let window = FingerTipWindow(windowScene: windowScene)
 
         let examplesTableViewController = ExampleTableViewController()
         let navigationController = UINavigationController(rootViewController: examplesTableViewController)

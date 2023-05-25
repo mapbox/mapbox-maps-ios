ac03ae3bb55710764fdd8cac52e50569b57f0246
Roman Laitarenko <roman.laitarenko@mapbox.com>
2023-05-25 13:52:48 +0300

    Add integration test for simultaneous pitch + pinch pan rotate recognition (#1570)

--- mapbox-maps-ios/Tests/GestureTests/PitchGesture.test.swift
+++ mapbox-maps-ios/Tests/GestureTests/PitchGesture.test.swift
@@ -34,6 +34,52 @@ final class PitchGestureTestCase: GestureTestCase {
         XCTAssertTrue(mapView.camera.cameraAnimators.isEmpty)
         XCTAssertEqual(mapView.cameraState.pitch, 85)
     }
+
+    func testPitchDoesntRecognizeSimultaneouslyWithPinchPanAndRotate() async throws {
+        let indices: [FingerIndex] = [.rightThumb, .rightIndex]
+        let distance = 200.0
+        let originalZoomLevel: CGFloat = 5
+        let originalLocation = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+        let originalBearing: CLLocationDirection = 45
+        let origin = mapView.center
+        let pitchEnd = origin.offsetBy(y: -60)
+        let panEnd = pitchEnd.offsetBy(x: -60)
+        camera.zoom = originalZoomLevel
+        camera.center = originalLocation
+        camera.bearing = originalBearing
+
+        try eventGenerator.twoFingerDown(indices, at: origin, withDistance: distance)
+
+        // pitch
+        try eventGenerator.twoFingerMove(indices,
+                                         to: pitchEnd,
+                                         withDistance: distance,
+                                         duration: Constants.pinchDuration)
+        // pinch(zoom)
+        try eventGenerator.twoFingerMove(indices,
+                                         to: pitchEnd,
+                                         withDistance: 80,
+                                         duration: Constants.pinchDuration)
+        // pan
+        try eventGenerator.twoFingerMove(indices,
+                                         to: panEnd,
+                                         withDistance: 80,
+                                         duration: Constants.pinchDuration)
+
+        // rotate
+        try eventGenerator.fingerPivot(indices,
+                                       aroundAnchor: panEnd,
+                                       byAngle: .pi / 2.0,
+                                       duration: Constants.pinchDuration)
+
+        try eventGenerator.twoFingerUp()
+
+        XCTAssertTrue(mapView.camera.cameraAnimators.isEmpty)
+        XCTAssertEqual(mapView.cameraState.pitch, 25)
+        XCTAssertEqual(mapView.cameraState.zoom, originalZoomLevel)
+        XCTAssertEqual(mapView.cameraState.center, originalLocation)
+        XCTAssertEqual(mapView.cameraState.bearing, originalBearing)
+    }
 }
 
 extension CGPoint {

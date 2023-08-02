e9f9c2d5771d4cc725915e807e85c5d596404b64
Tatiana Ian <tatiana.ian@mapbox.com>
2023-08-02 20:41:05 +0300

    update mapbox common dependency to test the bugfix

--- Package.swift
+++ Package.swift
@@ -3,6 +3,17 @@
 
 import PackageDescription
 
+let commonVersion = "23.7.0-7fa37a087805f1f045c68bfc65dbf7f281055117-SNAPSHOT"
+let commonChecksum = "6d25fc4b89cc0b8a951a838959d7b72e2c8a18719187b3777ef55d42fe8083ad"
+
+let coreVersion = "10.15.0"
+let coreChecksum = "97d1523212abf482deae5874bc8844663d89051cdaf737e941e1f8aff9e0ff9e"
+
+func folder(_ version: String) -> String { version.contains("SNAPSHOT") ? "snapshots" : "releases" }
+
+let mapboxMapsPath: String? = nil
+let mapboxMapsTestsPath: String? = nil
+
 let package = Package(
     name: "MapboxMaps",
     defaultLocalization: "en",
@@ -13,14 +24,22 @@ let package = Package(
             targets: ["MapboxMaps"]),
     ],
     dependencies: [
-        .package(name: "MapboxCoreMaps", url: "https://github.com/mapbox/mapbox-core-maps-ios.git", .exact("10.15.0")),
         // We keep MME dependency for compatibility reasons
         .package(name: "MapboxMobileEvents", url: "https://github.com/mapbox/mapbox-events-ios.git", .exact("1.0.10")),
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", .exact("23.7.0-7fa37a087805f1f045c68bfc65dbf7f281055117-SNAPSHOT")),
         .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", from: "2.0.0"),
         .package(name: "CocoaImageHashing", url: "https://github.com/ameingast/cocoaimagehashing", .exact("1.9.0"))
     ],
     targets: [
+        .binaryTarget(
+            name: "MapboxCoreMaps",
+            url: "https://api.mapbox.com/downloads/v2/mobile-maps-core/\(folder(coreVersion))/ios/packages/\(coreVersion)/MapboxCoreMaps.xcframework-dynamic.zip",
+            checksum: coreChecksum
+        ),
+        .binaryTarget(
+            name: "MapboxCommon",
+            url: "https://api.mapbox.com/downloads/v2/mapbox-common/\(folder(commonVersion))/ios/packages/\(commonVersion)/MapboxCommon.zip",
+            checksum: commonChecksum
+        ),
         .target(
             name: "MapboxMaps",
             dependencies: ["MapboxCoreMaps", "Turf", "MapboxCommon"],

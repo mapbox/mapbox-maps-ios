8b63df6895e182441bf621766b5379daa1893650
Roman Gardukevich <roman.gardukevich@mapbox.com>
2023-08-07 15:23:43 +0300

    Support store(in:) Combine cancellables in Mapbox Set<AnyCancelable> (#1723)

--- Sources/MapboxMaps/Foundation/AnyCancelable+Combine.swift
+++ Sources/MapboxMaps/Foundation/AnyCancelable+Combine.swift
@@ -1,6 +1,6 @@
 import Combine
 
-@available(iOS 13, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension AnyCancelable {
     /// Stores cancellable object in set of `Combine.AnyCancellable`.
     public func store(in set: inout Set<Combine.AnyCancellable>) {
@@ -10,5 +10,15 @@ extension AnyCancelable {
     /// Stores cancellable object in collection of `Combine.AnyCancellable`.
     public func store<C: RangeReplaceableCollection>(in collection: inout C) where C.Element == Combine.AnyCancellable {
         Combine.AnyCancellable(self.cancel).store(in: &collection)
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+extension AnyCancellable {
+    /// Stores this type-erasing cancellable instance in the specified set.
+    ///
+    /// - Parameter set: The set in which to store this ``AnyCancellable``.
+    public func store(in set: inout Set<AnyCancelable>) {
+        set.insert(AnyCancelable(cancel))
     }
 }

6e361b00ef832ad2ac1c950c47524abac02061a5
Roman Gardukevich <roman.gardukevich@mapbox.com>
2023-09-08 12:22:23 +0300

    Migrate debug-env XcodeGen to depend on other specs, more XcodeGen improvements (#1788)
    * Extract MapboxMaps target dependencies into XcodeGen template
    
    * Reuse internal and public xcodegen template for debug-env
    
    * Fix Xcode openning the wrong project
    
    * Reuse binary dependencies preparation script
    
    * Enable YAML autocompletion
    
    * Add extra repo files as Xcode resources
    
    * Remove circleci empty config
    
    * Automatically choose release folder based on version name
    
    * Skip binary preparation step if no change in versions/config were made

---
+++ scripts/prepare_binary_dependencies.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR/../" || exit
+
+VERSIONS_PATH="scripts/release/packager/versions.json"
+MAPBOX_CORE_MAPS_VERSION="$(jq -r .MapboxCoreMaps $VERSIONS_PATH)"
+MAPBOX_COMMON_VERSION="$(jq -r .MapboxCommon $VERSIONS_PATH)"
+
+release_folder() {
+    [[ $1 = *"SNAPSHOT"* ]] && echo "snapshots" || echo "releases"
+}
+
+cat <<EOF > Cartfile.MapboxCoreMaps.json
+{"$MAPBOX_CORE_MAPS_VERSION": "https://api.mapbox.com/downloads/v2/mobile-maps-core/$(release_folder "$MAPBOX_CORE_MAPS_VERSION")/ios/packages/$MAPBOX_CORE_MAPS_VERSION/MapboxCoreMaps.xcframework-dynamic.zip"}
+EOF
+
+cat <<EOF > Cartfile.MapboxCommon.json
+{"$MAPBOX_COMMON_VERSION": "https://api.mapbox.com/downloads/v2/mapbox-common/$(release_folder "$MAPBOX_COMMON_VERSION")/ios/packages/$MAPBOX_COMMON_VERSION/MapboxCommon.zip"}
+EOF
+
+cat <<EOF > Cartfile
+binary "Cartfile.MapboxCoreMaps.json" == $MAPBOX_CORE_MAPS_VERSION
+binary "Cartfile.MapboxCommon.json" == $MAPBOX_COMMON_VERSION
+github "mapbox/turf-swift" == $(jq -r .Turf $VERSIONS_PATH | sed 's/^v//')
+
+EOF
+
+cat <<EOF > Cartfile.xcconfig
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+EOF
+
+mkdir -p Carthage
+CURRENT_CONFIG_HASH=$(cat Cartfile Cartfile.xcconfig Cartfile.MapboxCoreMaps.json Cartfile.MapboxCommon.json | shasum -a 256)
+
+EXPECTED_CONFIG_HASH=""
+if [[ -f Carthage/config.version ]]; then
+    EXPECTED_CONFIG_HASH=$(cat Carthage/config.version)
+fi
+
+if [[ "$CURRENT_CONFIG_HASH" != "$EXPECTED_CONFIG_HASH" ]]; then
+    if [[ $(command -v "carthage") ]]; then
+        CARTHAGE_BINARY="carthage"
+    elif [[ $(command -v "mint") ]]; then
+        CARTHAGE_BINARY="mint run Carthage/Carthage"
+    else
+        echo "error: Carthage not found"
+        exit 1
+    fi
+    $CARTHAGE_BINARY update --use-netrc --platform ios --verbose --use-xcframeworks --cache-builds --configuration Cartfile.xcconfig
+
+    echo "$CURRENT_CONFIG_HASH" > Carthage/config.version
+fi
+
+rm -f Cartfile Cartfile.resolved Cartfile.xcconfig Cartfile.MapboxCoreMaps.json Cartfile.MapboxCommon.json

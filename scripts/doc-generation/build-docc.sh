5e697b623b1055a624ede7a0f1ae2fec93fa81f1
Chris Whong <chris.m.whong@gmail.com>
2023-11-15 06:58:09 -0500

    add custom css to make the docc site match docs.mapbox.com styles (#1905)
    * add custom css to make the docc site match docs.mapbox.com styles
    
    * Support docc theme on CI
    
    * Fix issue with invalid Open Sans font
    
    * Fix logo on mobile
    
    * Use build-docc script for DocC preview
    
    * Update favicons
    
    * Fix base path for docc docs in prs
    
    * Fix / issue
    
    ---------
    
    Co-authored-by: Ivan Persidsky <ivan.persidskii@mapbox.com>
    Co-authored-by: Roman Gardukevich <roman.gardukevich@mapbox.com>

---
+++ scripts/doc-generation/build-docc.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+public_repo_root="$script_dir/../../"
+
+# Usage: build-docc.sh <hosting_base_path> <output_path>
+
+hosting_base_path=${1:-"/ios/maps/api/latest/"}
+docc_archive=${2:-"$(pwd)/MapboxMaps.doccarchive"}
+theme_dir="$script_dir/docc-theme"
+docc_sources_path="$public_repo_root/Sources/MapboxMaps/Documentation.docc"
+
+
+cleanup() {
+    rm -fr "$docc_sources_path/"*.html
+}
+
+process_template() {
+    cp -f "$theme_dir/$1" "$docc_sources_path/$1"
+    sed -i "" "s|BASE_PATH|$hosting_base_path|g" "$docc_sources_path/$1"
+}
+
+build_doc() {
+    pushd "$public_repo_root/"
+    xcodebuild docbuild -config Release -scheme MapboxMaps -destination "generic/platform=iOS" \
+        OTHER_DOCC_FLAGS="--warnings-as-errors --experimental-enable-custom-templates --output-path $docc_archive --hosting-base-path $hosting_base_path"
+    popd
+}
+
+patch_docc_theme() {
+    index_css=$(find "$docc_archive/css" -name 'index.*.css')
+    cat "$theme_dir/docc-inject.css" >> "$index_css"
+
+    cp "$theme_dir/assembly.min.css"  "$docc_archive/css/"
+    cp "$theme_dir/page-shell-styles.css"  "$docc_archive/css/"
+}
+
+update_favicon() {
+    cp "$theme_dir/favicon.ico" "$docc_archive/"
+    cp "$theme_dir/favicon.svg" "$docc_archive/"
+}
+
+rm -fr "$docc_archive"
+trap cleanup EXIT ERR
+process_template footer.html
+process_template header.html
+build_doc
+patch_docc_theme
+update_favicon
+echo Created "$docc_archive"

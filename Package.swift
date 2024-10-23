92da9aed4a94d440b9ddeacd879d6f9eb61ad14b
Ivan Persidsky <ivan.persidskii@mapbox.com>
2024-10-23 12:41:54 +0300

    Bump versions for 11.8.0-rc.1

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreMaps = MapsDependency.coreMaps(version: "11.8.0-beta.1")
-let common = MapsDependency.common(version: "24.8.0-beta.1")
+let coreMaps = MapsDependency.coreMaps(version: "11.8.0-rc.1")
+let common = MapsDependency.common(version: "24.8.0-rc.1")
 
 let mapboxMapsPath: String? = nil
 

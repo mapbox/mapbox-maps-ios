b73a7e83d85a62e9dea7b0b24916108340f85864
Sergey Akentev <sergey.akentev@mapbox.com>
2025-04-08 10:05:29 +0300

    Bump versions v0.13 (#3152)
    GitOrigin-RevId: 72b1c4e256912c790c9e8d97e34f8712290fe26c

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.12.0-beta.1"
-let coreMapsVersion: Version = "11.12.0-beta.1"
+let commonVersion: Version = "24.13.0-beta.1"
+let coreMapsVersion: Version = "11.13.0-beta.1"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

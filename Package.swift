5f46396a6816c6d89a4ca8df39c22db2f9f9e3d2
Sergey Akentev <sergey.akentev@mapbox.com>
2025-04-23 14:26:14 +0300

    Prepare release v0.12-rc.1 (#3414)
    Co-authored-by: Ivan Persidsky <ivan.persidskii@mapbox.com>
    GitOrigin-RevId: f75d611102a45d980314d65f0620cd10e0b14b35

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.13.0-SNAPSHOT-04-23--05-20.git-ad48dc8"
-let coreMapsVersion: Version = "11.13.0-SNAPSHOT-04-23--05-20.git-ad48dc8"
+let commonVersion: Version = "24.12.0-rc.1"
+let coreMapsVersion: Version = "11.12.0-rc.1"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

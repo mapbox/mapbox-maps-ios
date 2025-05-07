c141954157c3aaf09bc1bc3d233239666421b96e
Sergey Akentev <sergey.akentev@mapbox.com>
2025-05-07 10:58:40 +0300

    Write versions v0.12
    GitOrigin-RevId: ede0e9f411d03fb82ec5f410c1f046e34692032f

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.12.0-rc.1"
-let coreMapsVersion: Version = "11.12.0-rc.1"
+let commonVersion: Version = "24.12.0"
+let coreMapsVersion: Version = "11.12.0"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

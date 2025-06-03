960e16dbcb09fd10f340a5776abb9e6fc1ea1a44
Sergey Akentev <sergey.akentev@mapbox.com>
2025-06-03 16:52:23 +0300

    Bump versions to rc.1
    GitOrigin-RevId: 0ea02efba146c1f9904680d866963e370ec3eb1a

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.13.0-SNAPSHOT-06-02--09-46.git-cfe1535"
-let coreMapsVersion: Version = "11.13.0-SNAPSHOT-06-02--09-46.git-cfe1535"
+let commonVersion: Version = "24.13.0-rc.1"
+let coreMapsVersion: Version = "11.13.0-rc.1"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

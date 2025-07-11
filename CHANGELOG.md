d38179972d513ddeb259ac8fbcb28ae6504620bb
Ivan Persidskii <ivan.persidskii@mapbox.com>
2025-07-11 18:44:28 +0300

    Bump version 0.12.5 (#5129)
    GitOrigin-RevId: c20bf2d654e13d97801189375e32ed59827d5f5f

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ Mapbox welcomes participation and contributions from everyone.
 
 ## main
 
+## 11.12.5 - 11 July, 2025
+
 ## 11.12.4 - 12 June, 2025
 
 ## 11.12.3 - 05 June, 2025
--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.12.4"
-let coreMapsVersion: Version = "11.12.4"
+let commonVersion: Version = "24.12.5"
+let coreMapsVersion: Version = "11.12.5"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

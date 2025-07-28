538bbe681d7ed86e83f50bb0e3a044feae941eba
Ivan Persidskii <ivan.persidskii@mapbox.com>
2025-07-28 17:03:47 +0300

    Bump versions 0.14.0 (#5475)
    GitOrigin-RevId: d42629ac315c56ee52c4bc8f4d480199af9ed88d

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.14.0-rc.1"
-let coreMapsVersion: Version = "11.14.0-rc.1"
+let commonVersion: Version = "24.14.0"
+let coreMapsVersion: Version = "11.14.0"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

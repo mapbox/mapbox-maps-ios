16bf8065216910d5777a21cf0de7e3f8191f3424
Alexander Vasin <aleksandr.vasin@mapbox.com>
2025-07-28 16:19:02 +0300

    Preparing v0.13.5 patch release (#5468)
    https://github.com/mapbox/change-management/issues/4916
    
    GitOrigin-RevId: 2b406716f5cd1ef5476b20fb520026a59192e4d5

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.13.4"
-let coreMapsVersion: Version = "11.13.4"
+let commonVersion: Version = "24.13.5"
+let coreMapsVersion: Version = "11.13.5"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

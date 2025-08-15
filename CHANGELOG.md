ffb33dc0c7f380fc373196a28577b93a478e1711
Dmitrii Serebriakov <dmitrii.serebriakov@mapbox.com>
2025-08-15 16:55:05 +0300

    Bump versions and update changelogs for v0.14.2 (#5892)
    GitOrigin-RevId: 144720625c5415a470c7f3e5762b69604b2e7d00

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ Mapbox welcomes participation and contributions from everyone.
 
 ## main
 
+## 11.14.2 - 15 August, 2025
+
 ## 11.14.1 - 07 August, 2025
 
 ## 11.14.0 - 30 July, 2025
--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.14.1"
-let coreMapsVersion: Version = "11.14.1"
+let commonVersion: Version = "24.14.2"
+let coreMapsVersion: Version = "11.14.2"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

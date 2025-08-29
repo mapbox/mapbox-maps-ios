fe365e03af2800ca1731b48c17f3fcb712c5535d
Johann Muszynski <johann.muszynski@mapbox.com>
2025-08-29 14:39:01 +0300

    [release/v0.14] Version bump and changelogs for v0.14.3 (#6153)
    Co-authored-by: Natig Babayev <natig.babayev@mapbox.com>
    GitOrigin-RevId: 595e38aa46a49b059404319fb887f379b7e626a3

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.14.2"
-let coreMapsVersion: Version = "11.14.2"
+let commonVersion: Version = "24.14.3"
+let coreMapsVersion: Version = "11.14.3"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

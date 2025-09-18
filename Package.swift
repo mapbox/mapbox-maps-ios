c89b9e15545247647b03c2ba8e0bf6b7e59d1297
Sergey Akentev <sergey.akentev@mapbox.com>
2025-09-18 11:20:20 +0300

    Bump versions to alpha.4
    GitOrigin-RevId: d4d5a46d4f069e186a90959b452b84ba24964750

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.16.0-alpha.3"
-let coreMapsVersion: Version = "11.16.0-alpha.3"
+let commonVersion: Version = "24.16.0-alpha.4"
+let coreMapsVersion: Version = "11.16.0-alpha.4"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

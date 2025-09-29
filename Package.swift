3043e4c6f994babb264698b7103b0f8197dc7488
Barbara Flindt <barbara.flindt@mapbox.com>
2025-09-29 14:01:36 +0300

    [release/v0.14] Bump to v0.14.7 (#6929)
    Bump to v0.14.7
    
    cc @mapbox/nav-core-sdk
    cc @mapbox/core-sdk
    cc @mapbox/search-sdk-native
    cc @mapbox/maps-android
    cc @mapbox/maps-ios
    cc @mapbox/gl-native
    cc @mapbox/navnative
    cc @mapbox/sdk-ci
    
    GitOrigin-RevId: a967d71c2ded6747e32c672d53e56ec4e5922749

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.14.6"
-let coreMapsVersion: Version = "11.14.6"
+let commonVersion: Version = "24.14.7"
+let coreMapsVersion: Version = "11.14.7"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

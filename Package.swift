0c92cef511d5b17d1eced72e78308c68fc01675b
Artem Panfilov <artem.panfilov@mapbox.com>
2025-10-03 16:45:05 +0300

    Bump SDK versions to v0.17.0 release (#7102)
    Updates all SDK projects tov v0.17
    
    cc @mapbox/sdk-ci
    cc @mapbox/maps-ios
    cc @mapbox/navnative
    cc @mapbox/search-sdk-android
    cc @mapbox/core-sdk
    cc @mapbox/maps-android
    cc @mapbox/search-sdk-native
    cc @mapbox/gl-native
    
    GitOrigin-RevId: 8de4a542882ec00d9e7ebbc92d32bd5140ff8082

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.16.0-SNAPSHOT-10-03--04-29.git-684cf83"
-let coreMapsVersion: Version = "11.16.0-SNAPSHOT-10-03--04-29.git-684cf83"
+let commonVersion: Version = "24.17.0"
+let coreMapsVersion: Version = "11.17.0"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

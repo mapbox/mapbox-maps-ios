6288533347a1f27de8c442a56baded1e4a2c2f38
Artem Panfilov <artem.panfilov@mapbox.com>
2025-10-07 17:27:42 +0300

    [skip ci] Bump release version to v0.16.0-rc.1
    GitOrigin-RevId: 69107669eb45a197de954b954640491ae2e41107

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.16.0-SNAPSHOT-10-06--14-47.git-6d4bb33"
-let coreMapsVersion: Version = "11.16.0-SNAPSHOT-10-06--14-47.git-6d4bb33"
+let commonVersion: Version = "24.16.0-rc.1"
+let coreMapsVersion: Version = "11.16.0-rc.1"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

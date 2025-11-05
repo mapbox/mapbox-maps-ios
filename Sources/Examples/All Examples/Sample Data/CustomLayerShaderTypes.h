61ef040c11333c5c6486af4e4dbf606e72bef9be
Ivan Persidskii <ivan.persidskii@mapbox.com>
2025-11-05 12:54:17 +0200

    Enable pre-commit-hooks in maps-ios (#7830)
    GitOrigin-RevId: 0e292b49fbb399f5fcfeb6764b2e284ce8fed92c

--- Sources/Examples/All Examples/Sample Data/CustomLayerShaderTypes.h
+++ Sources/Examples/All Examples/Sample Data/CustomLayerShaderTypes.h
@@ -3,32 +3,28 @@
 
 #include <simd/simd.h>
 
-// Buffer indexes shared between shader and platform code to ensure Metal shader buffer inputs
-// match Metal API buffer set calls.
-typedef enum
-{
-    VertexInputIndexVertices = 0,
-    VertexInputIndexTransformation = 1,
+// Buffer indexes shared between shader and platform code to ensure Metal shader
+// buffer inputs match Metal API buffer set calls.
+typedef enum {
+  VertexInputIndexVertices = 0,
+  VertexInputIndexTransformation = 1,
 } VertexInputIndex;
 
-typedef struct
-{
-    simd_float3 position;
-    simd_float4 color;
+typedef struct {
+  simd_float3 position;
+  simd_float4 color;
 } VertexData;
 
-typedef struct
-{
-    simd_float3 pos_merc;
-    simd_float3 pos_ecef;
-    simd_float3 color;
+typedef struct {
+  simd_float3 pos_merc;
+  simd_float3 pos_ecef;
+  simd_float3 color;
 } GlobeVertexData;
 
-typedef struct
-{
-    simd_float4x4 u_matrix_merc;
-    simd_float4x4 u_matrix_ecef;
-    float    u_transition;
-    float    u_point_size;
+typedef struct {
+  simd_float4x4 u_matrix_merc;
+  simd_float4x4 u_matrix_ecef;
+  float u_transition;
+  float u_point_size;
 } GlobeUniforms;
 #endif /* CustomLayerShaderTypes_h */

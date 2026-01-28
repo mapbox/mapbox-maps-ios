b4cd2093fd134777edbf3c7f1698cd6ae782af04
Sergey Akentev <sergey.akentev@mapbox.com>
2026-01-28 19:04:40 +0200

    [skip ci] Bump release version to v0.19.0-beta.1
    GitOrigin-RevId: c74c7a33ff0df6a310dd1eef747ce5f02f9c1167

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.19.0-SNAPSHOT-01-26--10-41.git-2116ed4"
-let coreMapsVersion: Version = "11.19.0-SNAPSHOT-01-26--10-41.git-2116ed4"
+let commonVersion: Version = "24.19.0"
+let coreMapsVersion: Version = "11.19.0"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

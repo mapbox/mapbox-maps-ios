1c38d46526eecc731c9a7d68548216477d6fa4bf
Fedor Tolstonozhenko <fedor.tolstonozhenko@mapbox.com>
2026-03-06 11:19:49 +0100

    [skip ci][no ci] Set versions to 0.21.0
    GitOrigin-RevId: 868f6b0db91fc38f712ce8ab04da2f305a54a9c6

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.21.0-rc.1-SNAPSHOT-03-05--04-54.git-a72f5d4"
-let coreMapsVersion: Version = "11.21.0-rc.1-SNAPSHOT-03-05--04-54.git-a72f5d4"
+let commonVersion: Version = "24.21.0"
+let coreMapsVersion: Version = "11.21.0"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

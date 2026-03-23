f03cb23774ae0c737dfffba9507b41920c24f83f
Fedor Tolstonozhenko <fedor.tolstonozhenko@mapbox.com>
2026-03-23 15:29:25 +0100

    [skip ci] Bump release version to v0.21.0-rc.1
    GitOrigin-RevId: 0116def830de3d143a23707b7c698015d1023d73

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.22.0"
-let coreMapsVersion: Version = "11.22.0"
+let commonVersion: Version = "24.21.0-rc.1"
+let coreMapsVersion: Version = "11.21.0-rc.1"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil

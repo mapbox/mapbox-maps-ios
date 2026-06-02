mapbox/mapbox-search-ios

snapshot: update local versions to SNAPSHOT-05-21--13-19.git-fede0de (#13849)
GitOrigin-RevId: ccb89dbb07b9355da315c2b4f263e7eb835cb3bf

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let coreSearchVersion: Version = "2.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
-let mapboxCommonSDKVersion: Version = "24.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
+let coreSearchVersion: Version = "2.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
+let mapboxCommonSDKVersion: Version = "24.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
 
 let package = Package(
     name: "MapboxSearch",

mapbox/mapbox-maps-ios

Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
-let coreMapsVersion: Version = "11.25.0-SNAPSHOT-05-19--06-51.git-92385b2"
+let commonVersion: Version = "24.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
+let coreMapsVersion: Version = "11.25.0-SNAPSHOT-05-21--13-19.git-fede0de"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil
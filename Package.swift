2c90c0b2506980c70cf29d9047b8a3d2a51a4929
mapbox-ci <no-reply@mapbox.com>
2026-09-07 11:56:58 +0000

    [skip ci][no ci] Bump snapshot versions `SNAPSHOT-09-07--10-14.git-86caf07`
    GitOrigin-RevId: 805763dfcf3f0a8be89fb3ca498dc6465bf4952e

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 import Foundation
 
-let commonVersion: Version = "24.32.0"
-let coreMapsVersion: Version = "11.32.0"
+let commonVersion: Version = "24.32.0-SNAPSHOT-09-07--10-14.git-86caf07"
+let coreMapsVersion: Version = "11.32.0-SNAPSHOT-09-07--10-14.git-86caf07"
 let turfVersion: Version = "4.0.0"
 
 let mapboxMapsPath: String? = nil
